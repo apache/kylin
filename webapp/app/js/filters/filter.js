2799812032b2a03dfc2399a1bfbd2be88c1766be
jiazhong <jiazhong@ebay.com>
2014-11-10 17:13:11 +0800

    add filter for pie height

--- webapp/app/js/filters/filter.js
+++ webapp/app/js/filters/filter.js
@@ -78,4 +78,11 @@ KylinApp
                 number = Math.floor(Math.log(bytes) / Math.log(1024));
             return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
         }
+    }).filter('resizePieHeight',function(){
+    return function(item){
+        if(item<150){
+            return 1300;
+        }
+        return 1300;
+    }
     });
