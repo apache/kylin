463d157dfd7e7b5e41998fffe63b51c8477ffc35
Li Yang <liyang@apache.org>
2014-11-28 03:15:42 +0000

    initial realization

--- metadata/src/main/java/com/kylinolap/metadata/model/realization/IDataModelRealization.java
+++ metadata/src/main/java/com/kylinolap/metadata/model/realization/IDataModelRealization.java
@@ -1,9 +1,11 @@
 package com.kylinolap.metadata.model.realization;
 
+import java.util.Set;
+
 public interface IDataModelRealization {
 
     /** Return a cost indicator for the realization, the lower, the better. Range from 0 - 100 typically. */
     int getCost();
     
-    
+    Set<TblColRef> getSupportedColumns();
 }
