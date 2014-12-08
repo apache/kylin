ed94db4849c196e1b7239fe48d48e27e3ddcae6b
shaofengshi <shaoshi@ebay.com>
2014-12-08 13:09:58 +0800

    Update test cases (for tables that not in ‘default’ schema).

--- query/src/test/resources/query/sql/query51.sql
+++ query/src/test/resources/query/sql/query51.sql
@@ -1,7 +1,7 @@
 select test_kylin_fact.lstg_format_name, test_cal_dt.week_beg_dt,sum(test_kylin_fact.price) as GMV 
  , count(*) as TRANS_CNT 
  from test_kylin_fact 
- inner JOIN test_cal_dt 
+ inner JOIN edw.test_cal_dt as test_cal_dt 
  ON test_kylin_fact.cal_dt = test_cal_dt.cal_dt 
  where  DATE '2013-03-24'  <= test_cal_dt.week_beg_dt
  group by test_kylin_fact.lstg_format_name, test_cal_dt.week_beg_dt 
--- query/src/test/resources/query/sql/query56.sql
+++ query/src/test/resources/query/sql/query56.sql
@@ -1,7 +1,7 @@
 select test_cal_dt.week_beg_dt, sum(test_kylin_fact.price) as GMV 
  , count(*) as TRANS_CNT 
  from test_kylin_fact 
- inner JOIN test_cal_dt 
+ inner JOIN edw.test_cal_dt as test_cal_dt 
  ON test_kylin_fact.cal_dt = test_cal_dt.cal_dt
  where
  extract(MONTH from test_cal_dt.week_beg_dt) = 12
