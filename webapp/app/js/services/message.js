963818cd44681cd112330d773d4cbed00ce108e8
cxiao <cxiao@ebay.com>
2014-12-11 17:18:02 +0800

    Customize Messenger widget

--- webapp/app/js/services/message.js
+++ webapp/app/js/services/message.js
@@ -1,10 +1,23 @@
 KylinApp.service('MessageService', function (GraphBuilder) {
-    this.sendMsg = function ($msg, $type, $actions) {
-        Messenger().post({
-            message: $msg,
-            type: angular.isDefined($type) ? $type : 'info',
-            actions: $actions,
+    var options = {
+        extraClasses: 'messenger-fixed messenger-on-top messenger-on-right',
+        theme: 'air'
+    };
+
+    this.sendMsg = function (msg, type, actions, sticky) {
+
+        var data = {
+            message: msg,
+            type: angular.isDefined(type) ? type : 'info',
+            actions: actions,
             showCloseButton: true
-        });
+        };
+
+        // Whether sticky the message, otherwise it will hide after a period.
+        if (angular.isDefined(sticky) && sticky === true) {
+            data.hideAfter = false;
+        }
+
+        Messenger(options).post(data);
     }
 });
