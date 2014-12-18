0681b39a815b864576b279db38a0a19cd559178d
shaofengshi <shaoshi@ebay.com>
2014-12-18 17:50:12 +0800

    Update KylinQueryTest with mini cluster

--- query/src/test/java/com/kylinolap/query/test/KylinQueryTest.java
+++ query/src/test/java/com/kylinolap/query/test/KylinQueryTest.java
@@ -18,12 +18,12 @@ package com.kylinolap.query.test;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Properties;
 
-import com.kylinolap.storage.hbase.coprocessor.observer.ObserverEnabler;
 import org.apache.commons.lang3.StringUtils;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
@@ -35,11 +35,13 @@ import org.junit.Test;
 import com.kylinolap.common.KylinConfig;
 import com.kylinolap.common.util.AbstractKylinTestCase;
 import com.kylinolap.common.util.HBaseMetadataTestCase;
+import com.kylinolap.common.util.HBaseMiniclusterMetadataTestCase;
 import com.kylinolap.cube.CubeManager;
 import com.kylinolap.metadata.project.ProjectInstance;
 import com.kylinolap.query.enumerator.OLAPQuery;
 import com.kylinolap.query.relnode.OLAPContext;
 import com.kylinolap.query.schema.OLAPSchemaFactory;
+import com.kylinolap.storage.hbase.coprocessor.observer.ObserverEnabler;
 
 public class KylinQueryTest extends KylinTestBase {
 
@@ -52,14 +54,15 @@ public class KylinQueryTest extends KylinTestBase {
         preferCubeOf(joinType);
     }
 
-    protected static void setupAll() throws SQLException {
+    protected static void setupAll() throws SQLException, IOException, ClassNotFoundException, InterruptedException {
         setUpEnv();
         setUpCubeConn();
         setUpH2Conn();
     }
 
-    private static void setUpEnv() {
-        HBaseMetadataTestCase.staticCreateTestMetadata(AbstractKylinTestCase.SANDBOX_TEST_DATA);
+    private static void setUpEnv() throws IOException, ClassNotFoundException, InterruptedException {
+        HBaseMetadataTestCase.staticCreateTestMetadata(AbstractKylinTestCase.MINICLUSTER_TEST_DATA);
+        HBaseMiniclusterMetadataTestCase.startupMinicluster();
         config = KylinConfig.getInstanceFromEnv();
     }
 
@@ -84,6 +87,7 @@ public class KylinQueryTest extends KylinTestBase {
         printInfo("tearDown");
         printInfo("Closing connection...");
         clean();
+        HBaseMiniclusterMetadataTestCase.shutdownMiniCluster();
     }
 
     protected static void clean() {
