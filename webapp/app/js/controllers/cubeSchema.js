faf781cb64e56ba6c92a1b4d24e194ecc297bdae
cxiao <cxiao@ebay.com>
2014-12-23 23:48:34 +0800

    prototype for cube new dimensions

--- webapp/app/js/controllers/cubeSchema.js
+++ webapp/app/js/controllers/cubeSchema.js
@@ -10,6 +10,8 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
 
     $scope.wizardSteps = [
         {title: 'Cube Info', src: 'partials/cubeDesigner/info.html', isComplete: false},
+        {title: 'Data Model', src: 'partials/cubeDesigner/data_model.html', isComplete: false},
+        {title: 'Dimensions', src: 'partials/cubeDesigner/data_dimensions.html', isComplete: false},
         {title: 'Dimensions', src: 'partials/cubeDesigner/dimensions.html', isComplete: false},
         {title: 'Measures', src: 'partials/cubeDesigner/measures.html', isComplete: false},
         {title: 'Filter', src: 'partials/cubeDesigner/filter.html', isComplete: false},
@@ -21,6 +23,258 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
     $scope.wizardSteps.push({title: 'Overview', src: 'partials/cubeDesigner/overview.html', isComplete: false});
 
     $scope.curStep = $scope.wizardSteps[0];
+
+
+
+    /** START: js about data model **/
+    var DataModel = {
+        init: function () {
+            return {
+                "name": "",
+                "fact_table": "",
+                "lookups": []
+            }
+        }
+    };
+
+    $scope.newModel = DataModel.init();
+
+    var Lookup = {
+        init: function () {
+            return {
+                "table": "",
+                "join": {
+                    "type": "",
+                    "primary_key": [],
+                    "foreign_key": []
+                }
+            };
+        }
+    };
+
+    // Initialize params.
+    $scope.lookupState = {
+        editing: false,
+        editingIndex: -1,
+        showPanel: false,
+        filter: ''
+    };
+
+    $scope.newLookup = Lookup.init();
+
+    var lookupList = $scope.newModel.lookups;
+
+    $scope.addLookup = function () {
+        // Push newLookup which bound user input data.
+        lookupList.push(angular.copy($scope.newLookup));
+
+        $scope.resetParams();
+    };
+
+    $scope.editLookup = function (lookup) {
+        $scope.lookupState.editingIndex = lookupList.indexOf(lookup);
+        $scope.lookupState.editing = true;
+
+        // Make a copy of model will be editing.
+        $scope.newLookup = angular.copy(lookup);
+
+        $scope.lookupState.showPanel = true;
+    };
+
+    $scope.doneEditLookup = function () {
+        // Copy edited model to destination model.
+        angular.copy($scope.newLookup, lookupList[$scope.lookupState.editingIndex]);
+
+        $scope.resetParams();
+    };
+
+    $scope.cancelLookup = function () {
+        $scope.resetParams();
+    };
+
+    $scope.removeLookup = function (lookup) {
+        lookupList.splice(lookupList.indexOf(lookup), 1);
+    };
+
+    $scope.resetParams = function () {
+        $scope.lookupState.editing = false;
+        $scope.lookupState.editingIndex = -1;
+        $scope.lookupState.showPanel = false;
+
+        $scope.newLookup = Lookup.init();
+    };
+
+    $scope.logModel = function () {
+        console.dir($scope.newModel);
+    };
+    /** END: js about data model **/
+
+
+    /** START: js about data model dimensions **/
+    $scope.availableColumns = [];
+    $scope.chosenColumns = [];
+
+    // Selected item bound to UI.
+    $scope.selectedColumns = {
+        available: [],
+        chosen: []
+    };
+
+    // Dump available columns plus column table name, whether is from lookup table.
+    $scope.dumpColumns = function () {
+        // At first dump the columns of fact table.
+        var cols = $scope.getColumnsByTable($scope.newModel.fact_table);
+
+        for (var i = 0; i < cols.length; i++) {
+            cols[i].table = $scope.newModel.fact_table;
+            cols[i].isLookup = false;
+        }
+
+        $scope.availableColumns = $scope.availableColumns.concat(cols);
+
+        // Then dump each lookup tables.
+        var lookups = $scope.newModel.lookups;
+
+        for (var j = 0; j < lookups.length; j++) {
+            var cols2 = $scope.getColumnsByTable(lookups[j].table);
+
+            for (var k = 0; k < cols2.length; k++) {
+                cols2[k].table = lookups[j].table;
+                cols2[k].isLookup = true;
+            }
+
+            $scope.availableColumns = $scope.availableColumns.concat(cols2);
+        }
+
+        console.dir($scope.availableColumns);
+    };
+
+    var columnKey = function (col) {
+        return col.table + '.' + col.name;
+    };
+
+    var filterOut = function (original, toFilter) {
+        var filtered = [];
+
+        angular.forEach(original, function (entity) {
+            var match = false;
+
+            for (var i = 0; i < toFilter.length; i++) {
+                // Equals columns.
+                if (columnKey(toFilter[i]) == columnKey(entity)) {
+                    match = true;
+                    break;
+                }
+            }
+
+            if (!match) {
+                filtered.push(entity);
+            }
+        });
+
+        return filtered;
+    };
+
+    $scope.refreshAvailable = function () {
+        $scope.availableColumns = filterOut($scope.availableColumns, $scope.chosenColumns);
+        $scope.selectedColumns.available = [];
+        $scope.selectedColumns.chosen = [];
+    };
+
+    $scope.addDim = function () {
+        $scope.chosenColumns = $scope.chosenColumns.concat($scope.selectedColumns.available);
+        $scope.refreshAvailable();
+    };
+
+    $scope.removeDim = function () {
+        $scope.availableColumns = $scope.availableColumns.concat($scope.selectedColumns.chosen);
+        $scope.chosenColumns = filterOut($scope.chosenColumns, $scope.selectedColumns.chosen);
+        $scope.refreshAvailable();
+    };
+    /** END: js about data model dimensions **/
+
+
+    /** START: js about dimension advanced options **/
+    // Init the dimension option.
+    var DimensionOption = {
+        init: function (dimCol) {
+            return {
+                name: '',
+                hierarchy: [],
+                derived: [],
+                status: {
+                    hierarchyCount: 1,
+                    useHierarchy: false
+                },
+                dimCol: dimCol
+            };
+        }
+    };
+
+    // Cache for all the editing dimension options.
+    $scope.advancedDimOptions = {
+        cache: {}
+    };
+
+    $scope.newDimensionOption = {};
+
+    $scope.switchDimOptions = function () {
+        // Show advanced options panel for 1st selected dimension column since the select allows multiple.
+        var dimCol = $scope.selectedColumns.chosen[0];
+        var colKey = columnKey(dimCol);
+
+        if ($scope.advancedDimOptions.cache.hasOwnProperty(colKey)) {
+            $scope.newDimensionOption = $scope.advancedDimOptions.cache[colKey];
+        } else {
+            $scope.newDimensionOption = DimensionOption.init(dimCol);
+            $scope.advancedDimOptions.cache[colKey] = $scope.newDimensionOption;
+        }
+    };
+
+    $scope.getAvailableDimOptCols = function (dimCol) {
+        var availableCols = $scope.getColumnsByTable(dimCol.table);
+
+        // Whether the dimension column comes from lookup table.
+        if (dimCol.isLookup) {
+            availableCols = availableCols.concat($scope.getColumnsByTable($scope.newModel.fact_table));
+        }
+
+        return availableCols;
+    };
+
+    // Adapter between new data model/dimensions and original dimensions.
+    $scope.dimensionsAdapter = function () {
+        var dimensions = [];
+        for (var i = 0; i < $scope.chosenColumns.length; i++) {
+            var dimOptions = $scope.advancedDimOptions.cache[columnKey($scope.chosenColumns[i])];
+            // TODO Do some validation.
+
+            var dim = {
+                name: dimOptions.name,
+                table: dimOptions.dimCol.table,
+                column: dimOptions.dimCol.name,
+                derived: dimOptions.derived,
+                hierarchy: dimOptions.hierarchy
+            };
+
+            // Lookup table column.
+            if (dimOptions.dimCol.isLookup) {
+                // Add join info from model.
+                for (var j = 0; j < $scope.newModel.lookups.length; j++) {
+                    if ($scope.newModel.lookups[j].table == dimOptions.dimCol.table) {
+                        dim.join = $scope.newModel.lookups[j].join;
+                        break;
+                    }
+                }
+            }
+
+            dimensions.push(dim);
+        }
+
+        // $scope.cubeMetaFrame.dimensions
+        console.dir(dimensions);
+    };
+    /** END: js about dimension advanced options **/
 
     var Dimension = {
         createNew: function () {
@@ -89,8 +343,9 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
     // ~ public methods
     $scope.filterProj = function(project){
         return $scope.userService.hasRole('ROLE_ADMIN') || $scope.hasPermission(project,$scope.permissions.ADMINISTRATION.mask);
-    }
+    };
 
+    /**
     $scope.addNewDimension = function (dimension) {
         $scope.newDimension = (!!dimension)? dimension: Dimension.createNew();
         if(!$scope.newDimension.join){
@@ -117,21 +372,22 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
         }
         $scope.newDimension = null;
     }
+    **/
 
     $scope.addNewMeasure = function (measure) {
         $scope.newMeasure = (!!measure)? measure:Measure.createNew();
-    }
+    };
 
     $scope.clearNewMeasure = function () {
         $scope.newMeasure = null;
-    }
+    };
 
     $scope.saveNewMeasure = function () {
         if ($scope.cubeMetaFrame.measures.indexOf($scope.newMeasure) === -1) {
             $scope.cubeMetaFrame.measures.push($scope.newMeasure);
         }
         $scope.newMeasure = null;
-    }
+    };
 
     $scope.addNewRowkeyColumn = function () {
         $scope.cubeMetaFrame.rowkey.rowkey_columns.push({
@@ -140,17 +396,17 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
             "dictionary": "true",
             "mandatory": false
         });
-    }
+    };
 
     $scope.addNewAggregationGroup = function () {
         $scope.cubeMetaFrame.rowkey.aggregation_groups.push([]);
-    }
+    };
 
     $scope.refreshAggregationGroup = function (list, index, aggregation_groups) {
         if (aggregation_groups) {
             list[index] = aggregation_groups;
         }
-    }
+    };
 
     $scope.addNewHierarchy = function (dimension) {
         if (!dimension.hierarchy) {
@@ -160,15 +416,16 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
             "level": (dimension.hierarchy.length + 1),
             "column": undefined
         });
-    }
+    };
 
     $scope.addNewDerived = function (dimension) {
         if(!dimension.derived){
             dimension.derived = [];
         }
         dimension.derived.push("");
-    }
+    };
 
+    /**
     $scope.toggleJoin = function (dimension,$event) {
         if (dimension.join&&dimension.join.type!='') {
             if(!confirm('Delete the join relation?')){
@@ -183,20 +440,22 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
             dimension.join.type = 'left';
         }
     }
+    **/
 
     $scope.toggleHierarchy = function (dimension) {
         if (dimension.status.useHierarchy) {
             dimension.hierarchy = [];
         }
-    }
+    };
 
     $scope.removeElement = function (arr, element) {
         var index = arr.indexOf(element);
         if (index > -1) {
             arr.splice(index, 1);
         }
-    }
+    };
 
+    /**
     $scope.removeDimension = function (arr, element) {
         var index = arr.indexOf(element);
         if (index > -1) {
@@ -207,7 +466,8 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
                 $scope.editFlag.dimensionEdited=!$scope.editFlag.dimensionEdited;
             }
         }
-    }
+    };
+    **/
 
     $scope.open = function ($event) {
         $event.preventDefault();
@@ -222,7 +482,7 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
             $scope.curStep.isComplete = false;
             $scope.curStep = $scope.wizardSteps[stepIndex - 1];
         }
-    }
+    };
 
     $scope.nextView = function () {
         var stepIndex = $scope.wizardSteps.indexOf($scope.curStep);
@@ -234,7 +494,7 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
                 UserService.setCurUser(data);
             });
         }
-    }
+    };
 
     $scope.getJoinToolTip = function (dimension) {
         var tooltip = "";
@@ -246,7 +506,7 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
         }
 
         return tooltip;
-    }
+    };
 
     // ~ private methods
     function initProject() {
