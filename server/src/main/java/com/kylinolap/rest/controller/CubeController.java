5d5359c40bc1bcbad62717f3f4629f3b1e35c6aa
Li Yang <liyang@apache.org>
2014-12-25 16:26:04 +0800

    better error handling

--- server/src/main/java/com/kylinolap/rest/controller/CubeController.java
+++ server/src/main/java/com/kylinolap/rest/controller/CubeController.java
@@ -24,13 +24,12 @@ import java.util.Map;
 import java.util.UUID;
 
 import org.apache.commons.lang.StringUtils;
+import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
