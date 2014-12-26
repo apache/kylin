70ef8d14e28f9b50e32a7be1dd8a333d5e3bcedc
honma <honma@ebay.com>
2014-12-26 10:43:03 +0800

    fix ii test

--- dictionary/src/main/java/com/kylinolap/dict/ColumnDictInfo.java
+++ dictionary/src/main/java/com/kylinolap/dict/ColumnDictInfo.java
@@ -6,6 +6,7 @@ import com.kylinolap.metadata.model.TblColRef;
  * Created by Hongbin Ma(Binmahone) on 12/17/14.
  */
 public interface ColumnDictInfo {
+
     public abstract int getColumnLength(TblColRef col);
 
     public abstract Dictionary<?> getDictionary(TblColRef col);
