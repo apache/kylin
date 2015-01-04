6e2eca980b2e91fb4fd577b188732589170cb2ce
cxiao <cxiao@ebay.com>
2015-01-04 11:34:44 +0800

    Cube data model and dimensions refine

--- webapp/app/js/controllers/cubeSchema.js
+++ webapp/app/js/controllers/cubeSchema.js
@@ -12,7 +12,7 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
         {title: 'Cube Info', src: 'partials/cubeDesigner/info.html', isComplete: false},
         {title: 'Data Model', src: 'partials/cubeDesigner/data_model.html', isComplete: false},
         {title: 'Dimensions', src: 'partials/cubeDesigner/data_dimensions.html', isComplete: false},
-        {title: 'Dimensions', src: 'partials/cubeDesigner/dimensions.html', isComplete: false},
+        //{title: 'Dimensions', src: 'partials/cubeDesigner/dimensions.html', isComplete: false},
         {title: 'Measures', src: 'partials/cubeDesigner/measures.html', isComplete: false},
         {title: 'Filter', src: 'partials/cubeDesigner/filter.html', isComplete: false},
         {title: 'Refresh Setting', src: 'partials/cubeDesigner/incremental.html', isComplete: false}
@@ -23,301 +23,6 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
     $scope.wizardSteps.push({title: 'Overview', src: 'partials/cubeDesigner/overview.html', isComplete: false});
 
     $scope.curStep = $scope.wizardSteps[0];
-
-
-
-    /** START: js about data model **/
-    var DataModel = {
-        init: function () {
-            return {
-                name: '',
-                fact_table: '',
-                lookups: []
-            };
-        }
-    };
-
-    $scope.newModel = DataModel.init();
-
-    var Lookup = {
-        init: function () {
-            return {
-                table: '',
-                join: {
-                    type: '',
-                    primary_key: [],
-                    foreign_key: []
-                }
-            };
-        }
-    };
-
-    // Initialize params.
-    $scope.lookupState = {
-        editing: false,
-        editingIndex: -1,
-        showPanel: false,
-        filter: ''
-    };
-
-    $scope.newLookup = Lookup.init();
-
-    var lookupList = $scope.newModel.lookups;
-
-    $scope.addLookup = function () {
-        // Push newLookup which bound user input data.
-        lookupList.push(angular.copy($scope.newLookup));
-
-        $scope.resetParams();
-    };
-
-    $scope.editLookup = function (lookup) {
-        $scope.lookupState.editingIndex = lookupList.indexOf(lookup);
-        $scope.lookupState.editing = true;
-
-        // Make a copy of model will be editing.
-        $scope.newLookup = angular.copy(lookup);
-
-        $scope.lookupState.showPanel = true;
-    };
-
-    $scope.doneEditLookup = function () {
-        // Copy edited model to destination model.
-        angular.copy($scope.newLookup, lookupList[$scope.lookupState.editingIndex]);
-
-        $scope.resetParams();
-    };
-
-    $scope.cancelLookup = function () {
-        $scope.resetParams();
-    };
-
-    $scope.removeLookup = function (lookup) {
-        lookupList.splice(lookupList.indexOf(lookup), 1);
-    };
-
-    $scope.resetParams = function () {
-        $scope.lookupState.editing = false;
-        $scope.lookupState.editingIndex = -1;
-        $scope.lookupState.showPanel = false;
-
-        $scope.newLookup = Lookup.init();
-    };
-    /** END: js about data model **/
-
-
-    /** START: js about data model dimensions **/
-    $scope.availableColumns = {};
-    $scope.chosenColumns = [];
-
-    // Selected item bound to UI.
-    $scope.selectedColumns = {
-        available: {},
-        chosen: []
-    };
-
-    // Dump available columns plus column table name, whether is from lookup table.
-    $scope.initializeColumns = function () {
-        // At first dump the columns of fact table.
-        var cols = $scope.getColumnsByTable($scope.newModel.fact_table);
-
-        // Initialize selected available.
-        var factAvailable = {};
-        var factSelectAvailable = {};
-
-        for (var i = 0; i < cols.length; i++) {
-            cols[i].table = $scope.newModel.fact_table;
-            cols[i].isLookup = false;
-
-            factAvailable[cols[i].name] = cols[i];
-
-            // TODO Default not selected and not disabled.
-            factSelectAvailable[cols[i].name] = {selected: false, disabled: false};
-        }
-
-        $scope.availableColumns[$scope.newModel.fact_table] = factAvailable;
-        $scope.selectedColumns.available[$scope.newModel.fact_table] = factSelectAvailable;
-
-        // Then dump each lookup tables.
-        var lookups = $scope.newModel.lookups;
-
-        for (var j = 0; j < lookups.length; j++) {
-            var cols2 = $scope.getColumnsByTable(lookups[j].table);
-
-            // Initialize selected available.
-            var lookupAvailable = {};
-            var lookupSelectAvailable = {};
-
-            for (var k = 0; k < cols2.length; k++) {
-                cols2[k].table = lookups[j].table;
-                cols2[k].isLookup = true;
-
-                lookupAvailable[cols2[k].name] = cols2[k];
-
-                // TODO Default not selected and not disabled.
-                lookupSelectAvailable[cols2[k].name] = {selected: false, disabled: false};
-            }
-
-            $scope.availableColumns[lookups[j].table] = lookupAvailable;
-            $scope.selectedColumns.available[lookups[j].table] = lookupSelectAvailable;
-        }
-    };
-
-    // Helper func to get identity of the column.
-    var columnKey = function (col) {
-        return col.table + '.' + col.name;
-    };
-
-    // Helper func to filter out a list from another list, depends on the column identity.
-    var filterOut = function (original, toFilter) {
-        var filtered = [];
-
-        angular.forEach(original, function (entity) {
-            var match = false;
-
-            for (var i = 0; i < toFilter.length; i++) {
-                // Equals columns.
-                if (columnKey(toFilter[i]) == columnKey(entity)) {
-                    match = true;
-                    break;
-                }
-            }
-
-            if (!match) {
-                filtered.push(entity);
-            }
-        });
-
-        return filtered;
-    };
-
-    // Reset status of available list of columns based on selected chosen columns.
-    $scope.refreshAvailable = function () {
-        var list = $scope.selectedColumns.chosen;
-
-        for (var i = 0; i < list.length; i++) {
-            var availableCol = $scope.selectedColumns.available[list[i].table][list[i].name];
-            availableCol.selected = false;
-            availableCol.disabled = false;
-        }
-
-        // Reset selected chosen column.
-        $scope.selectedColumns.chosen = [];
-    };
-
-    // Helper func to get selected available column.
-    $scope.selectedAvailable = function() {
-        var found = [];
-        var list = $scope.selectedColumns.available;
-
-        angular.forEach(list, function (value, tableName) {
-            // Key is table name.
-            angular.forEach(value, function (v, colName) {
-                if (v.selected && !v.disabled) {
-                    found.push($scope.availableColumns[tableName][colName]);
-
-                    // Disable the selected at this time.
-                    if (!v.disabled) {
-                        v.disabled = true;
-                    }
-                }
-            });
-        });
-
-        return found;
-    };
-
-    // Add dimension alternative.
-    $scope.addDim = function () {
-        $scope.chosenColumns = $scope.chosenColumns.concat($scope.selectedAvailable());
-    };
-
-    // Remove dimension alternative.
-    $scope.removeDim = function () {
-        $scope.chosenColumns = filterOut($scope.chosenColumns, $scope.selectedColumns.chosen);
-        $scope.refreshAvailable();
-    };
-    /** END: js about data model dimensions **/
-
-
-    /** START: js about dimension advanced options **/
-    // Init the dimension option, dimension name default as the column key.
-    var DimensionOption = {
-        init: function (dimCol) {
-            return {
-                name: columnKey(dimCol),
-                hierarchy: [],
-                derived: [],
-                status: {
-                    includeFK: false,
-                    hierarchyCount: 1,
-                    useHierarchy: false
-                },
-                dimCol: dimCol
-            };
-        }
-    };
-
-    // Cache for all the editing dimension options.
-    $scope.advancedDimOptions = {
-        cache: {}
-    };
-
-    $scope.newDimensionOption = {};
-
-    $scope.switchDimOptions = function () {
-        // Show advanced options panel for 1st selected dimension column since the select allows multiple.
-        var dimCol = $scope.selectedColumns.chosen[0];
-        var colKey = columnKey(dimCol);
-
-        if ($scope.advancedDimOptions.cache.hasOwnProperty(colKey)) {
-            $scope.newDimensionOption = $scope.advancedDimOptions.cache[colKey];
-        } else {
-            $scope.newDimensionOption = DimensionOption.init(dimCol);
-            $scope.advancedDimOptions.cache[colKey] = $scope.newDimensionOption;
-        }
-    };
-
-    // Adapter between new data model/dimensions and original dimensions.
-    $scope.dimensionsAdapter = function () {
-        var dimensions = [];
-        for (var i = 0; i < $scope.chosenColumns.length; i++) {
-            var dimOptions = $scope.advancedDimOptions.cache[columnKey($scope.chosenColumns[i])];
-            // TODO Do some validation.
-
-            var dim = {
-                name: dimOptions.name,
-                table: dimOptions.dimCol.table,
-                column: dimOptions.status.includeFK ? '{FK}' : dimOptions.dimCol.name
-            };
-
-            // Derived and hierarchy info.
-            if (dimOptions.derived.length) {
-                dim.derived = dimOptions.derived;
-            }
-
-            if (dimOptions.hierarchy.length) {
-                dim.hierarchy = dimOptions.hierarchy;
-            }
-
-            // Lookup table column, add 'join' info.
-            if (dimOptions.dimCol.isLookup) {
-                // Add join info from model.
-                for (var j = 0; j < $scope.newModel.lookups.length; j++) {
-                    if ($scope.newModel.lookups[j].table == dimOptions.dimCol.table) {
-                        dim.join = $scope.newModel.lookups[j].join;
-                        break;
-                    }
-                }
-            }
-
-            dimensions.push(dim);
-        }
-
-        // $scope.cubeMetaFrame.dimensions
-        console.dir(dimensions);
-    };
-    /** END: js about dimension advanced options **/
 
     var Dimension = {
         createNew: function () {
@@ -539,6 +244,7 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
         }
     };
 
+    /**
     $scope.getJoinToolTip = function (dimension) {
         var tooltip = "";
 
@@ -550,6 +256,7 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
 
         return tooltip;
     };
+     **/
 
     // ~ private methods
     function initProject() {
@@ -581,8 +288,13 @@ KylinApp.controller('CubeSchemaCtrl', function ($scope, QueryService, UserServic
             dimension.status = {};
             if (dimension.hierarchy) {
                 dimension.status.useHierarchy = true;
-                dimension.status.joinCount = (!!dimension.join.primary_key) ? dimension.join.primary_key.length : 0;
+                //dimension.status.joinCount = (!!dimension.join.primary_key) ? dimension.join.primary_key.length : 0;
                 dimension.status.hierarchyCount = (!!dimension.hierarchy) ? dimension.hierarchy.length : 0;
+            }
+
+            // Add 'includeFK' for new cube data model.
+            if (dimension.join) {
+                dimension.status.includeFK = dimension.column === '{FK}';
             }
         });
     }
