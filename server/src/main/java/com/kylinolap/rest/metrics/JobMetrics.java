65190417dc3481227278d1b9a415a41876d1846e
qianhao.zhou <qianzhou@ebay.com>
2015-01-08 15:56:22 +0800

    refactor BuildJobWithEngine

--- server/src/main/java/com/kylinolap/rest/metrics/JobMetrics.java
+++ server/src/main/java/com/kylinolap/rest/metrics/JobMetrics.java
@@ -16,21 +16,17 @@
 
 package com.kylinolap.rest.metrics;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import com.codahale.metrics.Gauge;
 import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricSet;
-import com.kylinolap.job.JobManager;
+
+import java.util.Collections;
+import java.util.Map;
 
 /**
  * @author xduo
  * 
  */
 public class JobMetrics implements MetricSet {
-
-    private JobManager jobManager;
 
     static class JobMetricsHolder {
         static final JobMetrics INSTANCE = new JobMetrics();
@@ -47,70 +43,7 @@ public class JobMetrics implements MetricSet {
      */
     @Override
     public Map<String, Metric> getMetrics() {
-        Map<String, Metric> metricSet = new HashMap<String, Metric>();
-
-        metricSet.put("PercentileJobStepDuration",
-                new Gauge<Double>() {
-            @Override
-            public Double getValue() {
-                return jobManager.getPercentileJobStepDuration(95);
-            }
-        });
-
-        metricSet.put("scheduledJobs", new Gauge<Integer>() {
-            @Override
-            public Integer getValue() {
-                return jobManager.getScheduledJobsSzie();
-            }
-        });
-
-        metricSet.put("EngineThreadPool", new Gauge<Integer>() {
-            @Override
-            public Integer getValue() {
-                return jobManager.getEngineThreadPoolSize();
-            }
-        });
-
-        metricSet.put("MaxJobStep", new Gauge<Double>() {
-            @Override
-            public Double getValue() {
-                return jobManager.getMaxJobStepDuration();
-            }
-        });
-
-        metricSet.put("MinJobStep", new Gauge<Double>() {
-            @Override
-            public Double getValue() {
-                return jobManager.getMinJobStepDuration();
-            }
-        });
-
-        metricSet.put("IdleSlots", new Gauge<Integer>() {
-            @Override
-            public Integer getValue() {
-                return jobManager.getNumberOfIdleSlots();
-            }
-        });
-
-        metricSet.put("JobStepsExecuted", new Gauge<Integer>() {
-            @Override
-            public Integer getValue() {
-                return jobManager.getNumberOfJobStepsExecuted();
-            }
-        });
-
-        metricSet.put("JobStepsRunning", new Gauge<Integer>() {
-            @Override
-            public Integer getValue() {
-                return jobManager.getNumberOfJobStepsRunning();
-            }
-        });
-
-        return metricSet;
-    }
-
-    public void setJobManager(JobManager jobManager) {
-        this.jobManager = jobManager;
+        return Collections.emptyMap();
     }
 
 }
