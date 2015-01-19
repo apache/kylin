1e1a6394064a0e7ac1c06d1c5c2a403175d51cd8
shaofengshi <shaofengshi@apache.org>
2015-01-19 15:14:32 +0800

    Improve and fix bug in the II build flow.

--- job/src/main/java/com/kylinolap/job/hadoop/cube/FactDistinctColumnsMapper.java
+++ job/src/main/java/com/kylinolap/job/hadoop/cube/FactDistinctColumnsMapper.java
@@ -105,7 +105,7 @@ public class FactDistinctColumnsMapper<KEYIN> extends Mapper<KEYIN, HCatRecord, 
                 fieldSchema = schema.get(flatTableIndexes[i]);
                 Object fieldValue = record.get(fieldSchema.getName(), schema);
                 if (fieldValue == null)
-                    fieldValue = "NULL";
+                    continue;
                 byte[] bytes = Bytes.toBytes(fieldValue.toString());
                 outputValue.set(bytes, 0, bytes.length);
                 context.write(outputKey, outputValue);
