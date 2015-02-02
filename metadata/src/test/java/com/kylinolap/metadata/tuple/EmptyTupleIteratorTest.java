35135325d3ad2cbb2e39b0e8cce46ee09530d850
Li, Yang <yangli9@ebay.com>
2015-02-02 18:35:26 +0800

    more test cases

---
+++ metadata/src/test/java/com/kylinolap/metadata/tuple/EmptyTupleIteratorTest.java
@@ -0,0 +1,17 @@
+package com.kylinolap.metadata.tuple;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmptyTupleIteratorTest {
+
+    @Test
+    public void testListAllTables() throws Exception {
+        ITupleIterator it = ITupleIterator.EMPTY_TUPLE_ITERATOR;
+        assertFalse(it.hasNext());
+        assertNull(it.next());
+        it.close(); // for coverage
+    }
+
+}
