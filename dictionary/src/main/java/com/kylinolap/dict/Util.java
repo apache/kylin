cbf235cc6e2fd19950452efc196269c4e67bc144
Li, Yang <yangli9@ebay.com>
2015-02-03 10:52:01 +0800

    deal with serialized class names in dict file after apache package renaming

---
+++ dictionary/src/main/java/com/kylinolap/dict/Util.java
@@ -0,0 +1,26 @@
+package com.kylinolap.dict;
+
+class Util {
+    
+    /*
+     * There are class names serialized into dict files. After apache package renaming, the old class names
+     * requires adjustment in order to find the right class under new package.
+     */
+    static Class<?> classForName(String className) throws ClassNotFoundException {
+        try {
+            return Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            
+            // if package names were changed?
+            int cut = className.lastIndexOf('.');
+            String pkg = className.substring(0, cut);
+            String newPkg = Util.class.getPackage().getName();
+            if (pkg.equals(newPkg))
+                throw e;
+            
+            // find again under new package
+            String newClassName = newPkg + className.substring(cut);
+            return Class.forName(newClassName);
+        }
+    }
+}
