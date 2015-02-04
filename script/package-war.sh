69717e02aa539eb17c3f6875dba9f1d00a04d3d6
qianhao.zhou <qianzhou@ebay.com>
2015-02-04 17:06:03 +0800

    refactor script

--- script/package-war.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "package back-end"
-
-dir=$(dirname ${0})
-cd ${dir}/..
-
-mvn clean install -DskipTests
---
+++ script/release.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Checking maven..."
+
+if [ -z "$(command -v mvn)" ]
+then
+    echo "Please install maven first so that Kylin-Deploy can proceed"
+    exit 1
+else
+    echo "maven check passed"
+fi
+
+echo "Checking npm..."
+
+if [ -z "$(command -v npm)" ]
+then
+    echo "Please install npm first so that Kylin-Deploy can proceed"
+    exit 1
+else
+    echo "npm check passed"
+fi
+
+version=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\['`
+echo "kylin version: ${version}"
+export version
+
+dir=$(dirname ${0})
+echo ${pwd}
+echo ${dir}
+
+sh ${dir}/package.sh
+sh ${dir}/prepare.sh
+sh ${dir}/compress.sh
+
