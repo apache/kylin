865452ae0c7fd4e78cecaae0ab1c0c34e8b3d925
jiazhong <jiazhong@ebay.com>
2015-02-04 23:20:34 +0800

    fix add cube bug

--- webapp/app/js/model/tableModel.js
+++ webapp/app/js/model/tableModel.js
@@ -5,6 +5,11 @@ KylinApp.service('TableModel', function() {
 
     this.selectProjectTables = [];
 
+
+    this.initTables = function(){
+        this.selectProjectTables = [];
+    }
+
     this.addTable = function(table){
         this.selectProjectTables.push(table);
     }
