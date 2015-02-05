1ba78ce0bc7c4980424d077959aef2ecb288c908
honma <honma@ebay.com>
2015-02-05 15:12:28 +0800

    minor change

--- job/src/test/java/org/apache/kylin/job/BuildIIWithEngineTest.java
+++ job/src/test/java/org/apache/kylin/job/BuildIIWithEngineTest.java
@@ -247,7 +247,6 @@ public class BuildIIWithEngineTest {
         ExportHBaseData export = new ExportHBaseData();
         export.exportTables();
     }
-}
 
     public static void main(String[] args) throws Exception {
         BuildIIWithEngineTest instance = new BuildIIWithEngineTest();
