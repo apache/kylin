9bcdfab34cbb6c695ce1daa57a8c916742fb96ce
qianhao.zhou <qianzhou@ebay.com>
2015-02-05 15:42:14 +0800

    fix script

--- sbin/start-kylin.sh
+++ sbin/start-kylin.sh
@@ -2,6 +2,9 @@
 
 dir=$(dirname ${0})
 
+tomcat_root=${dir}/../tomcat
+export tomcat_root
+
 sh ${dir}/check-env.sh
 
 #if [ ! -z "$KYLIN_LD_LIBRARY_PATH" ]
@@ -22,17 +25,21 @@ sh ${dir}/check-env.sh
 
 #-Djava.library.path=${KYLIN_LD_LIBRARY_PATH} \
 
-export HBASE_CLASSPATH_PREFIX=${CATALINA_HOME}/bin/bootstrap.jar:${CATALINA_HOME}/bin/tomcat-juli.jar:${CATALINA_HOME}/lib/*:
-hbase -Djava.util.logging.config.file=${CATALINA_HOME}/conf/logging.properties \
+
+rm -rf ${tomcat_root}/webapps/kylin*
+cp ${dir}/../lib/kylin-server-*.war ${tomcat_root}/webapps/kylin.war
+
+export HBASE_CLASSPATH_PREFIX=${tomcat_root}/bin/bootstrap.jar:${tomcat_root}/bin/tomcat-juli.jar:${tomcat_root}/lib/*:
+hbase -Djava.util.logging.config.file=${tomcat_root}/conf/logging.properties \
 -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager \
 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true \
 -Dorg.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true \
 -Dspring.profiles.active=sandbox \
--Djava.endorsed.dirs=${CATALINA_HOME}/endorsed  \
--Dcatalina.base=${CATALINA_HOME} \
--Dcatalina.home=${CATALINA_HOME} \
--Djava.io.tmpdir=${CATALINA_HOME}/temp  \
-org.apache.hadoop.util.RunJar ${CATALINA_HOME}/bin/bootstrap.jar  org.apache.catalina.startup.Bootstrap start > ${CATALINA_HOME}/logs/kylin_sandbox.log 2>&1 &
+-Djava.endorsed.dirs=${tomcat_root}/endorsed  \
+-Dcatalina.base=${tomcat_root} \
+-Dcatalina.home=${tomcat_root} \
+-Djava.io.tmpdir=${tomcat_root}/temp  \
+org.apache.hadoop.util.RunJar ${tomcat_root}/bin/bootstrap.jar  org.apache.catalina.startup.Bootstrap start > ${tomcat_root}/logs/kylin_sandbox.log 2>&1 &
 echo "A new Kylin instance is started by $USER, stop it using \"kylin.sh stop\""
 echo "Please visit http://<your_sandbox_ip>:7070/kylin to play with the cubes! (Useranme: ADMIN, Password: KYLIN)"
-echo "You can check the log at ${CATALINA_HOME}/logs/kylin_sandbox.log"
+echo "You can check the log at ${tomcat_root}/logs/kylin_sandbox.log"
--- script/download-tomcat.sh
+++ script/download-tomcat.sh
@@ -3,6 +3,8 @@
 dir=$(dirname ${0})
 cd ${dir}/..
 
+rm apache-tomcat-7.0.57.tar.gz
+rm -rf tomcat
 wget http://mirror.sdunix.com/apache/tomcat/tomcat-7/v7.0.57/bin/apache-tomcat-7.0.57.tar.gz
 
 tar -zxvf apache-tomcat-7.0.57.tar.gz
