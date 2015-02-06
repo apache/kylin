f7acce05d55ed9e467ca6edb5f95f345f9a6a2b4
qianhao.zhou <qianzhou@ebay.com>
2015-02-06 16:30:51 +0800

    fix script

--- script/release.sh
+++ script/release.sh
@@ -20,16 +20,14 @@ else
     echo "npm check passed"
 fi
 
+dir=$(dirname ${0})
+cd ${dir}/..
 version=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\['`
 echo "kylin version: ${version}"
 export version
 
-dir=$(dirname ${0})
-echo ${pwd}
-echo ${dir}
-
-sh ${dir}/package.sh || { exit 1; }
-sh ${dir}/prepare.sh || { exit 1; }
-#sh ${dir}/download-tomcat.sh || { exit 1; }
-sh ${dir}/compress.sh || { exit 1; }
+sh script/package.sh || { exit 1; }
+sh script/prepare.sh || { exit 1; }
+sh script/download-tomcat.sh || { exit 1; }
+sh scirpt/compress.sh || { exit 1; }
 
