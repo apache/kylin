1de6ed6fa761292a0a1c8ae46bb6c7ba8e266e86
qianhao.zhou <qianzhou@ebay.com>
2015-02-06 16:16:07 +0800

    fix classpath

--- sbin/start-kylin-sandbox.sh
+++ sbin/start-kylin-sandbox.sh
@@ -31,14 +31,18 @@ cp ${dir}/../lib/kylin-server-*.war ${tomcat_root}/webapps/kylin.war
 
 source ${dir}/find-hive-dependency.sh
 
-export HBASE_CLASSPATH_PREFIX=$HBASE_CLASSPATH_PREFIX:${tomcat_root}/bin/bootstrap.jar:${tomcat_root}/bin/tomcat-juli.jar:${tomcat_root}/lib/*:$hive_dependency
+export HBASE_CLASSPATH_PREFIX=${tomcat_root}/bin/bootstrap.jar:${tomcat_root}/bin/tomcat-juli.jar:${tomcat_root}/lib/*:$HBASE_CLASSPATH_PREFIX
+export HBASE_CLASSPATH=$hive_dependency
+
 hbase -Djava.util.logging.config.file=${tomcat_root}/conf/logging.properties \
 -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager \
 -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true \
 -Dorg.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true \
+\
 -Dspring.profiles.active=sandbox \
 -Dkylin.job.yarn.app.rest.check.status.url='http://sandbox:8088/ws/v1/cluster/apps/${job_id}?anonymous=true' \
 -Dkylin.hive.dependency=${hive_dependency} \
+\
 -Djava.endorsed.dirs=${tomcat_root}/endorsed  \
 -Dcatalina.base=${tomcat_root} \
 -Dcatalina.home=${tomcat_root} \
