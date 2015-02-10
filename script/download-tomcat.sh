216514973cc7aef8cf99bc0106defac2e334ed08
qianhao.zhou <qianzhou@ebay.com>
2015-02-10 16:49:08 +0800

    refactor script

--- script/download-tomcat.sh
+++ script/download-tomcat.sh
@@ -9,6 +9,7 @@ wget http://mirror.sdunix.com/apache/tomcat/tomcat-7/v7.0.57/bin/apache-tomcat-7
 
 tar -zxvf apache-tomcat-7.0.57.tar.gz
 mv apache-tomcat-7.0.57 tomcat
+rm -rf tomcat/webapps/*
 
 mv tomcat/conf/server.xml tomcat/conf/server.xml.bak
 cp deploy/server.xml tomcat/conf/server.xml
