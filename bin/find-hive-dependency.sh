375e3a84aa525d75d96350efc748432358f41728
Li Yang <liyang@apache.org>
2015-02-13 09:03:21 +0800

    better deployment

--- bin/find-hive-dependency.sh
+++ bin/find-hive-dependency.sh
@@ -4,6 +4,7 @@ hive_env=`hive -e set | grep 'env:CLASSPATH'`
 
 hive_classpath=`echo $hive_env | grep 'env:CLASSPATH' | awk -F '=' '{print $2}'`
 arr=(`echo $hive_classpath | cut -d ":"  --output-delimiter=" " -f 1-`)
+hive_conf_path=
 hive_exec_path=
 for data in ${arr[@]}
 do
@@ -12,9 +13,14 @@ do
     then
         hive_exec_path=$data
     fi
+    result=`echo $data | grep 'hive/conf'`
+    if [ $result ]
+    then
+        hive_conf_path=$data
+    fi
 done
 hdp_home=`echo $hive_exec_path | awk -F '/hive/lib/' '{print $1}'`
 
-hive_dependency=/usr/hdp/current/hive-client/conf/:${hdp_home}/hive/lib/*:${hdp_home}/hive-hcatalog/share/hcatalog/*
+hive_dependency=$hive_conf_path:${hdp_home}/hive/lib/*:${hdp_home}/hive-hcatalog/share/hcatalog/*
 echo "hive dependency: $hive_dependency"
 export hive_dependency
