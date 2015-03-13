1b5e9269bf012e9f78ec5c991cc58dbf74af5e93
Shao Feng, Shi <shaoshi@ebay.com>
2015-03-13 10:57:11 +0800

    KYLIN-634 Script to import sample data and cube metadata

--- bin/sample.sh
+++ bin/sample.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 dir=$(dirname ${0})
-job_jar=`ls -ls ${KYLIN_HOME}/lib | grep kylin-job | awk '{print $9}'`
+source ${dir}/check-env.sh
+job_jar=`find ${KYLIN_HOME}/lib/ -name kylin-job*.jar`
 cd ${KYLIN_HOME}/sample_cube/data
 hive -f ${KYLIN_HOME}/sample_cube/create_sample_tables.sql  || { exit 1; }
-hbase org.apache.hadoop.util.RunJar ${KYLIN_HOME}/lib/${job_jar} org.apache.kylin.common.persistence.ResourceTool upload ${KYLIN_HOME}/sample_cube/metadata  || { exit 1; }
+cd ${KYLIN_HOME}
+hbase org.apache.hadoop.util.RunJar ${job_jar} org.apache.kylin.common.persistence.ResourceTool upload ${KYLIN_HOME}/sample_cube/metadata  || { exit 1; }
