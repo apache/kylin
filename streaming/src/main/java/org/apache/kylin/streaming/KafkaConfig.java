e0c60382fd6f754821be811c4a28d9e4fe8d433b
qianhao.zhou <qianzhou@ebay.com>
2015-03-25 11:34:21 +0800

    refactor

--- streaming/src/main/java/org/apache/kylin/streaming/KafkaConfig.java
+++ streaming/src/main/java/org/apache/kylin/streaming/KafkaConfig.java
@@ -34,28 +34,41 @@
 
 package org.apache.kylin.streaming;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Function;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import kafka.cluster.Broker;
+import org.apache.kylin.common.persistence.RootPersistentEntity;
+import org.apache.kylin.streaming.invertedindex.BrokerConfig;
 
+import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Properties;
 
 /**
  * Created by qianzhou on 3/2/15.
  */
-public class KafkaConfig {
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE, getterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
+public class KafkaConfig extends RootPersistentEntity {
 
-    private List<Broker> brokers;
+    @JsonProperty("brokers")
+    private List<BrokerConfig> brokerConfigs;
 
+    @JsonProperty("zookeeper")
     private String zookeeper;
 
+    @JsonProperty("topic")
     private String topic;
 
+    @JsonProperty("timeout")
     private int timeout;
 
+    @JsonProperty("maxReadCount")
     private int maxReadCount;
 
+    @JsonProperty("bufferSize")
     private int bufferSize;
 
     private int partitionId;
@@ -84,14 +97,6 @@ public class KafkaConfig {
         this.bufferSize = bufferSize;
     }
 
-    public List<Broker> getBrokers() {
-        return brokers;
-    }
-
-    public void setBrokers(List<Broker> brokers) {
-        this.brokers = brokers;
-    }
-
     public String getZookeeper() {
         return zookeeper;
     }
@@ -116,36 +121,50 @@ public class KafkaConfig {
         this.partitionId = partitionId;
     }
 
-    public static KafkaConfig load(KafkaConfig config) {
-        KafkaConfig result = new KafkaConfig();
-        result.setBufferSize(config.getBufferSize());
-        result.setMaxReadCount(config.getMaxReadCount());
-        result.setTimeout(config.getTimeout());
-        result.setTopic(config.getTopic());
-        result.setZookeeper(config.getZookeeper());
-        result.setPartitionId(config.getPartitionId());
-        result.setBrokers(config.getBrokers());
-        return result;
+    public void setBrokerConfigs(List<BrokerConfig> brokerConfigs) {
+        this.brokerConfigs = brokerConfigs;
     }
 
-    public static KafkaConfig load(Properties properties) {
-        Preconditions.checkNotNull(properties);
-        KafkaConfig result = new KafkaConfig();
-        result.setBufferSize(Integer.parseInt(properties.getProperty("consumer.bufferSize")));
-        result.setMaxReadCount(Integer.parseInt(properties.getProperty("consumer.maxReadCount")));
-        result.setTimeout(Integer.parseInt(properties.getProperty("consumer.timeout")));
-        result.setTopic(properties.getProperty("topic"));
-        result.setZookeeper(properties.getProperty("zookeeper"));
-        result.setPartitionId(Integer.parseInt(properties.getProperty("partitionId")));
-
-        int id = 0;
-        List<Broker> brokers = Lists.newArrayList();
-        for (String str: properties.getProperty("brokers").split(",")) {
-            final String[] split = str.split(":");
-            final Broker broker = new Broker(id++, split[0], Integer.parseInt(split[1]));
-            brokers.add(broker);
-        }
-        result.setBrokers(brokers);
-        return result;
+    public List<Broker> getBrokers() {
+        return Lists.transform(brokerConfigs, new Function<BrokerConfig, Broker>() {
+            @Nullable
+            @Override
+            public Broker apply(BrokerConfig input) {
+                return new Broker(input.getId(), input.getHost(), input.getPort());
+            }
+        });
     }
+
+    //    public static KafkaConfig load(KafkaConfig config) {
+//        KafkaConfig result = new KafkaConfig();
+//        result.setBufferSize(config.getBufferSize());
+//        result.setMaxReadCount(config.getMaxReadCount());
+//        result.setTimeout(config.getTimeout());
+//        result.setTopic(config.getTopic());
+//        result.setZookeeper(config.getZookeeper());
+//        result.setPartitionId(config.getPartitionId());
+//        result.setBrokers(config.getBrokers());
+//        return result;
+//    }
+//
+//    public static KafkaConfig load(Properties properties) {
+//        Preconditions.checkNotNull(properties);
+//        KafkaConfig result = new KafkaConfig();
+//        result.setBufferSize(Integer.parseInt(properties.getProperty("consumer.bufferSize")));
+//        result.setMaxReadCount(Integer.parseInt(properties.getProperty("consumer.maxReadCount")));
+//        result.setTimeout(Integer.parseInt(properties.getProperty("consumer.timeout")));
+//        result.setTopic(properties.getProperty("topic"));
+//        result.setZookeeper(properties.getProperty("zookeeper"));
+//        result.setPartitionId(Integer.parseInt(properties.getProperty("partitionId")));
+//
+//        int id = 0;
+//        List<Broker> brokers = Lists.newArrayList();
+//        for (String str: properties.getProperty("brokers").split(",")) {
+//            final String[] split = str.split(":");
+//            final Broker broker = new Broker(id++, split[0], Integer.parseInt(split[1]));
+//            brokers.add(broker);
+//        }
+//        result.setBrokers(brokers);
+//        return result;
+//    }
 }
