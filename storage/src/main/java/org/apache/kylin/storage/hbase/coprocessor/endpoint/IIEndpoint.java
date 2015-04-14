bcdc3b0a50ed01903ee406bfc3c7517ca97cd996
qianhao.zhou <qianzhou@ebay.com>
2015-04-14 10:42:04 +0800

    KYLIN-678 fix compile error

--- storage/src/main/java/org/apache/kylin/storage/hbase/coprocessor/endpoint/IIEndpoint.java
+++ storage/src/main/java/org/apache/kylin/storage/hbase/coprocessor/endpoint/IIEndpoint.java
@@ -241,13 +241,13 @@ public class IIEndpoint extends IIProtos.RowsService implements Coprocessor, Cop
         for (int i = 0; i < columnSize; i++) {
             final TblColRef column = columns[i];
             if (isMetric[i]) {
-                rowKeyColumnIO.writeColumnWith(encodedRecord.getBytes(), encodedRecord.offset(i), encodedRecord.length(i), recordBuffer, digest.offset(i), rowKeyColumnIO.getColumnLength(column));
+                rowKeyColumnIO.writeColumnWithoutDictionary(encodedRecord.getBytes(), encodedRecord.offset(i), encodedRecord.length(i), recordBuffer, digest.offset(i), rowKeyColumnIO.getColumnLength(column));
             } else {
                 if (emptyDictionary) {
-                    rowKeyColumnIO.writeColumnWith(encodedRecord.getBytes(), encodedRecord.offset(i), encodedRecord.length(i), recordBuffer, digest.offset(i), rowKeyColumnIO.getColumnLength(column));
+                    rowKeyColumnIO.writeColumnWithoutDictionary(encodedRecord.getBytes(), encodedRecord.offset(i), encodedRecord.length(i), recordBuffer, digest.offset(i), rowKeyColumnIO.getColumnLength(column));
                 } else {
                     final int length = localDictionaries[i].getValueBytesFromId(encodedRecord.getValueID(i), buffer, 0);
-                    rowKeyColumnIO.writeColumnWith(buffer, 0, length, recordBuffer, digest.offset(i), rowKeyColumnIO.getColumnLength(column));
+                    rowKeyColumnIO.writeColumnWithoutDictionary(buffer, 0, length, recordBuffer, digest.offset(i), rowKeyColumnIO.getColumnLength(column));
                 }
             }
         }
