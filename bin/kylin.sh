0b722c2b25723173a75c068f12f5265e1c331a98
honma <honma@ebay.com>
2015-05-07 16:06:26 +0800

    KYLIN-756 enforce all customized jvm settings located in setenv.sh

--- bin/kylin.sh
+++ bin/kylin.sh
@@ -37,9 +37,9 @@ then
 
     export HBASE_CLASSPATH_PREFIX=${tomcat_root}/bin/bootstrap.jar:${tomcat_root}/bin/tomcat-juli.jar:${tomcat_root}/lib/*:$HBASE_CLASSPATH_PREFIX
     export HBASE_CLASSPATH=$hive_dependency:${HBASE_CLASSPATH}
-    export JAVA_OPTS="-Xms2048M -Xmx2048M -XX:MaxPermSize=512m"
 
-    hbase ${JAVA_OPTS} ${KYLIN_EXTRA_START_OPTS} \
+    # KYLIN_EXTRA_START_OPTS is for customized settings, checkout bin/setenv.sh
+    hbase ${KYLIN_EXTRA_START_OPTS} \
     -Djava.util.logging.config.file=${tomcat_root}/conf/logging.properties \
     -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager \
     -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true \
@@ -101,9 +101,9 @@ then
 
         mkdir -p ${KYLIN_HOME}/ext
         export HBASE_CLASSPATH=$hive_dependency:${KYLIN_HOME}/lib/*:${KYLIN_HOME}/ext/*:${HBASE_CLASSPATH}
-        export JAVA_OPTS="-Xms4g -Xmx4g -XX:MaxPermSize=512m"
 
-        hbase ${JAVA_OPTS} ${KYLIN_EXTRA_START_OPTS} \
+        # KYLIN_EXTRA_START_OPTS is for customized settings, checkout bin/setenv.sh
+        hbase ${KYLIN_EXTRA_START_OPTS} \
         -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager \
         -Dorg.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true \
         -Dkylin.hive.dependency=${hive_dependency} \
