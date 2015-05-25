e1fc34f52eb249d2914e6b0db978669becabcc64
honma <honma@ebay.com>
2015-05-25 10:32:03 +0800

    KYLIN-697 fix mock htable bug

--- common/src/main/java/org/apache/kylin/common/util/MockHTable.java
+++ common/src/main/java/org/apache/kylin/common/util/MockHTable.java
@@ -680,7 +680,6 @@ public class MockHTable implements HTableInterface {
     public <R extends Message> void batchCoprocessorService(Descriptors.MethodDescriptor methodDescriptor, Message request, byte[] startKey, byte[] endKey, R responsePrototype, Batch.Callback<R> callback) throws ServiceException, Throwable {
         throw new NotImplementedException();
 
-
     }
 
     @Override
