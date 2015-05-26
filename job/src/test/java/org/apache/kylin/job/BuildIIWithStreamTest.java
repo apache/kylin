c97dc15cdf66750bcf9c460a4caad6f676930df0
honma <honma@ebay.com>
2015-05-26 11:20:03 +0800

    KYLIN-783 change hdp.version to 2.2.4.2-2

--- job/src/test/java/org/apache/kylin/job/BuildIIWithStreamTest.java
+++ job/src/test/java/org/apache/kylin/job/BuildIIWithStreamTest.java
@@ -92,7 +92,7 @@ public class BuildIIWithStreamTest {
     public static void beforeClass() throws Exception {
         logger.info("Adding to classpath: " + new File(HBaseMetadataTestCase.SANDBOX_TEST_DATA).getAbsolutePath());
         ClassUtil.addClasspath(new File(HBaseMetadataTestCase.SANDBOX_TEST_DATA).getAbsolutePath());
-        System.setProperty("hdp.version", "2.2.0.0-2041"); // mapred-site.xml ref this
+        System.setProperty("hdp.version", "2.2.4.2-2"); // mapred-site.xml ref this
     }
 
     @Before
