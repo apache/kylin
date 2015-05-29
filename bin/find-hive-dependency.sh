cd65dff0f602479e92115d45efd2d27b462f006a
c-horn <chorn4033@gmail.com>
2015-05-29 14:30:53 -0400

    fixed hadoop/hive lib directory detection to work with MapR paths (such as /opt/mapr/hive/hive-0.13/)

--- bin/find-hive-dependency.sh
+++ bin/find-hive-dependency.sh
@@ -25,21 +25,21 @@ hive_conf_path=
 hive_exec_path=
 for data in ${arr[@]}
 do
-    result=`echo $data | grep -e 'hive-exec[0-9\.-]*jar'`
+    result=`echo $data | grep -e 'hive-exec-.*jar'`
     if [ $result ]
     then
         hive_exec_path=$data
     fi
-    result=`echo $data | grep 'hive/conf'`
+    result=`echo $data | grep -e 'hive[^/]*/conf'`
     if [ $result ]
     then
         hive_conf_path=$data
     fi
 done
-hdp_home=`echo $hive_exec_path | awk -F '/hive/lib/' '{print $1}'`
+hdp_home=`echo $hive_exec_path | awk -F '/hive.*/lib/' '{print $1}'`
 
 hcatalog=`find $hdp_home -name "hive-hcatalog-core[0-9\.-]*jar" 2>&1 | grep -m 1 -v 'Permission denied'`
-hive_lib=`find "$hdp_home/hive/lib" -name '*.jar' ! -name '*calcite*' -printf '%p:' | sed 's/:$//'`
+hive_lib=`find "$(dirname $hive_exec_path)" -name '*.jar' ! -name '*calcite*' -printf '%p:' | sed 's/:$//'`
 
 if [ -z "$hcatalog" ]
 then
