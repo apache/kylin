57da8a50eecb2c73dbeff35111a764c2a8d848f9
c-horn <chorn4033@gmail.com>
2015-05-29 14:48:24 -0400

    added accepts json request header so RM responds with JSON rather than XML
    Signed-off-by: Li, Yang <yangli9@ebay.com>

--- job/src/main/java/org/apache/kylin/job/tools/HadoopStatusGetter.java
+++ job/src/main/java/org/apache/kylin/job/tools/HadoopStatusGetter.java
@@ -71,6 +71,8 @@ public class HadoopStatusGetter {
             }
 
             HttpMethod get = new GetMethod(url);
+            get.addRequestHeader("accept", "application/json");
+
             try {
                 client.executeMethod(get);
 
