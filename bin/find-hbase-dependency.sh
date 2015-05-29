d2d88f79763f9ffd72e505fe114eb88626c991a3
shaofengshi <shaofengshi@apache.org>
2015-05-29 11:32:47 +0800

    KYLIN-753 add shell to find hbase-common.jar in kylin.sh and then add the dependency in MR job

---
+++ bin/find-hbase-dependency.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+hbase_classpath=`hbase classpath`
+arr=(`echo $hbase_classpath | cut -d ":"  --output-delimiter=" " -f 1-`)
+hbase_common_path=
+for data in ${arr[@]}
+do
+    result=`echo $data | grep -e 'hbase-common[0-9\.-]*jar'`
+    if [ $result ]
+    then
+        hbase_common_path=$data
+    fi
+done
+
+if [ -z "$hbase_common_path" ]
+then
+    echo "hbase-common lib not found"
+    exit 1
+fi
+
+hbase_dependency=${hbase_common_path}
+echo "hbase dependency: $hbase_dependency"
+export hbase_dependency
