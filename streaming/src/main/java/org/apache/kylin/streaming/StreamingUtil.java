d2adead7a94263d0bb60f5f37bf6ff43ab91c203
qianhao.zhou <qianzhou@ebay.com>
2015-06-09 12:38:50 +0800

    add log for KYLIN-820

--- streaming/src/main/java/org/apache/kylin/streaming/StreamingUtil.java
+++ streaming/src/main/java/org/apache/kylin/streaming/StreamingUtil.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.ByteBuffer;
+import java.util.Iterator;
 
 /**
  */
@@ -17,7 +18,8 @@ public final class StreamingUtil {
 
     private static final Logger logger = LoggerFactory.getLogger(StreamingUtil.class);
 
-    private StreamingUtil(){}
+    private StreamingUtil() {
+    }
 
     public static Broker getLeadBroker(KafkaClusterConfig kafkaClusterConfig, int partitionId) {
         final PartitionMetadata partitionMetadata = KafkaRequester.getPartitionMetadata(kafkaClusterConfig.getTopic(), partitionId, kafkaClusterConfig.getBrokers(), kafkaClusterConfig);
@@ -28,16 +30,38 @@ public final class StreamingUtil {
         }
     }
 
+    private static MessageAndOffset getKafkaMessage(KafkaClusterConfig kafkaClusterConfig, int partitionId, long offset) {
+        final String topic = kafkaClusterConfig.getTopic();
+        int retry = 3;
+        while (retry > 0) {
+            final Broker leadBroker = Preconditions.checkNotNull(getLeadBroker(kafkaClusterConfig, partitionId), "unable to find leadBroker with config:" + kafkaClusterConfig + " partitionId:" + partitionId);
+            final FetchResponse response = KafkaRequester.fetchResponse(topic, partitionId, offset, leadBroker, kafkaClusterConfig);
+            Preconditions.checkArgument(response.errorCode(topic, partitionId) == 0, "errorCode of FetchResponse is:" + response.errorCode(topic, partitionId));
+            final Iterator<MessageAndOffset> iterator = response.messageSet(topic, partitionId).iterator();
+            if (iterator.hasNext()) {
+                return iterator.next();
+            } else {
+                retry--;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                continue;
+            }
+        }
+        throw new IllegalStateException(String.format("try to get timestamp of topic: %s, partitionId: %d, offset: %d, failed to get StreamMessage from kafka", topic, partitionId, offset));
+    }
+
     public static long getDataTimestamp(KafkaClusterConfig kafkaClusterConfig, int partitionId, long offset, StreamParser streamParser) {
         final String topic = kafkaClusterConfig.getTopic();
-        final Broker leadBroker = Preconditions.checkNotNull(getLeadBroker(kafkaClusterConfig, partitionId), "unable to find leadBroker with config:" + kafkaClusterConfig + " partitionId:" + partitionId);
-        final FetchResponse response = KafkaRequester.fetchResponse(topic, partitionId, offset, leadBroker, kafkaClusterConfig);
-        Preconditions.checkArgument(response.errorCode(topic, partitionId) == 0, "errorCode of FetchResponse is:" + response.errorCode(topic, partitionId));
-        final MessageAndOffset messageAndOffset = response.messageSet(topic, partitionId).iterator().next();
+        logger.debug(String.format("try to get timestamp of topic: %s, partitionId: %d, offset: %d", topic, partitionId, offset));
+        final MessageAndOffset messageAndOffset = getKafkaMessage(kafkaClusterConfig, partitionId, offset);
         final ByteBuffer payload = messageAndOffset.message().payload();
         byte[] bytes = new byte[payload.limit()];
         payload.get(bytes);
         final ParsedStreamMessage parsedStreamMessage = streamParser.parse(new StreamMessage(messageAndOffset.offset(), bytes));
+        logger.debug(String.format("get timestamp:%d", parsedStreamMessage.getTimestamp()));
         return parsedStreamMessage.getTimestamp();
     }
 
@@ -47,7 +71,9 @@ public final class StreamingUtil {
         final long earliestOffset = KafkaRequester.getLastOffset(topic, partitionId, OffsetRequest.EarliestTime(), leadBroker, kafkaClusterConfig);
         final long latestOffset = KafkaRequester.getLastOffset(topic, partitionId, OffsetRequest.LatestTime(), leadBroker, kafkaClusterConfig);
         logger.info(String.format("topic: %s, partitionId: %d, try to find closest offset with timestamp: %d between offset {%d, %d}", topic, partitionId, timestamp, earliestOffset, latestOffset));
-        return binarySearch(kafkaClusterConfig, partitionId, earliestOffset, latestOffset, timestamp, streamParser);
+        final long result = binarySearch(kafkaClusterConfig, partitionId, earliestOffset, latestOffset, timestamp, streamParser);
+        logger.info("found offset:" + result);
+        return result;
     }
 
     private static long binarySearch(KafkaClusterConfig kafkaClusterConfig, int partitionId, long startOffset, long endOffset, long targetTimestamp, StreamParser streamParser) {
