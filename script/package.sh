04fc9e8c177dd511d6087ca4973394c4fa619a3e
honma <honma@ebay.com>
2015-06-19 10:13:50 +0800

    KYLIN-842 bundle commit id into binary package for online support

--- script/package.sh
+++ script/package.sh
@@ -20,17 +20,26 @@ echo "Checking maven..."
 
 if [ -z "$(command -v mvn)" ]
 then
-    echo "Please install maven first so that Kylin-Deploy can proceed"
+    echo "Please install maven first so that Kylin packaging can proceed"
     exit 1
 else
     echo "maven check passed"
 fi
 
+if [ -z "$(command -v git)" ]
+then
+    echo "Please install maven first so that Kylin packaging can proceed"
+    exit 1
+else
+    echo "git check passed"
+fi
+
+
 echo "Checking npm..."
 
 if [ -z "$(command -v npm)" ]
 then
-    echo "Please install npm first so that Kylin-Deploy can proceed"
+    echo "Please install npm first so that Kylin packaging can proceed"
     exit 1
 else
     echo "npm check passed"
@@ -41,6 +50,9 @@ cd ${dir}/..
 version=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\['`
 echo "kylin version: ${version}"
 export version
+
+#commit id
+git rev-parse HEAD > commit_SHA1
 
 sh script/download-tomcat.sh || { exit 1; }
 sh script/build.sh || { exit 1; }
