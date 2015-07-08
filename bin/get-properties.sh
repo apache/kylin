c0c37d35b30668626886be6f607334613f35c5b6
qianhao.zhou <qianzhou@ebay.com>
2015-07-08 15:37:46 +0800

    KYLIN-882

---
+++ bin/get-properties.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ $# != 1 ]
+then
+    echo 'invalid input'
+    exit -1
+fi
+
+result=
+for i in `cat ${KYLIN_HOME}/conf/kylin.properties | grep -w "$1" | grep -v '^#' |awk -F '=' '{print $2}' | cut -c 1-`
+do
+   :
+   result=$i
+done
+echo $result
