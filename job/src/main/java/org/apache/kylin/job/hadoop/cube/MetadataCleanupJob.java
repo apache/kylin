17daf82427be0d2f03d24e3b7670d2641e72a709
shaofengshi <shaofengshi@apache.org>
2015-07-22 16:34:34 +0800

    KYLIN-901 fix check style error

--- job/src/main/java/org/apache/kylin/job/hadoop/cube/MetadataCleanupJob.java
+++ job/src/main/java/org/apache/kylin/job/hadoop/cube/MetadataCleanupJob.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * 
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,8 +34,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- */
 public class MetadataCleanupJob extends AbstractHadoopJob {
 
     @SuppressWarnings("static-access")
@@ -47,7 +45,7 @@ public class MetadataCleanupJob extends AbstractHadoopJob {
 
     private KylinConfig config = null;
 
-    public static final long TIME_THREADSHOLD = 2 * 26 * 3600 * 1000l; // 2 days
+    public static final long TIME_THREADSHOLD = 2 * 24 * 3600 * 1000l; // 2 days
 
     /*
      * (non-Javadoc)
