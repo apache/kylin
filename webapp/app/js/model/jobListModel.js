32aa3396ce7d5c242576263a9cd9260863866a4a
Zhong Jian <zhongjian@apache.org>
2015-08-06 15:34:38 +0800

    KYLIN-936, fix can't see each step's log at jobs page

--- webapp/app/js/model/jobListModel.js
+++ webapp/app/js/model/jobListModel.js
@@ -18,7 +18,7 @@
 
 KylinApp.service('JobList', function (JobService, $q) {
   var _this = this;
-  this.jobs = [];
+  this.jobs = {};
 
   this.list = function (jobRequest) {
 
@@ -35,7 +35,6 @@ KylinApp.service('JobList', function (JobService, $q) {
           _this.jobs[id] = job;
         }
       });
-      _this.jobs = _this.jobs.concat(jobs);
       defer.resolve(jobs.length);
     },function(){
       defer.reject("failed to load jobs");
@@ -46,7 +45,7 @@ KylinApp.service('JobList', function (JobService, $q) {
   };
 
   this.removeAll = function () {
-    _this.jobs = [];
+    _this.jobs = {};
   };
 
 });
