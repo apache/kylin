e1e02cc662bacd679aa71ed16cc7338eeef1c973
honma <honma@ebay.com>
2015-08-06 15:30:21 +0800

    KYLIN-934 bug fix

--- cube/src/test/java/org/apache/kylin/cube/kv/RowValueDecoderTest.java
+++ cube/src/test/java/org/apache/kylin/cube/kv/RowValueDecoderTest.java
@@ -65,8 +65,9 @@ public class RowValueDecoderTest extends LocalFileMetadataTestCase {
         BigDecimal min = new BigDecimal("333.1111111");
         BigDecimal max = new BigDecimal("333.1999999");
         LongWritable count = new LongWritable(2);
+        LongWritable item_count = new LongWritable(99999);
         ByteBuffer buf = ByteBuffer.allocate(RowConstants.ROWVALUE_BUFFER_SIZE);
-        codec.encode(new Object[] { sum, min, max, count }, buf);
+        codec.encode(new Object[] { sum, min, max, count,item_count }, buf);
 
         buf.flip();
         byte[] valueBytes = new byte[buf.limit()];
@@ -83,8 +84,8 @@ public class RowValueDecoderTest extends LocalFileMetadataTestCase {
         List<String> measureNames = rowValueDecoder.getNames();
         Object[] measureValues = rowValueDecoder.getValues();
 
-        assertEquals("[PRICE, MIN_PRICE_, MAX_PRICE_, COUNT__]", measureNames.toString());
-        assertEquals("[333.1234567, 333.1111111, 333.1999999, 2]", Arrays.toString(measureValues));
+        assertEquals("[PRICE, MIN_PRICE_, MAX_PRICE_, COUNT__, ITEM_COUNT]", measureNames.toString());
+        assertEquals("[333.1234567, 333.1111111, 333.1999999, 2, 99999]", Arrays.toString(measureValues));
     }
 
 }
