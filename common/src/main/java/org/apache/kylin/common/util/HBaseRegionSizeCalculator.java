528d3a7bcc7e607e6fe8a7481259d8c8047f1fea
Li Yang <liyang@apache.org>
2015-08-12 10:32:07 +0800

    format code, adjust apache header according to starndard javadoc format

--- common/src/main/java/org/apache/kylin/common/util/HBaseRegionSizeCalculator.java
+++ common/src/main/java/org/apache/kylin/common/util/HBaseRegionSizeCalculator.java
@@ -14,7 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 /** This class will come with HBase 2.0 in package org.apache.hadoop.hbase.util **/
 package org.apache.kylin.common.util;
