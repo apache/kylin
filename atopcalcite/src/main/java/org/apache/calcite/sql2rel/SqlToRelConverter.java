6de333d08b7aee280b27a2e563f2c06a98c34bab
Li, Yang <yangli9@ebay.com>
2015-08-12 10:32:07 +0800

    format code, adjust apache header according to starndard javadoc format

--- atopcalcite/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
+++ atopcalcite/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
@@ -14,7 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 package org.apache.calcite.sql2rel;
 
 import static org.apache.calcite.sql.SqlUtil.stripAs;
