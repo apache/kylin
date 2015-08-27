dbbbc1e5541ee6f2cce272ec48c518624bc5392f
gaodayue <gaodayue@meituan.com>
2015-08-27 14:00:25 +0800

    KYLIN-958 disallow changing data model in the backend
    Signed-off-by: shaofengshi <shaofengshi@apache.org>

--- server/src/test/java/org/apache/kylin/rest/controller/CubeControllerTest.java
+++ server/src/test/java/org/apache/kylin/rest/controller/CubeControllerTest.java
@@ -19,32 +19,34 @@
 package org.apache.kylin.rest.controller;
 
 import java.io.IOException;
-import java.io.StringWriter;
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.kylin.common.util.JsonUtil;
 import org.apache.kylin.cube.CubeInstance;
 import org.apache.kylin.cube.model.CubeDesc;
+import org.apache.kylin.cube.model.RowKeyColDesc;
 import org.apache.kylin.metadata.model.DataModelDesc;
 import org.apache.kylin.rest.request.CubeRequest;
 import org.apache.kylin.rest.service.CubeService;
 import org.apache.kylin.rest.service.JobService;
 import org.apache.kylin.rest.service.ServiceTestBase;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 
 /**
  * @author xduo
  */
 public class CubeControllerTest extends ServiceTestBase {
+    private static final String SRC_CUBE_NAME = "test_kylin_cube_with_slr_ready";
+    private static final String TEST_CUBE_NAME = SRC_CUBE_NAME + "_test_save";
 
     private CubeController cubeController;
     private CubeDescController cubeDescController;
     private ModelController modelController;
+    private CubeDesc srcCubeDesc;
 
     @Autowired
     CubeService cubeService;
@@ -52,29 +54,39 @@ public class CubeControllerTest extends ServiceTestBase {
     JobService jobService;
 
     @Before
-    public void setup() throws Exception {
+    public void setUp() throws Exception {
         super.setUp();
-
         cubeController = new CubeController();
         cubeController.setCubeService(cubeService);
         cubeController.setJobService(jobService);
+
         cubeDescController = new CubeDescController();
         cubeDescController.setCubeService(cubeService);
 
         modelController = new ModelController();
         modelController.setCubeService(cubeService);
+
+        srcCubeDesc = getCubeDescByName(SRC_CUBE_NAME);
+
+        saveTestCube(TEST_CUBE_NAME);
     }
 
-    @Test
-    public void testBasics() throws IOException {
-        CubeDesc[] cubes = (CubeDesc[]) cubeDescController.getCube("test_kylin_cube_with_slr_ready");
-        Assert.assertNotNull(cubes);
-        Assert.assertNotNull(cubeController.getSql("test_kylin_cube_with_slr_ready", "20130331080000_20131212080000"));
-        Assert.assertNotNull(cubeController.getCubes(null, null, 0, 5));
+    @After
+    public void tearDown() throws Exception {
+        cubeController.deleteCube(TEST_CUBE_NAME);
+        super.after();
+    }
 
-        CubeDesc cube = cubes[0];
+    private CubeDesc getCubeDescByName(String cubeDescName) {
+        CubeDesc[] cubes = cubeDescController.getCube(cubeDescName);
+        if (cubes == null || cubes.length < 1) {
+            throw new IllegalStateException("cube desc " + cubeDescName + " not existed");
+        }
+        return cubes[0];
+    }
+
+    private void saveTestCube(final String newCubeName) throws IOException {
         CubeDesc newCube = new CubeDesc();
-        String newCubeName = cube.getName() + "_test_save";
 
         try {
             cubeController.deleteCube(newCubeName);
@@ -83,45 +95,124 @@ public class CubeControllerTest extends ServiceTestBase {
         }
 
         newCube.setName(newCubeName);
-        newCube.setModelName(cube.getModelName());
-        newCube.setModel(cube.getModel());
-        newCube.setDimensions(cube.getDimensions());
-        newCube.setHBaseMapping(cube.getHBaseMapping());
-        newCube.setMeasures(cube.getMeasures());
-        newCube.setConfig(cube.getConfig());
-        newCube.setRowkey(cube.getRowkey());
+        newCube.setModelName(newCubeName);
+        newCube.setModel(srcCubeDesc.getModel());
+        newCube.setDimensions(srcCubeDesc.getDimensions());
+        newCube.setHBaseMapping(srcCubeDesc.getHBaseMapping());
+        newCube.setMeasures(srcCubeDesc.getMeasures());
+        newCube.setConfig(srcCubeDesc.getConfig());
+        newCube.setRowkey(srcCubeDesc.getRowkey());
 
-        String newModelName = newCubeName + "_model_desc";
-        newCube.getModel().setName(newModelName);//generate a random model
+        newCube.getModel().setName(newCubeName);
         newCube.getModel().setLastModified(0);
 
-        ObjectMapper cubeDescMapper = new ObjectMapper();
-        StringWriter cubeDescWriter = new StringWriter();
-        cubeDescMapper.writeValue(cubeDescWriter, newCube);
-
-        ObjectMapper modelDescMapper = new ObjectMapper();
-        StringWriter modelDescWriter = new StringWriter();
-        modelDescMapper.writeValue(modelDescWriter, newCube.getModel());
-
         CubeRequest cubeRequest = new CubeRequest();
-        cubeRequest.setCubeDescData(cubeDescWriter.toString());
-        cubeRequest.setModelDescData(modelDescWriter.toString());
-        cubeRequest = cubeController.saveCubeDesc(cubeRequest);
+        cubeRequest.setCubeDescData(JsonUtil.writeValueAsIndentString(newCube));
+        cubeRequest.setModelDescData(JsonUtil.writeValueAsIndentString(newCube.getModel()));
 
-        DataModelDesc model = modelController.getModel(newModelName);
+        CubeRequest result = cubeController.saveCubeDesc(cubeRequest);
+        Assert.assertTrue(result.getSuccessful());
+    }
+
+    @Test
+    public void testBasics() throws IOException {
+
+        Assert.assertNotNull(cubeController.getSql(SRC_CUBE_NAME, "20130331080000_20131212080000"));
+        Assert.assertNotNull(cubeController.getCubes(null, null, 0, 5));
+
+        DataModelDesc model = modelController.getModel(TEST_CUBE_NAME);
         Assert.assertNotNull(model);
 
         List<String> notifyList = Lists.newArrayList();
         notifyList.add("john@example.com");
-        cubeController.updateNotifyList(newCubeName, notifyList);
-        cubeController.updateCubeCost(newCubeName, 80);
+        cubeController.updateNotifyList(TEST_CUBE_NAME, notifyList);
+        cubeController.updateCubeCost(TEST_CUBE_NAME, 80);
 
-        List<CubeInstance> cubeInstances = cubeController.getCubes(newCubeName, "default", 1, 0);
+        List<CubeInstance> cubeInstances = cubeController.getCubes(TEST_CUBE_NAME, "default", 1, 0);
 
         CubeInstance cubeInstance = cubeInstances.get(0);
         Assert.assertTrue(cubeInstance.getDescriptor().getNotifyList().contains("john@example.com"));
         Assert.assertTrue(cubeInstance.getCost() == 80);
-        cubeController.deleteCube(newCubeName);
     }
 
+    @Test
+    public void testUpdateCubeDesc() throws IOException {
+        CubeDesc newCubeDesc = getCubeDescByName(TEST_CUBE_NAME);
+
+        // -------------------------------------------------------
+        // negative case
+        // -------------------------------------------------------
+
+        // invalid cube desc
+        CubeRequest req = new CubeRequest();
+        req.setCubeDescData("invalid");
+        assertUpdateFail(req);
+
+        // invalid data model
+        req = new CubeRequest();
+        req.setCubeDescData(JsonUtil.writeValueAsIndentString(newCubeDesc));
+        req.setModelDescData("invalid");
+        assertUpdateFail(req);
+
+        // data model's model_name not consistent with model name
+        req = new CubeRequest();
+        req.setCubeDescData("{\"name\" : \"myCube\", \"model_name\" : \"anotherModelName\"}");
+        req.setModelDescData("{\"name\" : \"myCube\"}");
+        assertUpdateFail(req);
+
+        // non-existed data model
+        req = new CubeRequest();
+        req.setCubeDescData("{\"name\" : \"noSuchCube\", \"model_name\" : \"noSuchModel\"}");
+        req.setModelDescData("{\"name\" : \"noSuchModel\"}");
+        assertUpdateFail(req);
+
+        // modified data model
+        req = new CubeRequest();
+        req.setCubeDescData(JsonUtil.writeValueAsIndentString(newCubeDesc));
+
+        DataModelDesc modifiedModel = new DataModelDesc();
+        modifiedModel.setName(TEST_CUBE_NAME);
+        modifiedModel.setFactTable("anotherFactTable");
+        req.setModelDescData(JsonUtil.writeValueAsIndentString(modifiedModel));
+
+        assertUpdateFail(req);
+
+        // -------------------------------------------------------
+        // positive case
+        // -------------------------------------------------------
+        req = new CubeRequest();
+        req.setModelDescData(JsonUtil.writeValueAsIndentString(newCubeDesc.getModel()));
+
+        // no signature change
+        newCubeDesc.setDescription("hello cube");
+        req.setCubeDescData(JsonUtil.writeValueAsIndentString(newCubeDesc));
+        CubeRequest res = cubeController.updateCubeDesc(req);
+        Assert.assertTrue(res.getSuccessful());
+
+        CubeDesc resultDesc = getCubeDescByName(TEST_CUBE_NAME);
+        Assert.assertEquals("hello cube", resultDesc.getDescription());
+        Assert.assertEquals(newCubeDesc.getSignature(), resultDesc.getSignature());
+
+        // signature change (reverse row key column order)
+        newCubeDesc = getCubeDescByName(TEST_CUBE_NAME);
+        RowKeyColDesc[] rowkeyColumns = newCubeDesc.getRowkey().getRowKeyColumns();
+        for (int i = 0, j = rowkeyColumns.length - 1; i < j; i++, j--) {
+            RowKeyColDesc tmp = rowkeyColumns[i];
+            rowkeyColumns[i] = rowkeyColumns[j];
+            rowkeyColumns[j] = tmp;
+        }
+        req.setCubeDescData(JsonUtil.writeValueAsIndentString(newCubeDesc));
+        res = cubeController.updateCubeDesc(req);
+        Assert.assertTrue(res.getSuccessful());
+
+        resultDesc = getCubeDescByName(TEST_CUBE_NAME);
+        ;
+        Assert.assertNotEquals(newCubeDesc.getSignature(), resultDesc.getSignature());
+        Assert.assertEquals(newCubeDesc.calculateSignature(), resultDesc.getSignature());
+    }
+
+    private void assertUpdateFail(CubeRequest req) throws JsonProcessingException {
+        CubeRequest res = cubeController.updateCubeDesc(req);
+        Assert.assertFalse(res.getSuccessful());
+    }
 }
