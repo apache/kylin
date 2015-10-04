24e59fc7e54e354a50c2e263d039a849323b168a
Yang Li <liyang@apache.org>
2015-10-04 20:51:36 +0800

    KYLIN-457 Fix double log lines in kylin.log

--- bin/kylin.sh
+++ bin/kylin.sh
@@ -63,7 +63,6 @@ then
     hbase ${KYLIN_EXTRA_START_OPTS} \
     -Djava.util.logging.config.file=${tomcat_root}/conf/logging.properties \
     -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager \
-    -Dlog4j.configuration=kylin-log4j.properties \
     -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true \
     -Dorg.apache.catalina.connector.CoyoteAdapter.ALLOW_BACKSLASH=true \
     -Djava.endorsed.dirs=${tomcat_root}/endorsed  \
@@ -116,7 +115,7 @@ then
 
     export HBASE_CLASSPATH=${KYLIN_HOME}/lib/*:$hive_dependency:${HBASE_CLASSPATH}
 
-    exec hbase -Dlog4j.configuration=kylin-log4j.properties "$@"
+    exec hbase "$@"
 
 else
     echo "usage: kylin.sh start or kylin.sh stop"
