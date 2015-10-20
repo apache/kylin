91a579fddad66f5b26c942317c35e2ad5ec141ca
lidongsjtu <lidong_sjtu@126.com>
2015-10-20 17:06:11 +0800

    KYLIN-1075 Gives better (incorrect) result for simple queries without group-by
    Signed-off-by: Li, Yang <yangli9@ebay.com>

--- query/src/main/java/org/apache/kylin/query/enumerator/OLAPEnumerator.java
+++ query/src/main/java/org/apache/kylin/query/enumerator/OLAPEnumerator.java
@@ -29,6 +29,12 @@ import org.apache.kylin.metadata.filter.CompareTupleFilter;
 import org.apache.kylin.metadata.filter.TimeConditionLiteralsReplacer;
 import org.apache.kylin.metadata.filter.TupleFilter;
 import org.apache.kylin.metadata.filter.TupleFilterSerializer;
+import org.apache.kylin.metadata.model.FunctionDesc;
+import org.apache.kylin.metadata.model.MeasureDesc;
+import org.apache.kylin.metadata.model.ParameterDesc;
+import org.apache.kylin.metadata.model.TblColRef;
+import org.apache.kylin.metadata.realization.IRealization;
+import org.apache.kylin.metadata.realization.SQLDigest;
 import org.apache.kylin.metadata.tuple.ITuple;
 import org.apache.kylin.metadata.tuple.ITupleIterator;
 import org.apache.kylin.query.relnode.OLAPContext;
@@ -106,13 +112,17 @@ public class OLAPEnumerator implements Enumerator<Object[]> {
         // bind dynamic variables
         bindVariable(olapContext.filter);
 
-        //modify date condition
+        // modify date condition
         olapContext.filter = modifyTimeLiterals(olapContext.filter);
         olapContext.resetSQLDigest();
 
+        // cube don't have correct result for simple query without group by, but let's try to return something makes sense
+        SQLDigest sqlDigest = olapContext.getSQLDigest();
+        hackNoGroupByAggregation(sqlDigest);
+
         // query storage engine
         IStorageQuery storageEngine = StorageFactory.createQuery(olapContext.realization);
-        ITupleIterator iterator = storageEngine.search(olapContext.storageContext, olapContext.getSQLDigest(), olapContext.returnTupleInfo);
+        ITupleIterator iterator = storageEngine.search(olapContext.storageContext, sqlDigest, olapContext.returnTupleInfo);
         if (logger.isDebugEnabled()) {
             logger.debug("return TupleIterator...");
         }
@@ -160,4 +170,56 @@ public class OLAPEnumerator implements Enumerator<Object[]> {
         int threshold = Integer.valueOf(propThreshold);
         olapContext.storageContext.setThreshold(threshold);
     }
+
+    // Hack no-group-by query for better results
+    private void hackNoGroupByAggregation(SQLDigest sqlDigest) {
+        if (!sqlDigest.groupbyColumns.isEmpty() || !sqlDigest.metricColumns.isEmpty())
+            return;
+
+        // If no group by and metric found, then it's simple query like select ... from ... where ...,
+        // But we have no raw data stored, in order to return better results, we hack to output sum of metric column
+        logger.info("No group by and aggregation found in this query, will hack some result for better look of output...");
+
+        // If it's select * from ...,
+        // We need to retrieve cube to manually add columns into sqlDigest, so that we have full-columns results as output.
+        IRealization cube = olapContext.realization;
+        boolean isSelectAll = sqlDigest.allColumns.isEmpty() || sqlDigest.allColumns.equals(sqlDigest.filterColumns);
+        for (TblColRef col : cube.getAllColumns()) {
+            if (col.getTable().equals(sqlDigest.factTable) && (cube.getAllDimensions().contains(col) || isSelectAll)) {
+                sqlDigest.allColumns.add(col);
+            }
+        }
+
+        for (TblColRef col : sqlDigest.allColumns) {
+            // For dimension columns, take them as group by columns.
+            if (cube.getAllDimensions().contains(col)) {
+                sqlDigest.groupbyColumns.add(col);
+            }
+            // For measure columns, take them as metric columns with aggregation function SUM().
+            else {
+                ParameterDesc colParameter = new ParameterDesc();
+                colParameter.setType("column");
+                colParameter.setValue(col.getName());
+                FunctionDesc sumFunc = new FunctionDesc();
+                sumFunc.setExpression("SUM");
+                sumFunc.setParameter(colParameter);
+
+                boolean measureHasSum = false;
+                for (MeasureDesc colMeasureDesc : cube.getMeasures()) {
+                    if (colMeasureDesc.getFunction().equals(sumFunc)) {
+                        measureHasSum = true;
+                        break;
+                    }
+                }
+                if (measureHasSum) {
+                    sqlDigest.aggregations.add(sumFunc);
+                } else {
+                    logger.warn("SUM is not defined for measure column " + col + ", output will be meaningless.");
+                }
+
+                sqlDigest.metricColumns.add(col);
+            }
+        }
+    }
+
 }
