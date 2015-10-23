c93d05c1386ff157540f928688518882af9eb754
lidongsjtu <lidong_sjtu@126.com>
2015-10-23 18:43:56 +0800

    minor, fix CI failure for subquery
    Signed-off-by: honma <honma@ebay.com>

--- query/src/test/resources/query/sql_subquery/query08.sql
+++ query/src/test/resources/query/sql_subquery/query08.sql
@@ -18,15 +18,25 @@
 
 SELECT t1.week_beg_dt, t1.sum_price, t2.cnt
 FROM (
-  select test_cal_dt.week_beg_dt, sum(test_kylin_fact.price) as sum_price
+  select test_cal_dt.week_beg_dt, sum(price) as sum_price
   from test_kylin_fact
-  inner join edw.test_cal_dt as test_cal_dt ON test_kylin_fact.cal_dt = test_cal_dt.cal_dt
+  inner JOIN edw.test_cal_dt as test_cal_dt
+  ON test_kylin_fact.cal_dt = test_cal_dt.cal_dt
+  inner JOIN test_category_groupings
+  ON test_kylin_fact.leaf_categ_id = test_category_groupings.leaf_categ_id AND test_kylin_fact.lstg_site_id = test_category_groupings.site_id
+  inner JOIN edw.test_sites as test_sites
+  ON test_kylin_fact.lstg_site_id = test_sites.site_id
   group by test_cal_dt.week_beg_dt
 ) t1
 inner join  (
   select test_cal_dt.week_beg_dt, count(*) as cnt
   from test_kylin_fact
-  inner join edw.test_cal_dt as test_cal_dt ON test_kylin_fact.cal_dt = test_cal_dt.cal_dt
+  inner JOIN edw.test_cal_dt as test_cal_dt
+  ON test_kylin_fact.cal_dt = test_cal_dt.cal_dt
+  inner JOIN test_category_groupings
+  ON test_kylin_fact.leaf_categ_id = test_category_groupings.leaf_categ_id AND test_kylin_fact.lstg_site_id = test_category_groupings.site_id
+  inner JOIN edw.test_sites as test_sites
+  ON test_kylin_fact.lstg_site_id = test_sites.site_id
   group by test_cal_dt.week_beg_dt
 ) t2
 on t1.week_beg_dt=t2.week_beg_dt
