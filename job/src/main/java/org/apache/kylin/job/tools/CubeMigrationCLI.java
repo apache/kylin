10ff2894558dbc3eb2b9f428ef6f74fb14d1c0f4
shaofengshi <shaofengshi@apache.org>
2015-11-24 13:39:13 +0800

    KYLIN-1157 copy ACL when migrate cube - small fix

--- job/src/main/java/org/apache/kylin/job/tools/CubeMigrationCLI.java
+++ job/src/main/java/org/apache/kylin/job/tools/CubeMigrationCLI.java
@@ -76,7 +76,7 @@ public class CubeMigrationCLI {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        if (args.length != 6) {
+        if (args.length != 7) {
             usage();
             System.exit(1);
         }
@@ -123,7 +123,7 @@ public class CubeMigrationCLI {
         renameFoldersInHdfs(cube);
         changeHtableHost(cube);
         addCubeIntoProject(cubeName, projectName);
-        if (Boolean.valueOf(copyAcl).booleanValue() == true) {
+        if (Boolean.parseBoolean(copyAcl) == true) {
             copyACL(cube);
         }
 
