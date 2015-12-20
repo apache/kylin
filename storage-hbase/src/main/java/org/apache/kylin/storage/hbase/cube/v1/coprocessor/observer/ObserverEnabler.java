apache/kylin

commit af889ca002ae0d1859eb25a5a0f49ab44fa1dcf2

KYLIN-1222 fix bug & add topN to v1 query engine & restore testing v1 query engine in case need it as a fallback for v2

storage-hbase/src/main/java/org/apache/kylin/storage/hbase/cube/v1/coprocessor/observer/ObserverEnabler.java
@@ -38,8 +38,8 @@ import org.apache.kylin.storage.StorageContext;
 import org.apache.kylin.storage.hbase.common.coprocessor.CoprocessorBehavior;
 import org.apache.kylin.storage.hbase.common.coprocessor.CoprocessorFilter;
 import org.apache.kylin.storage.hbase.common.coprocessor.CoprocessorProjector;
-import org.apache.kylin.storage.hbase.common.coprocessor.FilterDecorator;
 import org.apache.kylin.storage.hbase.common.coprocessor.CoprocessorRowType;
+import org.apache.kylin.storage.hbase.common.coprocessor.FilterDecorator;
 import org.apache.kylin.storage.hbase.cube.v1.RegionScannerAdapter;
 import org.apache.kylin.storage.hbase.cube.v1.ResultScannerAdapter;
 import org.apache.kylin.storage.hbase.steps.RowValueDecoder;
@@ -62,7 +62,7 @@ public class ObserverEnabler {
     public static ResultScanner scanWithCoprocessorIfBeneficial(CubeSegment segment, Cuboid cuboid, TupleFilter tupleFiler, //
             Collection<TblColRef> groupBy, Collection<RowValueDecoder> rowValueDecoders, StorageContext context, HTableInterface table, Scan scan) throws IOException {
 
-        if (context.isCoprocessorEnabled() == false) {
+        if (true) {
             return table.getScanner(scan);
         }
 
@@ -119,10 +119,10 @@ public class ObserverEnabler {
             return r;
         }
 
-        //        if (RowValueDecoder.hasMemHungryCountDistinct(rowValueDecoders)) {
-        //            logger.info("Coprocessor is disabled because there is memory hungry count distinct");
-        //            return false;
-        //        }
+        if (RowValueDecoder.hasMemHungryMeasures(rowValueDecoders)) {
+            logger.info("Coprocessor is disabled because there is memory hungry count distinct");
+            return false;
+        }
 
         if (context.isExactAggregation()) {
             logger.info("Coprocessor is disabled because exactAggregation is true");

query/src/test/java/org/apache/kylin/query/test/ITCombinationTest.java
@@ -22,6 +22,7 @@ import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.apache.kylin.storage.hbase.HBaseStorage;
 import org.apache.kylin.storage.hbase.cube.v1.coprocessor.observer.ObserverEnabler;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -41,6 +42,7 @@ public class ITCombinationTest extends ITKylinQueryTest {
     @AfterClass
     public static void tearDown() {
         clean();
+        HBaseStorage.overwriteStorageQuery = null;
     }
 
     /**
@@ -51,10 +53,10 @@ public class ITCombinationTest extends ITKylinQueryTest {
     @Parameterized.Parameters
     public static Collection<Object[]> configs() {
         //       return Arrays.asList(new Object[][] { { "inner", "unset" }, { "left", "unset" }, { "inner", "off" }, { "left", "off" }, { "inner", "on" }, { "left", "on" }, });
-        return Arrays.asList(new Object[][] { { "inner", "on" }, { "left", "on" } });
+        return Arrays.asList(new Object[][] { { "inner", "on", "v2" }, { "left", "on", "v1" }, { "left", "on", "v2" } });
     }
 
-    public ITCombinationTest(String joinType, String coprocessorToggle) throws Exception {
+    public ITCombinationTest(String joinType, String coprocessorToggle, String queryEngine) throws Exception {
 
         ITKylinQueryTest.clean();
 
@@ -67,6 +69,10 @@ public class ITCombinationTest extends ITKylinQueryTest {
             ObserverEnabler.forceCoprocessorOff();
         } else if (coprocessorToggle.equals("unset")) {
             // unset
+        }
+
+        if ("v1".equalsIgnoreCase(queryEngine)) {
+            HBaseStorage.overwriteStorageQuery = HBaseStorage.v1CubeStorageQuery;
         }
     }
 }

query/src/test/java/org/apache/kylin/query/test/ITKylinQueryTest.java
@@ -31,8 +31,8 @@ import org.apache.kylin.metadata.project.ProjectInstance;
 import org.apache.kylin.query.enumerator.OLAPQuery;
 import org.apache.kylin.query.relnode.OLAPContext;
 import org.apache.kylin.query.schema.OLAPSchemaFactory;
-import org.apache.kylin.storage.hbase.steps.HBaseMetadataTestCase;
 import org.apache.kylin.storage.hbase.cube.v1.coprocessor.observer.ObserverEnabler;
+import org.apache.kylin.storage.hbase.steps.HBaseMetadataTestCase;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.junit.AfterClass;
@@ -95,7 +95,7 @@ public class ITKylinQueryTest extends KylinTestBase {
     @Test
     public void testSingleRunQuery() throws Exception {
 
-        String queryFileName = "src/test/resources/query/temp/query01.sql";
+        String queryFileName = "src/test/resources/query/sql_tableau/query20.sql";
 
         File sqlFile = new File(queryFileName);
         if (sqlFile.exists()) {
@@ -107,7 +107,7 @@ public class ITKylinQueryTest extends KylinTestBase {
     @Test
     public void testSingleExecuteQuery() throws Exception {
 
-        String queryFileName = "src/test/resources/query/sql/query58.sql";
+        String queryFileName = "src/test/resources/query/sql_tableau/query20.sql";
 
         File sqlFile = new File(queryFileName);
         String sql = getTextFromFile(sqlFile);
@@ -168,7 +168,7 @@ public class ITKylinQueryTest extends KylinTestBase {
 
     @Test
     public void testTableauQuery() throws Exception {
-        batchExecuteQuery("src/test/resources/query/sql_tableau");
+        execAndCompResultSize("src/test/resources/query/sql_tableau", null, true);
     }
 
     @Test

query/src/test/java/org/apache/kylin/query/test/KylinTestBase.java
@@ -340,6 +340,39 @@ public class KylinTestBase {
         }
     }
 
+    protected void execAndCompResultSize(String queryFolder, String[] exclusiveQuerys, boolean needSort) throws Exception {
+        printInfo("---------- test folder: " + queryFolder);
+        Set<String> exclusiveSet = buildExclusiveSet(exclusiveQuerys);
+
+        List<File> sqlFiles = getFilesFromFolder(new File(queryFolder), ".sql");
+        for (File sqlFile : sqlFiles) {
+            String queryName = StringUtils.split(sqlFile.getName(), '.')[0];
+            if (exclusiveSet.contains(queryName)) {
+                continue;
+            }
+            String sql = getTextFromFile(sqlFile);
+
+            // execute Kylin
+            printInfo("Query Result from Kylin - " + queryName + "  (" + queryFolder + ")");
+            IDatabaseConnection kylinConn = new DatabaseConnection(cubeConnection);
+            ITable kylinTable = executeQuery(kylinConn, queryName, sql, needSort);
+
+            // execute H2
+            printInfo("Query Result from H2 - " + queryName);
+            H2Connection h2Conn = new H2Connection(h2Connection, null);
+            h2Conn.getConfig().setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new TestH2DataTypeFactory());
+            ITable h2Table = executeQuery(h2Conn, queryName, sql, needSort);
+
+            // compare the result
+            Assert.assertEquals(h2Table.getRowCount(), kylinTable.getRowCount());
+
+            compQueryCount++;
+            if (kylinTable.getRowCount() == 0) {
+                zeroResultQueries.add(sql);
+            }
+        }
+    }
+
     protected void execAndCompQuery(String queryFolder, String[] exclusiveQuerys, boolean needSort) throws Exception {
         printInfo("---------- test folder: " + queryFolder);
         Set<String> exclusiveSet = buildExclusiveSet(exclusiveQuerys);

query/src/test/resources/query/sql/query90.sql
@@ -1,0 +1,24 @@
+--
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements.  See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership.  The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License.  You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+
+ 
+ 
+  select test_kylin_fact.lstg_format_name, sum(price) as GMV, count(seller_id) as TRANS_CNT 
+ from test_kylin_fact where test_kylin_fact.lstg_format_name > 'AB' 
+ group by test_kylin_fact.lstg_format_name having count(seller_id) > 2 

query/src/test/resources/query/sql/query91.sql
@@ -1,0 +1,24 @@
+--
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements.  See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership.  The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License.  You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+
+ 
+ 
+  select test_kylin_fact.lstg_format_name, sum(price) as GMV, count(seller_id) as TRANS_CNT 
+ from test_kylin_fact where test_kylin_fact.lstg_format_name <= 'ABZ' 
+ group by test_kylin_fact.lstg_format_name having count(seller_id) > 2 

storage-hbase/src/main/java/org/apache/kylin/storage/hbase/HBaseStorage.java
@@ -44,8 +44,9 @@ import com.google.common.base.Preconditions;
 //used by reflection
 public class HBaseStorage implements IStorage {
 
-    private final static String v2CubeStorageQuery = "org.apache.kylin.storage.hbase.cube.v2.CubeStorageQuery";
-    private final static String v1CubeStorageQuery = "org.apache.kylin.storage.hbase.cube.v1.CubeStorageQuery";
+    public final static String v2CubeStorageQuery = "org.apache.kylin.storage.hbase.cube.v2.CubeStorageQuery";
+    public final static String v1CubeStorageQuery = "org.apache.kylin.storage.hbase.cube.v1.CubeStorageQuery";
+    public static String overwriteStorageQuery = null;//for test case
 
     private final static String defaultIIStorageQuery = "org.apache.kylin.storage.hbase.ii.InvertedIndexStorageQuery";
 
@@ -71,7 +72,9 @@ public class HBaseStorage implements IStorage {
         } else if (realization.getType() == RealizationType.CUBE) {
 
             String cubeStorageQuery;
-            if ("v1".equalsIgnoreCase(BackdoorToggles.getHbaseCubeQueryVersion())) {
+            if (overwriteStorageQuery != null) {
+                cubeStorageQuery = overwriteStorageQuery;
+            } else if ("v1".equalsIgnoreCase(BackdoorToggles.getHbaseCubeQueryVersion())) {
                 cubeStorageQuery = v1CubeStorageQuery;
             } else {
                 cubeStorageQuery = v2CubeStorageQuery;//by default use v2

storage-hbase/src/main/java/org/apache/kylin/storage/hbase/cube/v1/CubeSegmentTupleIterator.java
@@ -40,6 +40,7 @@ import org.apache.kylin.common.util.Bytes;
 import org.apache.kylin.cube.CubeSegment;
 import org.apache.kylin.cube.cuboid.Cuboid;
 import org.apache.kylin.cube.model.HBaseColumnDesc;
+import org.apache.kylin.measure.MeasureType;
 import org.apache.kylin.metadata.filter.TupleFilter;
 import org.apache.kylin.metadata.model.TblColRef;
 import org.apache.kylin.metadata.tuple.ITupleIterator;
@@ -83,6 +84,10 @@ public class CubeSegmentTupleIterator implements ITupleIterator {
     protected Tuple next;
     protected final Cuboid cuboid;
 
+    private List<MeasureType.IAdvMeasureFiller> advMeasureFillers;
+    private int advMeasureRowsRemaining;
+    private int advMeasureRowIndex;
+
     public CubeSegmentTupleIterator(CubeSegment cubeSeg, List<HBaseKeyRange> keyRanges, HConnection conn, //
             Set<TblColRef> dimensions, TupleFilter filter, Set<TblColRef> groupBy, //
             List<RowValueDecoder> rowValueDecoders, StorageContext context, TupleInfo returnTupleInfo) {
@@ -115,6 +120,17 @@ public class CubeSegmentTupleIterator implements ITupleIterator {
         if (next != null)
             return true;
 
+        // consume any left rows from advanced measure filler
+        if (advMeasureRowsRemaining > 0) {
+            for (MeasureType.IAdvMeasureFiller filler : advMeasureFillers) {
+                filler.fillTuplle(oneTuple, advMeasureRowIndex);
+            }
+            advMeasureRowIndex++;
+            advMeasureRowsRemaining--;
+            next = oneTuple;
+            return true;
+        }
+
         if (resultIterator == null) {
             if (rangeIterator.hasNext() == false)
                 return false;
@@ -132,9 +148,30 @@ public class CubeSegmentTupleIterator implements ITupleIterator {
         scanCount++;
         if (++scanCountDelta >= 1000)
             flushScanCountDelta();
-        tupleConverter.translateResult(result, oneTuple);
-        next = oneTuple;
-        return true;
+
+        // translate into tuple
+        advMeasureFillers = tupleConverter.translateResult(result, oneTuple);
+
+        // the simple case
+        if (advMeasureFillers == null) {
+            next = oneTuple;
+            return true;
+        }
+
+        // advanced measure filling, like TopN, will produce multiple tuples out of one record
+        advMeasureRowsRemaining = -1;
+        for (MeasureType.IAdvMeasureFiller filler : advMeasureFillers) {
+            if (advMeasureRowsRemaining < 0)
+                advMeasureRowsRemaining = filler.getNumOfRows();
+            if (advMeasureRowsRemaining != filler.getNumOfRows())
+                throw new IllegalStateException();
+        }
+        if (advMeasureRowsRemaining < 0)
+            throw new IllegalStateException();
+
+        advMeasureRowIndex = 0;
+        return hasNext();
+
     }
 
     @Override

commit acd1410ed34a7c86e243714da573c34ddd4f8a03

minor, fix a bug introduced in af889ca002ae0d1859eb25a5a0f49ab44fa1dcf2

storage-hbase/src/main/java/org/apache/kylin/storage/hbase/cube/v1/coprocessor/observer/ObserverEnabler.java
@@ -62,7 +62,7 @@ public class ObserverEnabler {
     public static ResultScanner scanWithCoprocessorIfBeneficial(CubeSegment segment, Cuboid cuboid, TupleFilter tupleFiler, //
             Collection<TblColRef> groupBy, Collection<RowValueDecoder> rowValueDecoders, StorageContext context, HTableInterface table, Scan scan) throws IOException {
 
-        if (true) {
+        if (context.isCoprocessorEnabled() == false) {
             return table.getScanner(scan);
         }
 