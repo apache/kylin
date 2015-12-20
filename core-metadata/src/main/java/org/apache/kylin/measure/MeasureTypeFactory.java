8c2d658082d8c07194ba9c3a4c152b410df2823e
Yang Li <liyang@apache.org>
2015-12-20 08:59:11 +0800

    KYLIN-976 Support multiple implementations for one aggr function

--- core-metadata/src/main/java/org/apache/kylin/measure/MeasureTypeFactory.java
+++ core-metadata/src/main/java/org/apache/kylin/measure/MeasureTypeFactory.java
@@ -76,8 +76,8 @@ abstract public class MeasureTypeFactory<T> {
 
     // ============================================================================
 
-    private static Map<String, MeasureTypeFactory<?>> factories = Maps.newHashMap();
-    private static MeasureTypeFactory<?> defaultFactory = new BasicMeasureType.Factory();
+    private static Map<String, List<MeasureTypeFactory<?>>> factories = Maps.newHashMap();
+    private static List<MeasureTypeFactory<?>> defaultFactory = Lists.newArrayListWithCapacity(2);
 
     static {
         init();
@@ -98,19 +98,28 @@ abstract public class MeasureTypeFactory<T> {
          * More MeasureType cannot be configured via kylin.properties alone,
          * because used in coprocessor, the new classes must be on classpath
          * and be packaged into coprocessor jar. This inevitably involves
-         * rebuild Kylin from code and redeploy.
+         * rebuild Kylin from code.
          */
 
         // register factories & data type serializers
         for (MeasureTypeFactory<?> factory : factoryInsts) {
-            String funcName = factory.getAggrFunctionName().toUpperCase();
+            String funcName = factory.getAggrFunctionName();
+            if (funcName.equals(funcName.toUpperCase()) == false)
+                throw new IllegalArgumentException("Aggregation function name '" + funcName + "' must be in upper case");
             String dataTypeName = factory.getAggrDataTypeName().toLowerCase();
+            if (dataTypeName.equals(dataTypeName.toLowerCase()) == false)
+                throw new IllegalArgumentException("Aggregation data type name '" + dataTypeName + "' must be in lower case");
             Class<? extends DataTypeSerializer<?>> serializer = factory.getAggrDataTypeSerializer();
 
             DataType.register(dataTypeName);
             DataTypeSerializer.register(dataTypeName, serializer);
-            factories.put(funcName, factory);
+            List<MeasureTypeFactory<?>> list = factories.get(funcName);
+            if (list == null)
+                factories.put(funcName, list = Lists.newArrayListWithCapacity(2));
+            list.add(factory);
         }
+
+        defaultFactory.add(new BasicMeasureType.Factory());
     }
 
     public static MeasureType<?> create(String funcName, String dataType) {
@@ -120,10 +129,62 @@ abstract public class MeasureTypeFactory<T> {
     public static MeasureType<?> create(String funcName, DataType dataType) {
         funcName = funcName.toUpperCase();
 
-        MeasureTypeFactory<?> factory = factories.get(funcName);
+        List<MeasureTypeFactory<?>> factory = factories.get(funcName);
         if (factory == null)
             factory = defaultFactory;
 
-        return factory.createMeasureType(funcName, dataType);
+        // a special case where in early stage of sql parsing, the data type is unknown; only needRewrite() is required at that stage
+        if (dataType == null) {
+            return new NeedRewriteOnlyMeasureType(funcName, factory);
+        }
+
+        // the normal case, only one factory for a function
+        if (factory.size() == 1) {
+            return factory.get(0).createMeasureType(funcName, dataType);
+        }
+
+        // sometimes multiple factories are registered for the same function, then data types must tell them apart
+        for (MeasureTypeFactory<?> f : factory) {
+            if (f.getAggrDataTypeName().equals(dataType.getName()))
+                return f.createMeasureType(funcName, dataType);
+        }
+        throw new IllegalStateException();
+    }
+
+    @SuppressWarnings("rawtypes")
+    private static class NeedRewriteOnlyMeasureType extends MeasureType {
+
+        private Boolean needRewrite;
+
+        public NeedRewriteOnlyMeasureType(String funcName, List<MeasureTypeFactory<?>> factory) {
+            for (MeasureTypeFactory<?> f : factory) {
+                boolean b = f.createMeasureType(funcName, null).needRewrite();
+                if (needRewrite == null)
+                    needRewrite = Boolean.valueOf(b);
+                else if (needRewrite.booleanValue() != b)
+                    throw new IllegalStateException("needRewrite() of factorys " + factory + " does not have consensus");
+            }
+        }
+
+        @Override
+        public MeasureIngester newIngester() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public MeasureAggregator newAggregator() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean needRewrite() {
+            return needRewrite;
+        }
+
+        @Override
+        public Class getRewriteCalciteAggrFunctionClass() {
+            throw new UnsupportedOperationException();
+        }
+        
     }
 }
