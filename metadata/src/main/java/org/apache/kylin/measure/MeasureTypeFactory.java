ae891eeb97c731746c610c910012947c299629bf
lidongsjtu <lidong@apache.org>
2015-12-27 15:13:52 +0800

    KYLIN-976 Support multiple implementations for one aggr function

--- metadata/src/main/java/org/apache/kylin/measure/MeasureTypeFactory.java
+++ metadata/src/main/java/org/apache/kylin/measure/MeasureTypeFactory.java
@@ -31,19 +31,8 @@ import com.google.common.collect.Maps;
 
 abstract public class MeasureTypeFactory<T> {
 
-    abstract public MeasureType<T> createMeasureType(String funcName, DataType dataType);
-
-    abstract public String getAggrFunctionName();
-
-    abstract public String getAggrDataTypeName();
-
-    abstract public Class<? extends DataTypeSerializer<T>> getAggrDataTypeSerializer();
-
-    // ============================================================================
-
-
-    private static Map<String, MeasureTypeFactory<?>> factories = Maps.newHashMap();
-    private static MeasureTypeFactory<?> defaultFactory = new BasicMeasureType.Factory();
+    private static Map<String, List<MeasureTypeFactory<?>>> factories = Maps.newHashMap();
+    private static List<MeasureTypeFactory<?>> defaultFactory = Lists.newArrayListWithCapacity(2);
 
     static {
         init();
@@ -67,15 +56,26 @@ abstract public class MeasureTypeFactory<T> {
 
         // register factories & data type serializers
         for (MeasureTypeFactory<?> factory : factoryInsts) {
-            String funcName = factory.getAggrFunctionName().toUpperCase();
-            String dataTypeName = factory.getAggrDataTypeName().toLowerCase();
+            String funcName = factory.getAggrFunctionName();
+            if (funcName.equals(funcName.toUpperCase()) == false)
+                throw new IllegalArgumentException("Aggregation function name '" + funcName + "' must be in upper case");
+            String dataTypeName = factory.getAggrDataTypeName();
+            if (dataTypeName.equals(dataTypeName.toLowerCase()) == false)
+                throw new IllegalArgumentException("Aggregation data type name '" + dataTypeName + "' must be in lower case");
             Class<? extends DataTypeSerializer<?>> serializer = factory.getAggrDataTypeSerializer();
 
             DataType.register(dataTypeName);
             DataTypeSerializer.register(dataTypeName, serializer);
-            factories.put(funcName, factory);
+            List<MeasureTypeFactory<?>> list = factories.get(funcName);
+            if (list == null)
+                factories.put(funcName, list = Lists.newArrayListWithCapacity(2));
+            list.add(factory);
         }
+
+        defaultFactory.add(new BasicMeasureType.Factory());
     }
+
+    // ============================================================================
 
     public static MeasureType<?> create(String funcName, String dataType) {
         return create(funcName, DataType.getInstance(dataType));
@@ -84,10 +84,70 @@ abstract public class MeasureTypeFactory<T> {
     public static MeasureType<?> create(String funcName, DataType dataType) {
         funcName = funcName.toUpperCase();
 
-        MeasureTypeFactory<?> factory = factories.get(funcName);
+        List<MeasureTypeFactory<?>> factory = factories.get(funcName);
         if (factory == null)
             factory = defaultFactory;
 
-        return factory.createMeasureType(funcName, dataType);
+        // a special case where in early stage of sql parsing, the data type is unknown; only needRewrite() is required at that stage
+        if (dataType == null) {
+            return new NeedRewriteOnlyMeasureType(funcName, factory);
+        }
+
+        // the normal case, only one factory for a function
+        if (factory.size() == 1) {
+            return factory.get(0).createMeasureType(funcName, dataType);
+        }
+
+        // sometimes multiple factories are registered for the same function, then data types must tell them apart
+        for (MeasureTypeFactory<?> f : factory) {
+            if (f.getAggrDataTypeName().equals(dataType.getName()))
+                return f.createMeasureType(funcName, dataType);
+        }
+        throw new IllegalStateException();
+    };
+
+    abstract public MeasureType<T> createMeasureType(String funcName, DataType dataType);
+
+    abstract public String getAggrFunctionName();
+
+    abstract public String getAggrDataTypeName();
+
+    abstract public Class<? extends DataTypeSerializer<T>> getAggrDataTypeSerializer();
+
+    @SuppressWarnings("rawtypes")
+    private static class NeedRewriteOnlyMeasureType extends MeasureType {
+
+        private Boolean needRewrite;
+
+        public NeedRewriteOnlyMeasureType(String funcName, List<MeasureTypeFactory<?>> factory) {
+            for (MeasureTypeFactory<?> f : factory) {
+                boolean b = f.createMeasureType(funcName, null).needRewrite();
+                if (needRewrite == null)
+                    needRewrite = Boolean.valueOf(b);
+                else if (needRewrite.booleanValue() != b)
+                    throw new IllegalStateException("needRewrite() of factorys " + factory + " does not have consensus");
+            }
+        }
+
+        @Override
+        public MeasureIngester newIngester() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public MeasureAggregator newAggregator() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean needRewrite() {
+            return needRewrite;
+        }
+
+        @Override
+        public Class getRewriteCalciteAggrFunctionClass() {
+            throw new UnsupportedOperationException();
+        }
+
     }
 }
