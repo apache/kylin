ba06c6a3b868c9722bb5b2a25f46b451b7c0472e
honma <honma@ebay.com>
2015-12-30 21:33:37 +0800

    KYLIN-1270 improve TimedJsonStreamParser to support week_start, month_start, quarter_start, year_start

--- core-common/src/main/java/org/apache/kylin/common/util/TimeUtil.java
+++ core-common/src/main/java/org/apache/kylin/common/util/TimeUtil.java
@@ -1,12 +1,13 @@
 package org.apache.kylin.common.util;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 /**
  */
 public class TimeUtil {
-    public enum NormalizedTimeUnit {
-        MINUTE, HOUR, DAY
-    }
 
+    private static TimeZone gmt = TimeZone.getTimeZone("GMT");
     private static long ONE_MINUTE_TS = 60 * 1000;
     private static long ONE_HOUR_TS = 60 * ONE_MINUTE_TS;
     private static long ONE_DAY_TS = 24 * ONE_HOUR_TS;
@@ -23,7 +24,40 @@ public class TimeUtil {
         return ts / ONE_DAY_TS * ONE_DAY_TS;
     }
 
-    public static long getNextPeriodStart(long ts, long period) {
-        return ((ts + period - 1) / period) * period;
+    public static long getWeekStart(long ts) {
+        Calendar calendar = Calendar.getInstance(gmt);
+        calendar.setTimeInMillis(getDayStart(ts));
+        calendar.add(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek() - calendar.get(Calendar.DAY_OF_WEEK));
+        return calendar.getTimeInMillis();
     }
+
+    public static long getMonthStart(long ts) {
+        Calendar calendar = Calendar.getInstance(gmt);
+        calendar.setTimeInMillis(ts);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        calendar.clear();
+        calendar.set(year, month, 1);
+        return calendar.getTimeInMillis();
+    }
+
+    public static long getQuarterStart(long ts) {
+        Calendar calendar = Calendar.getInstance(gmt);
+        calendar.setTimeInMillis(ts);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        calendar.clear();
+        calendar.set(year, month / 3 * 3, 1);
+        return calendar.getTimeInMillis();
+    }
+
+    public static long getYearStart(long ts) {
+        Calendar calendar = Calendar.getInstance(gmt);
+        calendar.setTimeInMillis(ts);
+        int year = calendar.get(Calendar.YEAR);
+        calendar.clear();
+        calendar.set(year, 0, 1);
+        return calendar.getTimeInMillis();
+    }
+
 }
