fb2f2cc030b39afddddd590893482fa83e730371
jian <zhongjian@apache.org>
2016-01-12 17:05:27 +0800

    KYLIN-1251 enable nav tree expand and hide funciton

---
+++ webapp/app/js/directives/angular-tree-control.js
@@ -0,0 +1,227 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013 Steve
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ *   subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+  (function ( angular ) {
+    'use strict';
+
+    angular.module( 'treeControl', [] )
+        .directive( 'treecontrol', ['$compile', function( $compile ) {
+            /**
+             * @param cssClass - the css class
+             * @param addClassProperty - should we wrap the class name with class=""
+             */
+            function classIfDefined(cssClass, addClassProperty) {
+                if (cssClass) {
+                    if (addClassProperty)
+                        return 'class="' + cssClass + '"';
+                    else
+                        return cssClass;
+                }
+                else
+                    return "";
+            }
+
+            function ensureDefault(obj, prop, value) {
+                if (!obj.hasOwnProperty(prop))
+                    obj[prop] = value;
+            }
+
+            return {
+                restrict: 'EA',
+                require: "treecontrol",
+                transclude: true,
+                scope: {
+                    treeModel: "=",
+                    selectedNode: "=?",
+                    onSelection: "&",
+                    options: "=?",
+                    onDblclick: "&"
+                },
+                controller: function( $scope ) {
+                    function defaultIsSelectable() {
+                      return true;
+                    }
+
+                    $scope.options = $scope.options || {};
+                    ensureDefault($scope.options, "nodeChildren", "children");
+                    ensureDefault($scope.options, "dirSelectable", "true");
+                    ensureDefault($scope.options, "injectClasses", {});
+                    ensureDefault($scope.options.injectClasses, "ul", "");
+                    ensureDefault($scope.options.injectClasses, "li", "");
+                    ensureDefault($scope.options.injectClasses, "liSelected", "");
+                    ensureDefault($scope.options.injectClasses, "iExpanded", "");
+                    ensureDefault($scope.options.injectClasses, "iCollapsed", "");
+                    ensureDefault($scope.options.injectClasses, "iLeaf", "");
+                    ensureDefault($scope.options.injectClasses, "label", "");
+                    ensureDefault($scope.options.injectClasses, "labelSelected", "");
+                    ensureDefault($scope.options, "isSelectable", defaultIsSelectable);
+                  ensureDefault($scope.options, "equality", function (a, b) {
+                        if (a === undefined || b === undefined)
+                            return false;
+                        a = angular.copy(a); a[$scope.options.nodeChildren] = [];
+                        b = angular.copy(b); b[$scope.options.nodeChildren] = [];
+                        return angular.equals(a, b);
+                    });
+
+                    $scope.expandedNodes = {};
+                    $scope.parentScopeOfTree = $scope.$parent;
+
+                    $scope.headClass = function(node) {
+                        var liSelectionClass = classIfDefined($scope.options.injectClasses.liSelected, false);
+                        var injectSelectionClass = "";
+                        if (liSelectionClass && (this.$id == $scope.selectedScope))
+                            injectSelectionClass = " " + liSelectionClass;
+                        if (!node[$scope.options.nodeChildren] || node[$scope.options.nodeChildren].length === 0)
+                            return "tree-leaf" + injectSelectionClass
+                        if ($scope.expandedNodes[this.$id])
+                            return "tree-expanded" + injectSelectionClass;
+                        else
+                            return "tree-collapsed" + injectSelectionClass;
+                    };
+
+                    $scope.iBranchClass = function() {
+                        if ($scope.expandedNodes[this.$id])
+                            return classIfDefined($scope.options.injectClasses.iExpanded);
+                        else
+                            return classIfDefined($scope.options.injectClasses.iCollapsed);
+                    };
+
+                    $scope.nodeExpanded = function() {
+                        return !!$scope.expandedNodes[this.$id];
+                    };
+
+                    $scope.selectNodeHead = function() {
+                        $scope.expandedNodes[this.$id] = ($scope.expandedNodes[this.$id] === undefined ? this.node : undefined);
+                    };
+
+                    $scope.selectNodeLabel = function( selectedNode ){
+                        if (selectedNode[$scope.options.nodeChildren] && selectedNode[$scope.options.nodeChildren].length > 0 &&
+                          (!$scope.options.dirSelectable||!$scope.options.isSelectable(selectedNode))) {
+                            this.selectNodeHead();
+                        }
+                        else {
+                            $scope.selectedScope = this.$id;
+                            $scope.selectedNode = selectedNode;
+                            if ($scope.onSelection)
+                                $scope.onSelection({node: selectedNode});
+                        }
+                    };
+
+                    $scope.dblClickNode = function(selectedNode){
+                        if($scope.onDblclick!=null){
+                           $scope.onDblclick({node:selectedNode});
+                        }
+                    }
+
+                    $scope.selectedClass = function() {
+                        var labelSelectionClass = classIfDefined($scope.options.injectClasses.labelSelected, false);
+                        var injectSelectionClass = "";
+                        if (labelSelectionClass && (this.$id == $scope.selectedScope))
+                            injectSelectionClass = " " + labelSelectionClass;
+
+                        return (this.$id == $scope.selectedScope)?"tree-selected" + injectSelectionClass:"";
+                    };
+
+                    //tree template
+                    var template =
+                        '<ul '+classIfDefined($scope.options.injectClasses.ul, true)+'>' +
+                            '<li ng-repeat="node in node.' + $scope.options.nodeChildren+'" ng-class="headClass(node)" '+classIfDefined($scope.options.injectClasses.li, true)+'>' +
+                            '<i class="tree-branch-head" ng-class="iBranchClass()" ng-click="selectNodeHead(node)"></i>' +
+                            '<i class="tree-leaf-head '+classIfDefined($scope.options.injectClasses.iLeaf, false)+'"></i>' +
+                            '<div class="tree-label '+classIfDefined($scope.options.injectClasses.label, false)+'" ng-class="selectedClass()" ng-click="selectNodeLabel(node)" ng-dblclick="dblClickNode(node)" tree-transclude></div>' +
+                            '<treeitem ng-if="nodeExpanded()"></treeitem>' +
+                            '</li>' +
+                            '</ul>';
+
+                    return {
+                        template: $compile(template)
+                    }
+                },
+                compile: function(element, attrs, childTranscludeFn) {
+                    return function ( scope, element, attrs, treemodelCntr ) {
+
+                        scope.$watch("treeModel", function updateNodeOnRootScope(newValue) {
+                            if (angular.isArray(newValue)) {
+                                if (angular.isDefined(scope.node) && angular.equals(scope.node[scope.options.nodeChildren], newValue))
+                                    return;
+                                scope.node = {};
+                                scope.node[scope.options.nodeChildren] = newValue;
+                            }
+                            else {
+                                if (angular.equals(scope.node, newValue))
+                                    return;
+                                scope.node = newValue;
+                            }
+                        });
+
+                        //Rendering template for a root node
+                        treemodelCntr.template( scope, function(clone) {
+                            element.html('').append( clone );
+                        });
+                        // save the transclude function from compile (which is not bound to a scope as apposed to the one from link)
+                        // we can fix this to work with the link transclude function with angular 1.2.6. as for angular 1.2.0 we need
+                        // to keep using the compile function
+                        scope.$treeTransclude = childTranscludeFn;
+                    }
+                }
+            };
+        }])
+        .directive("treeitem", function() {
+            return {
+                restrict: 'E',
+                require: "^treecontrol",
+                link: function( scope, element, attrs, treemodelCntr) {
+                    // Rendering template for the current node
+                    treemodelCntr.template(scope, function(clone) {
+                        element.html('').append(clone);
+                    });
+                }
+            }
+        })
+        .directive("treeTransclude", function() {
+            return {
+                link: function(scope, element, attrs, controller) {
+                    angular.forEach(scope.expandedNodes, function (node, id) {
+                        if (scope.options.equality(node, scope.node)) {
+                            scope.expandedNodes[scope.$id] = scope.node;
+                            scope.expandedNodes[id] = undefined;
+                        }
+                    });
+                    if (scope.options.equality(scope.node, scope.selectedNode)) {
+                        scope.selectNodeLabel(scope.node);
+                    }
+
+                    // create a scope for the transclusion, whos parent is the parent of the tree control
+                    scope.transcludeScope = scope.parentScopeOfTree.$new();
+                    scope.transcludeScope.node = scope.node;
+                    scope.$on('$destroy', function() {
+                        scope.transcludeScope.$destroy();
+                    });
+
+                    scope.$treeTransclude(scope.transcludeScope, function(clone) {
+                        element.empty();
+                        element.append(clone);
+                    });
+                }
+            }
+        });
+})( angular );
