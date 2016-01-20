1932990ba47558b60f32180c8639c6f368aa3824
lidongsjtu <lidong@apache.org>
2016-01-20 15:27:58 +0800

    KYLIN-1346 Fix IllegalStateException issue in SparkCubing

--- engine-spark/src/main/java/org/apache/kylin/engine/spark/cube/DefaultTupleConverter.java
+++ engine-spark/src/main/java/org/apache/kylin/engine/spark/cube/DefaultTupleConverter.java
@@ -84,7 +84,7 @@ public final class DefaultTupleConverter implements TupleConverter {
         }
 
         byte[] rowKey = rowkeyEncoder.createBuf();
-        rowkeyEncoder.encode(new ByteArray(rowKeyBodyBuf), new ByteArray(rowKey));
+        rowkeyEncoder.encode(new ByteArray(rowKeyBodyBuf, 0, offSet), new ByteArray(rowKey));
 
         ByteBuffer valueBuf = getValueBuf();
         valueBuf.clear();
