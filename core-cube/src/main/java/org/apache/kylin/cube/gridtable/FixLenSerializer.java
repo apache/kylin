d2673969fb40436d1c55f359b2b709c55cfab8da
honma <honma@ebay.com>
2016-01-26 09:54:57 +0800

    minor, avoid verbose logging in Serializer

--- core-cube/src/main/java/org/apache/kylin/cube/gridtable/FixLenSerializer.java
+++ core-cube/src/main/java/org/apache/kylin/cube/gridtable/FixLenSerializer.java
@@ -36,6 +36,7 @@ public class FixLenSerializer extends DataTypeSerializer {
     private ThreadLocal<byte[]> current = new ThreadLocal<byte[]>();
 
     private int fixLen;
+    transient int avoidVerbose = 0;
 
     FixLenSerializer(int fixLen) {
         this.fixLen = fixLen;
@@ -59,7 +60,9 @@ public class FixLenSerializer extends DataTypeSerializer {
         } else {
             byte[] bytes = Bytes.toBytes(value.toString());
             if (bytes.length > fixLen) {
-                logger.warn("Expect at most " + fixLen + " bytes, but got " + bytes.length + ", will truncate, value string: " + value.toString());
+                if (avoidVerbose++ % 10000 == 0) {
+                    logger.warn("Expect at most " + fixLen + " bytes, but got " + bytes.length + ", will truncate, value string: " + value.toString() + " times:" + avoidVerbose);
+                }
             }
             out.put(bytes, 0, Math.min(bytes.length, fixLen));
             for (int i = bytes.length; i < fixLen; i++) {
