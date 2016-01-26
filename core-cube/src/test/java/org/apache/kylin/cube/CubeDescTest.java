5c1d0894791d5317a3c469a239aad5234c189629
Chen Luwei <chenluweits93114@hotmail.com>
2016-01-26 11:03:44 +0800

    KYLIN-1405 add validation when init cube desc
    Signed-off-by: Li, Yang <liyang@apache.org>

--- core-cube/src/test/java/org/apache/kylin/cube/CubeDescTest.java
+++ core-cube/src/test/java/org/apache/kylin/cube/CubeDescTest.java
@@ -18,23 +18,27 @@
 
 package org.apache.kylin.cube;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import com.google.common.collect.Maps;
 import org.apache.kylin.common.util.JsonUtil;
 import org.apache.kylin.common.util.LocalFileMetadataTestCase;
 import org.apache.kylin.cube.model.CubeDesc;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.apache.kylin.cube.model.SelectRule;
+import org.apache.kylin.metadata.MetadataManager;
+import org.junit.*;
+import org.junit.rules.ExpectedException;
 
-import com.google.common.collect.Maps;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author yangli9
  */
 public class CubeDescTest extends LocalFileMetadataTestCase {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
 
     @Before
     public void setUp() throws Exception {
@@ -44,6 +48,158 @@ public class CubeDescTest extends LocalFileMetadataTestCase {
     @After
     public void after() throws Exception {
         this.cleanupTestMetadata();
+    }
+
+    @Test
+    public void testGoodInit() throws Exception {
+        CubeDesc cubeDesc = CubeDescManager.getInstance(getTestConfig()).getCubeDesc("test_kylin_cube_with_slr_desc");
+        cubeDesc.init(getTestConfig(), MetadataManager.getInstance(getTestConfig()).getAllTablesMap());
+    }
+
+    @Test
+    public void testBadInit1() throws Exception {
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage("Aggregation group 0 includes field not set");
+
+        CubeDesc cubeDesc = CubeDescManager.getInstance(getTestConfig()).getCubeDesc("test_kylin_cube_with_slr_desc");
+        String[] temp = null;
+        cubeDesc.getAggregationGroups().get(0).setIncludes(temp);
+
+        cubeDesc.init(getTestConfig(), MetadataManager.getInstance(getTestConfig()).getAllTablesMap());
+    }
+
+    @Test
+    public void testBadInit2() throws Exception {
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage("Aggregation group 0 select rule field not set");
+
+        CubeDesc cubeDesc = CubeDescManager.getInstance(getTestConfig()).getCubeDesc("test_kylin_cube_with_slr_desc");
+        SelectRule temp = null;
+        cubeDesc.getAggregationGroups().get(0).setSelectRule(temp);
+
+        cubeDesc.init(getTestConfig(), MetadataManager.getInstance(getTestConfig()).getAllTablesMap());
+    }
+
+    @Test
+    public void testBadInit3() throws Exception {
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage("Aggregation group 0 Include dims not containing all the used dims");
+
+        CubeDesc cubeDesc = CubeDescManager.getInstance(getTestConfig()).getCubeDesc("test_kylin_cube_with_slr_desc");
+        String[] temp = Arrays.asList(cubeDesc.getAggregationGroups().get(0).getIncludes()).subList(0, 3).toArray(new String[3]);
+        cubeDesc.getAggregationGroups().get(0).setIncludes(temp);
+
+        cubeDesc.init(getTestConfig(), MetadataManager.getInstance(getTestConfig()).getAllTablesMap());
+    }
+
+    @Test
+    public void testBadInit4() throws Exception {
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage("Aggregation group 0 has too many dimensions");
+
+        CubeDesc desc = new CubeDesc() {
+            @Override
+            protected int getMaxAgrGroupSize() {
+                return 3;
+            }
+        };
+        CubeDesc cubeDesc = CubeDescManager.getInstance(getTestConfig()).getCubeDesc("test_kylin_cube_with_slr_desc");
+        desc.validate(cubeDesc);
+    }
+
+    @Test
+    public void testBadInit5() throws Exception {
+        CubeDesc cubeDesc = CubeDescManager.getInstance(getTestConfig()).getCubeDesc("test_kylin_cube_with_slr_desc");
+        cubeDesc.getAggregationGroups().get(0).getSelectRule().mandatory_dims = new String[] {
+                "seller_id", "META_CATEG_NAME"};
+
+        cubeDesc.init(getTestConfig(), MetadataManager.getInstance(getTestConfig()).getAllTablesMap());
+    }
+
+    @Test
+    public void testBadInit6() throws Exception {
+        CubeDesc cubeDesc = CubeDescManager.getInstance(getTestConfig()).getCubeDesc("test_kylin_cube_with_slr_desc");
+        cubeDesc.getAggregationGroups().get(0).getSelectRule().mandatory_dims = new String[] {
+                "seller_id", "lstg_format_name"};
+
+        cubeDesc.init(getTestConfig(), MetadataManager.getInstance(getTestConfig()).getAllTablesMap());
+    }
+
+    @Test
+    public void testBadInit7() throws Exception {
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage("Aggregation group 0 require at least 2 dims in a joint");
+
+        CubeDesc cubeDesc = CubeDescManager.getInstance(getTestConfig()).getCubeDesc("test_kylin_cube_with_slr_desc");
+        cubeDesc.getAggregationGroups().get(0).getSelectRule().joint_dims = new String[][] {
+                new String[] { "lstg_format_name" } };
+
+        cubeDesc.init(getTestConfig(), MetadataManager.getInstance(getTestConfig()).getAllTablesMap());
+    }
+
+    @Test
+    public void testBadInit8() throws Exception {
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage("Aggregation group 0 hierarchy dims overlap with joint dims");
+
+        CubeDesc cubeDesc = CubeDescManager.getInstance(getTestConfig()).getCubeDesc("test_kylin_cube_with_slr_desc");
+        cubeDesc.getAggregationGroups().get(0).getSelectRule().joint_dims = new String[][] {
+                new String[] { "META_CATEG_NAME", "CATEG_LVL2_NAME" } };
+
+        cubeDesc.init(getTestConfig(), MetadataManager.getInstance(getTestConfig()).getAllTablesMap());
+    }
+
+    @Test
+    public void testBadInit9() throws Exception {
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage("Aggregation group 0 hierarchy dims overlap with joint dims");
+
+        CubeDesc cubeDesc = CubeDescManager.getInstance(getTestConfig()).getCubeDesc("test_kylin_cube_with_slr_desc");
+        cubeDesc.getAggregationGroups().get(0).getSelectRule().hierarchy_dims = new String[][] {
+                new String[] { "META_CATEG_NAME", "CATEG_LVL2_NAME", "CATEG_LVL3_NAME" },
+                new String[] { "lstg_format_name", "lstg_site_id" } };
+        cubeDesc.getAggregationGroups().get(0).getSelectRule().joint_dims = new String[][] {
+                new String[] { "META_CATEG_NAME", "lstg_format_name" } };
+
+        cubeDesc.init(getTestConfig(), MetadataManager.getInstance(getTestConfig()).getAllTablesMap());
+    }
+
+    @Test
+    public void testBadInit10() throws Exception {
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage("Aggregation group 0 a dim exist in more than one joint");
+
+        CubeDesc cubeDesc = CubeDescManager.getInstance(getTestConfig()).getCubeDesc("test_kylin_cube_with_slr_desc");
+        cubeDesc.getAggregationGroups().get(0).getSelectRule().joint_dims = new String[][] {
+                new String[] { "lstg_format_name", "lstg_site_id", "slr_segment_cd" },
+                new String[] { "lstg_format_name", "lstg_site_id", "leaf_categ_id"} };
+
+        cubeDesc.init(getTestConfig(), MetadataManager.getInstance(getTestConfig()).getAllTablesMap());
+    }
+
+    @Test
+    public void testBadInit11() throws Exception {
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage("Aggregation group 0 require at least 2 dims in a hierarchy");
+
+        CubeDesc cubeDesc = CubeDescManager.getInstance(getTestConfig()).getCubeDesc("test_kylin_cube_with_slr_desc");
+        cubeDesc.getAggregationGroups().get(0).getSelectRule().hierarchy_dims = new String[][] {
+                new String[] { "META_CATEG_NAME" } };
+
+        cubeDesc.init(getTestConfig(), MetadataManager.getInstance(getTestConfig()).getAllTablesMap());
+    }
+
+    @Test
+    public void testBadInit12() throws Exception {
+        thrown.expect(IllegalStateException.class);
+        thrown.expectMessage("Aggregation group 0 a dim exist in more than one hierarchy");
+
+        CubeDesc cubeDesc = CubeDescManager.getInstance(getTestConfig()).getCubeDesc("test_kylin_cube_with_slr_desc");
+        cubeDesc.getAggregationGroups().get(0).getSelectRule().hierarchy_dims = new String[][] {
+                new String[] { "META_CATEG_NAME", "CATEG_LVL2_NAME", "CATEG_LVL3_NAME" },
+                new String[] { "META_CATEG_NAME", "CATEG_LVL2_NAME" } };
+
+        cubeDesc.init(getTestConfig(), MetadataManager.getInstance(getTestConfig()).getAllTablesMap());
     }
 
     @Test
