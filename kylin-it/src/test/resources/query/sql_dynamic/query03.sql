957dd42e7b5cff5dc70204d181aaeff3a9ae0349
honma <honma@ebay.com>
2016-02-01 13:44:02 +0800

    KYLIN-1372 fix CI

--- kylin-it/src/test/resources/query/sql_dynamic/query03.sql
+++ kylin-it/src/test/resources/query/sql_dynamic/query03.sql
@@ -17,7 +17,7 @@
 --
 
 select test_cal_dt.week_beg_dt,sum(test_kylin_fact.price) as GMV
- , count(1) as TRANS_CNT, count(distinct test_kylin_fact.leaf_categ_id) as LEAF_CATEG_CNT
+ , count(1) as TRANS_CNT
  from test_kylin_fact
  inner JOIN edw.test_cal_dt as test_cal_dt
  ON test_kylin_fact.cal_dt = test_cal_dt.cal_dt
