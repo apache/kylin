f8175780da0a18828189484e5a2689615caa76fd
lidongsjtu <lidong@apache.org>
2016-02-01 15:29:46 +0800

    KYLIN-1232 ODBC - Refine connection and configuration dialog UI

--- odbc/Driver/resource.h
+++ odbc/Driver/resource.h
@@ -23,6 +23,7 @@
 #define IDOK2                           3
 #define IDD_DSN_CFG1                    101
 #define IDD_DSN_CFG2                    102
+#define IDB_BITMAP1                     108
 #define IDC_DSNNAME                     1000
 #define IDC_SERVER                      1001
 #define IDC_PORT                        1002
@@ -31,20 +32,21 @@
 #define IDC_BTEST                       1005
 #define IDC_CONNECT                     1006
 #define IDC_COMBO1                      1007
-
 #define IDC_COMPANY                     1008
+#define IDC_PROTOCOL                    1008
 #define IDC_PREV                        1009
 #define IDC_DATABASE                    1010
 #define IDC_SERVER2                     1011
 #define IDC_BUTTON1                     1012
+#define IDC_COMBO2                      1015
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_RESOURCE_VALUE        111
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_CONTROL_VALUE         1016
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
