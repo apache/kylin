195f751041b7770f1c45497fe7728d968ebc3ad5
lidongsjtu <lidong@apache.org>
2016-04-01 13:36:27 +0800

    KYLIN-1555 Add missing license headers

--- core-metadata/src/test/java/org/apache/kylin/measure/raw/RawAggregatorTest.java
+++ core-metadata/src/test/java/org/apache/kylin/measure/raw/RawAggregatorTest.java
@@ -1,20 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.kylin.measure.raw;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.kylin.common.util.ByteArray;
 import org.apache.kylin.common.util.BytesUtil;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-
 public class RawAggregatorTest {
     private RawAggregator agg = new RawAggregator();
 
     @Test
-    public void testNormal(){
+    public void testNormal() {
         int size = 100;
         List<ByteArray> valueList = new ArrayList<ByteArray>(size);
         for (Integer i = 0; i < size; i++) {
@@ -28,7 +45,7 @@ public class RawAggregatorTest {
     }
 
     @Test
-    public void testNull(){
+    public void testNull() {
         agg.aggregate(null);
         assertEquals(agg.getState(), null);
     }
