19e9582fddd5bdabadb88e1a042b7874c11c38c6
Hongbin Ma <mahongbin@apache.org>
2016-04-20 17:15:43 +0800

    bug fix

--- core-common/src/main/java/org/apache/kylin/common/util/ByteArray.java
+++ core-common/src/main/java/org/apache/kylin/common/util/ByteArray.java
@@ -141,10 +141,10 @@ public class ByteArray implements Comparable<ByteArray>, Serializable {
         if (data == null) {
             return 0;
         } else {
-            if (length <= Bytes.SIZEOF_LONG) {
+            if (length <= Bytes.SIZEOF_LONG && length > 0) {
                 //to avoid hash collision of byte arrays those are converted from nearby integers/longs, 
                 //which is the case for kylin dictionary
-                return Long.valueOf(Bytes.toLong(data, offset, length)).hashCode();
+                return Long.valueOf(BytesUtil.readLong(data, offset, length)).hashCode();
             }
             return Bytes.hashCode(data, offset, length);
         }
