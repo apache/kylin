5705a18090d9d114f6c30bdbb31fe58f1f95b1d4
fengyu <hzfengyu@corp.netease.com>
2016-05-12 17:42:26 +0800

    KYLIN-1685,error happens while execute a sql contains '?' using Statement
    Signed-off-by: fengyu <hzfengyu@corp.netease.com>
    Signed-off-by: lidongsjtu <lidong@apache.org>

--- jdbc/src/main/java/org/apache/kylin/jdbc/KylinResultSet.java
+++ jdbc/src/main/java/org/apache/kylin/jdbc/KylinResultSet.java
@@ -50,6 +50,9 @@ public class KylinResultSet extends AvaticaResultSet {
         String sql = signature.sql;
         List<AvaticaParameter> params = signature.parameters;
         List<Object> paramValues = null;
+        if(statement instanceof KylinStatement) {
+            params = null;
+        }
         if (params != null && params.size() > 0) {
             paramValues = ((KylinPreparedStatement) statement).getParameterValues2();
         }
