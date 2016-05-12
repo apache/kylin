7101f09a5d6e7d14efbf69c5e6e04c911599cae9
fengyu <hzfengyu@corp.netease.com>
2016-05-12 17:42:26 +0800

    KYLIN-1685,error happens while execute a sql contains '?' using Statement
    Signed-off-by: fengyu <hzfengyu@corp.netease.com>
    Signed-off-by: lidongsjtu <lidong@apache.org>

--- jdbc/src/test/java/org/apache/kylin/jdbc/DriverTest.java
+++ jdbc/src/test/java/org/apache/kylin/jdbc/DriverTest.java
@@ -70,6 +70,25 @@ public class DriverTest {
         state.close();
         conn.close();
     }
+    
+    @Test
+    public void testStatementWithQuestionMask() throws SQLException {
+        Driver driver = new DummyDriver();
+
+        Connection conn = driver.connect("jdbc:kylin://test_url/test_db", null);
+        Statement state = conn.createStatement();
+        ResultSet resultSet = state.executeQuery("select * from test_table where url not in ('http://a.b.com/?a=b')");
+        ResultSetMetaData metadata = resultSet.getMetaData();
+        assertEquals(12, metadata.getColumnType(1));
+        assertEquals("varchar", metadata.getColumnTypeName(1));
+        assertEquals(1, metadata.isNullable(1));
+
+        while (resultSet.next()) {
+            assertEquals("foo", resultSet.getString(1));
+            assertEquals("bar", resultSet.getString(2));
+            assertEquals("tool", resultSet.getString(3));
+        }
+    }
 
     @Test
     public void testPreparedStatementWithMockData() throws SQLException {
