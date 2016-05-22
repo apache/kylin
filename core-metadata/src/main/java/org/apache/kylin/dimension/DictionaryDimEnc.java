3adecb0620489e511ecd7a9b49ae49498696056a
Yang Li <liyang@apache.org>
2016-05-22 13:15:09 +0800

    KYLIN-1722 add serialVersionUID in IntegerDimEnc

--- core-metadata/src/main/java/org/apache/kylin/dimension/DictionaryDimEnc.java
+++ core-metadata/src/main/java/org/apache/kylin/dimension/DictionaryDimEnc.java
@@ -30,6 +30,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DictionaryDimEnc extends DimensionEncoding {
+    private static final long serialVersionUID = 1L;
 
     private static final Logger logger = LoggerFactory.getLogger(DictionaryDimEnc.class);
 
