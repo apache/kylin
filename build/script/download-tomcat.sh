e7740c852e5db40e2c37527de2e99d81e3beb928
shaofengshi <shaofengshi@apache.org>
2016-06-08 16:39:17 +0800

    KYLIN-1774 update tomcat version

--- build/script/download-tomcat.sh
+++ build/script/download-tomcat.sh
@@ -27,22 +27,22 @@ if [[ `uname -a` =~ "Darwin" ]]; then
     alias md5cmd="md5 -q"
 fi
 
-if [ ! -f "build/apache-tomcat-7.0.59.tar.gz" ]
+if [ ! -f "build/apache-tomcat-7.0.69.tar.gz" ]
 then
     echo "no binary file found"
-    wget --directory-prefix=build/ http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.59/bin/apache-tomcat-7.0.59.tar.gz || echo "download tomcat failed"
+    wget --directory-prefix=build/ http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.69/bin/apache-tomcat-7.0.69.tar.gz || echo "download tomcat failed"
 else
-    if [ `md5cmd build/apache-tomcat-7.0.59.tar.gz | awk '{print $1}'` != "ec570258976edf9a833cd88fd9220909" ]
+    if [ `md5cmd build/apache-tomcat-7.0.69.tar.gz | awk '{print $1}'` != "10a071e5169a1a8b14ff35a0ad181052" ]
     then
         echo "md5 check failed"
-        rm build/apache-tomcat-7.0.59.tar.gz
-        wget --directory-prefix=build/ http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.59/bin/apache-tomcat-7.0.59.tar.gz || echo "download tomcat failed"
+        rm build/apache-tomcat-7.0.69.tar.gz
+        wget --directory-prefix=build/ http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.69/bin/apache-tomcat-7.0.69.tar.gz || echo "download tomcat failed"
     fi
 fi
 unalias md5cmd
 
-tar -zxvf build/apache-tomcat-7.0.59.tar.gz -C build/
-mv build/apache-tomcat-7.0.59 build/tomcat
+tar -zxvf build/apache-tomcat-7.0.69.tar.gz -C build/
+mv build/apache-tomcat-7.0.69 build/tomcat
 rm -rf build/tomcat/webapps/*
 
 mv build/tomcat/conf/server.xml build/tomcat/conf/server.xml.bak
