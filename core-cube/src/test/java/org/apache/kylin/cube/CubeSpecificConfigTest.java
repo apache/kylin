6eb646d8d1a372d65e235414d9c6d028245053eb
lidongsjtu <lidong@apache.org>
2016-07-22 17:48:49 +0800

    minor, fix ut

--- core-cube/src/test/java/org/apache/kylin/cube/CubeSpecificConfigTest.java
+++ core-cube/src/test/java/org/apache/kylin/cube/CubeSpecificConfigTest.java
@@ -54,7 +54,7 @@ public class CubeSpecificConfigTest extends LocalFileMetadataTestCase {
     }
 
     private void verifyOverride(KylinConfig base, KylinConfig override) {
-        assertEquals("gzip", base.getHbaseDefaultCompressionCodec());
+        assertEquals("", base.getHbaseDefaultCompressionCodec());
         assertEquals("lz4", override.getHbaseDefaultCompressionCodec());
     }
 }
