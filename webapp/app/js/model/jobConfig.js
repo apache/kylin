cce90a0347cff25a2854a617781f225c80873389
Jason <jiatuer@163.com>
2016-08-05 15:11:27 +0800

    KYLIN-1941 show user for slow query

--- webapp/app/js/model/jobConfig.js
+++ webapp/app/js/model/jobConfig.js
@@ -41,6 +41,7 @@ KylinApp.constant('jobConfig', {
   ],
   queryitems: [
   {attr: 'server', name: 'Server'},
+  {attr: 'user', name: 'User'},
   {attr: 'sql', name: 'Sql'},
   {attr: 'adj', name: 'Description'},
   {attr: 'running_seconds', name: 'Running Seconds'},
