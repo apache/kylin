ced72d5a6612bd969f4f88fa454e02b5ec63460b
Yiming Liu <liuyiming.vip@gmail.com>
2016-09-06 00:57:19 +0800

    KYLIN-1962: minor, fix Spring Security read Kylin properties
    Signed-off-by: shaofengshi <shaofengshi@apache.org>

--- tool/src/main/java/org/apache/kylin/tool/DiagnosisInfoCLI.java
+++ tool/src/main/java/org/apache/kylin/tool/DiagnosisInfoCLI.java
@@ -184,7 +184,7 @@ public class DiagnosisInfoCLI extends AbstractInfoExtractor {
                         File[] confFiles = srcConfDir.listFiles();
                         if (confFiles != null) {
                             for (File confFile : confFiles) {
-                                if (!KylinConfig.KYLIN_SECURITY_CONF_PROPERTIES_FILE.equals(confFile.getName())) {
+                                if (!KylinConfig.KYLIN_ACCOUNT_CONF_PROPERTIES_FILE.equals(confFile.getName())) {
                                     FileUtils.copyFileToDirectory(confFile, destConfDir);
                                 }
                             }
