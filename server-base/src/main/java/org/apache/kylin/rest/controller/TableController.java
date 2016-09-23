abc6a744783731b9d1b33166a98c6fcdda5fddb8
Yiming Liu <liuyiming.vip@gmail.com>
2016-09-23 21:59:59 +0800

    KYLIN-2041: fix GET Hive Tables parameter definition
    Signed-off-by: Yang Li <liyang@apache.org>

--- server-base/src/main/java/org/apache/kylin/rest/controller/TableController.java
+++ server-base/src/main/java/org/apache/kylin/rest/controller/TableController.java
@@ -90,7 +90,7 @@ public class TableController extends BasicController {
      */
     @RequestMapping(value = "", method = { RequestMethod.GET })
     @ResponseBody
-    public List<TableDesc> getHiveTables(@RequestParam(value = "ext", required = false) boolean withExt, @RequestParam(value = "project", required = false) String project) {
+    public List<TableDesc> getHiveTables(@RequestParam(value = "ext", required = false) boolean withExt, @RequestParam(value = "project", required = true) String project) {
         long start = System.currentTimeMillis();
         List<TableDesc> tables = null;
         try {
