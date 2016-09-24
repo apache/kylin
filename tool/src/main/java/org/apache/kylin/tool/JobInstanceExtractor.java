818188d484f2a848c84d3b1f79865e04e74dcd22
lidongsjtu <lidong@apache.org>
2016-09-24 10:27:25 +0800

    KYLIN-2045 Fix argument order in JobInstanceExtractor#executeExtract()

--- tool/src/main/java/org/apache/kylin/tool/JobInstanceExtractor.java
+++ tool/src/main/java/org/apache/kylin/tool/JobInstanceExtractor.java
@@ -85,7 +85,7 @@ public class JobInstanceExtractor extends AbstractInfoExtractor {
 
         long endTime = System.currentTimeMillis();
         long startTime = endTime - period * 24 * 3600 * 1000; // time in Millis
-        List<JobInstance> jobInstances = listJobInstances(cube, project, startTime, endTime);
+        List<JobInstance> jobInstances = listJobInstances(project, cube, startTime, endTime);
         logger.info("There are {} jobInstances to extract.", jobInstances.size());
 
         ObjectMapper mapper = new ObjectMapper();
