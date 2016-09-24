25f8ffc0ee16b1702723d5530b22084a608fed9a
shaofengshi <shaofengshi@apache.org>
2016-09-24 14:57:01 +0800

    Revert "Revert "KYLIN-1762 discard job when no stream message""
    This reverts commit da5ba276b972f8b3c0d220252e74ac2ff73298fc.

--- core-job/src/main/java/org/apache/kylin/job/execution/DefaultChainedExecutable.java
+++ core-job/src/main/java/org/apache/kylin/job/execution/DefaultChainedExecutable.java
@@ -88,6 +88,7 @@ public class DefaultChainedExecutable extends AbstractExecutable implements Chai
             boolean allSucceed = true;
             boolean hasError = false;
             boolean hasRunning = false;
+            boolean hasDiscarded = false;
             for (Executable task : jobs) {
                 final ExecutableState status = task.getStatus();
                 if (status == ExecutableState.ERROR) {
@@ -98,6 +99,9 @@ public class DefaultChainedExecutable extends AbstractExecutable implements Chai
                 }
                 if (status == ExecutableState.RUNNING) {
                     hasRunning = true;
+                }
+                if (status == ExecutableState.DISCARDED) {
+                    hasDiscarded = true;
                 }
             }
             if (allSucceed) {
@@ -110,6 +114,8 @@ public class DefaultChainedExecutable extends AbstractExecutable implements Chai
                 notifyUserStatusChange(executableContext, ExecutableState.ERROR);
             } else if (hasRunning) {
                 jobService.updateJobOutput(getId(), ExecutableState.RUNNING, null, null);
+            } else if (hasDiscarded) {
+                jobService.updateJobOutput(getId(), ExecutableState.DISCARDED, null, null);
             } else {
                 jobService.updateJobOutput(getId(), ExecutableState.READY, null, null);
             }
