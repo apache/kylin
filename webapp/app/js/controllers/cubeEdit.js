4ca3cfea6aec6e1302c48072085bf1e473f6383b
kangkaisen <kangkaisen@live.com>
2016-10-20 13:31:43 +0800

    KYLIN-1904-WEB-Global-Dictionary bug fix
    Signed-off-by: Jason <jiatuer@163.com>

--- webapp/app/js/controllers/cubeEdit.js
+++ webapp/app/js/controllers/cubeEdit.js
@@ -206,8 +206,9 @@ KylinApp.controller('CubeEditCtrl', function ($scope, $q, $routeParams, $locatio
       }
 
     });
-    angular.forEach($scope.cubeMetaFrame.measure,function(measure){
-         if(measure.function.parameter.type==column){
+
+    angular.forEach($scope.cubeMetaFrame.measures,function(measure){
+         if(measure.function.parameter.type == "column"){
            me_columns.push(measure.function.parameter.value);
          }
     });
