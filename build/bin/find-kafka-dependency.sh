9e8d13482a9ecaa4bfd08d7077a04f34cea19286
Billy Liu <billyliu@apache.org>
2016-12-09 15:23:12 +0800

    minor update find kafka dependency script

--- build/bin/find-kafka-dependency.sh
+++ build/bin/find-kafka-dependency.sh
@@ -21,16 +21,16 @@ source $(cd -P -- "$(dirname -- "$0")" && pwd -P)/header.sh
 
 kafka_home=
 
+echo Retrieving kafka dependency...
 if [ -n "$KAFKA_HOME" ]
 then
-    echo "KAFKA_HOME is set to: $KAFKA_HOME, use it to locate kafka dependencies."
+    verbose "KAFKA_HOME is set to: $KAFKA_HOME, use it to locate kafka dependencies."
     kafka_home=$KAFKA_HOME
 fi
 
 if [ -z "$KAFKA_HOME" ]
 then
-    echo "Couldn't find kafka home. Please set KAFKA_HOME to the path which contains kafka dependencies."
-    exit 1
+    quit "Couldn't find kafka home. Please set KAFKA_HOME to the path which contains kafka dependencies."
 fi
 
 # works for kafka 9+
@@ -41,13 +41,12 @@ then
     kafka_dependency=`find -L $kafka_home -name 'kafka_[a-z0-9A-Z\.-]*.jar' ! -name '*doc*' ! -name '*test*' ! -name '*sources*' ''-printf '%p:' | sed 's/:$//'`
     if [ -z "$kafka_dependency" ]
     then
-        echo "kafka client lib not found"
-        exit 1
+        quit "kafka client lib not found"
     else
-        echo "kafka dependency: $kafka_dependency"
+        verbose "kafka dependency: $kafka_dependency"
         export kafka_dependency
     fi
 else
-    echo "kafka dependency: $kafka_dependency"
+    verbose "kafka dependency: $kafka_dependency"
     export kafka_dependency
 fi
