64c3c61898d10f05c4350609517b2096c4171167
Li Yang <liyang@apache.org>
2016-12-29 13:44:40 +0800

    enhance LookupTable

--- core-dictionary/src/main/java/org/apache/kylin/dict/lookup/LookupTable.java
+++ core-dictionary/src/main/java/org/apache/kylin/dict/lookup/LookupTable.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -46,7 +47,7 @@ abstract public class LookupTable<T> {
     protected TableDesc tableDesc;
     protected String[] keyColumns;
     protected ReadableTable table;
-    protected ConcurrentHashMap<Array<T>, T[]> data;
+    protected Map<Array<T>, T[]> data;
 
     public LookupTable(TableDesc tableDesc, String[] keyColumns, ReadableTable table) throws IOException {
         this.tableDesc = tableDesc;
