c9ab289c09bf95b97d67315f5d7e0b9d634aa48b
Yang Li <liyang@apache.org>
2017-01-11 21:11:47 +0800

    minor, restore @Ignore to ITKylinQueryTest

--- kylin-it/src/test/java/org/apache/kylin/query/ITKylinQueryTest.java
+++ kylin-it/src/test/java/org/apache/kylin/query/ITKylinQueryTest.java
@@ -47,7 +47,7 @@ import org.junit.rules.ExpectedException;
 
 import com.google.common.collect.Maps;
 
-//@Ignore("KylinQueryTest is contained by ITCombinationTest")
+@Ignore("KylinQueryTest is contained by ITCombinationTest")
 public class ITKylinQueryTest extends KylinTestBase {
 
     @Rule
