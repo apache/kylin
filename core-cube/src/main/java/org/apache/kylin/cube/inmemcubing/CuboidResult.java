40a593541055cde65b16fc551345171b22456ab1
david.wang <wangyuxue@users.noreply.github.com>
2017-03-09 14:53:13 +0800

    changed unsafe code to safety
    changed unsafe code to safety

--- core-cube/src/main/java/org/apache/kylin/cube/inmemcubing/CuboidResult.java
+++ core-cube/src/main/java/org/apache/kylin/cube/inmemcubing/CuboidResult.java
@@ -24,11 +24,11 @@ import org.apache.kylin.gridtable.GridTable;
  */
 public class CuboidResult {
 
-    public long cuboidId;
-    public GridTable table;
-    public int nRows;
-    public long timeSpent;
-    public int aggrCacheMB;
+    public final  long cuboidId;
+    public final  GridTable table;
+    public final int nRows;
+    public final long timeSpent;
+    public final int aggrCacheMB;
 
     public CuboidResult(long cuboidId, GridTable table, int nRows, long timeSpent, int aggrCacheMB) {
         this.cuboidId = cuboidId;
