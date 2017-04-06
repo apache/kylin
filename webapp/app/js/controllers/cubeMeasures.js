023fa2387f9685efc64cc21a0fcc7b2e4457d9e7
chenzhx <346839943@qq.com>
2017-04-06 15:34:14 +0800

    KYLIN 2513 SUM measure return type should not overflow

--- webapp/app/js/controllers/cubeMeasures.js
+++ webapp/app/js/controllers/cubeMeasures.js
@@ -370,10 +370,10 @@ KylinApp.controller('CubeMeasuresCtrl', function ($scope, $modal,MetaModel,cubes
           if(colType==="smallint"||colType==="int"||colType==="bigint"||colType==="integer"){
             $scope.newMeasure.function.returntype= 'bigint';
           }else{
-            if(colType.indexOf('decimal')!=-1){
-              $scope.newMeasure.function.returntype= colType;
+           if(colType.indexOf('decimal')!=-1||colType==="double"||colType==="float"){
+              $scope.newMeasure.function.returntype= 'decimal(19,4)';
             }else{
-              $scope.newMeasure.function.returntype= 'decimal';
+              $scope.newMeasure.function.returntype= 'decimal(14,0)';
             }
           }
           break;
