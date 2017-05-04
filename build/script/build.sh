8a4b6b9611a25c8e0f8bf2ec99244a250d9dd91b
Hongbin Ma <mahongbin@apache.org>
2017-05-04 16:58:09 +0800

    minor, fix typo

--- build/script/build.sh
+++ build/script/build.sh
@@ -22,7 +22,7 @@ echo "package back-end"
 dir=$(dirname ${0})
 cd ${dir}/../..
 
-mvn clean intall -DskipTests $@ || { exit 1; }
+mvn clean install -DskipTests $@ || { exit 1; }
 
 #package webapp
 echo 'package front-end'
