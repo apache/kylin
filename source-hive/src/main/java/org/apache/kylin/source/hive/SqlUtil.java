5c7c757dd6a2c20ca92a954fce09266fbce55a88
chengyi <phillipchengyi@gmail.com>
2017-06-02 10:50:00 +0000

    added file header license info to pass build

--- source-hive/src/main/java/org/apache/kylin/source/hive/SqlUtil.java
+++ source-hive/src/main/java/org/apache/kylin/source/hive/SqlUtil.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 package org.apache.kylin.source.hive;
 
 import java.sql.Connection;
@@ -10,12 +28,9 @@ import javax.sql.DataSource;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
-
 public class SqlUtil {
     private static final Logger logger = LoggerFactory.getLogger(SqlUtil.class);
-	
-	
-	
+
 	public static void closeResources(Connection con, Statement statement){
 		try{
 			if (statement!=null && !statement.isClosed()){
@@ -58,7 +73,6 @@ public class SqlUtil {
 			closeResources(null, statement);
 		}
 	}
-	
 	
 	public static int tryTimes=10;
 	public static Connection getConnection(DBConnConf dbconf){
