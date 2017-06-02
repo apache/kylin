28d82763563de116dc18a0cf1d9963a8243c45ff
chengyi <phillipchengyi@gmail.com>
2017-06-02 22:16:53 +0000

    remove unused import

--- assembly/src/test/java/org/apache/kylin/job/DeployUtil.java
+++ assembly/src/test/java/org/apache/kylin/job/DeployUtil.java
@@ -26,7 +26,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
