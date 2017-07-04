7ac68400c1420dbea23b74ad56575e879c26cafb
luguosheng1314 <550175214@qq.com>
2017-07-04 16:16:08 +0800

    remove links of system and remove useless debug log

--- webapp/app/js/controllers/modelDataModel.js
+++ webapp/app/js/controllers/modelDataModel.js
@@ -218,7 +218,6 @@ KylinApp.controller('ModelDataModelCtrl', function ($location,$scope, $modal,cub
             lookupList.splice(lookupList.indexOf(lookup), 1);
             $scope.aliasName.splice($scope.aliasName.indexOf(lookup.alias),1);
         }
-        console.log($scope.aliasName);
     };
 
     $scope.changeKey = function(index){
