af4b5a552fe540c0e77389a6c1ecaa23dabb5375
Roger Shi <rogershijicheng@hotmail.com>
2017-09-04 18:37:29 +0800

    minor, fix CI

--- kylin-it/src/test/resources/query/sql_union/query01.sql
+++ kylin-it/src/test/resources/query/sql_union/query01.sql
@@ -1,13 +1,13 @@
 -- union subquery under join
-select sum(TEST_A.PRICE) as ITEM_CNT
+select count(*) as cnt
 FROM TEST_KYLIN_FACT as TEST_A
 join (
-    select sum(TEST_C.PRICE), TEST_C.TRANS_ID
+    select sum(TEST_C.PRICE), TEST_C.ORDER_ID
     from (
-        select * from TEST_KYLIN_FACT where CAL_DT < DATE '2012-06-01'
+        select * from TEST_KYLIN_FACT where CAL_DT < DATE '2012-08-01'
         union
         select * from TEST_KYLIN_FACT where CAL_DT > DATE '2013-06-01'
-    ) TEST_C group by TRANS_ID
+    ) TEST_C group by ORDER_ID
 ) TEST_B
-on TEST_A.TRANS_ID = TEST_B.TRANS_ID
+on TEST_A.ORDER_ID = TEST_B.ORDER_ID
 group by TEST_A.SELLER_ID
--- kylin-it/src/test/resources/query/sql_union/query02.sql
+++ kylin-it/src/test/resources/query/sql_union/query02.sql
@@ -1,10 +1,10 @@
 -- union subquery under join
-select sum(TEST_A.PRICE) as ITEM_CNT
+select count(*) as cnt
 FROM TEST_KYLIN_FACT as TEST_A
 join (
-    select * from TEST_KYLIN_FACT where CAL_DT < DATE '2012-06-01'
+    select * from TEST_KYLIN_FACT where CAL_DT < DATE '2012-08-01'
     union
     select * from TEST_KYLIN_FACT where CAL_DT > DATE '2013-06-01'
 ) TEST_B
-on TEST_A.TRANS_ID = TEST_B.TRANS_ID
+on TEST_A.ORDER_ID = TEST_B.ORDER_ID
 group by TEST_A.SELLER_ID
