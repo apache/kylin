084a6fb0bcd3d57737d18e097c8699a1c929d3f9
shaofengshi <shaofengshi@apache.org>
2017-09-09 22:25:14 +0800

    Revert "add retry strategy in cache sync"
    This reverts commit 7da2cf8a1818ab78c3df679c95b44a332c4d4bd0.

--- core-metadata/src/main/java/org/apache/kylin/metadata/cachesync/Broadcaster.java
+++ core-metadata/src/main/java/org/apache/kylin/metadata/cachesync/Broadcaster.java
@@ -111,7 +111,6 @@ public class Broadcaster {
 
     private Broadcaster(final KylinConfig config) {
         this.config = config;
-        final int retryLimitTimes = config.getCacheSyncRetrys();
 
         final String[] nodes = config.getRestServers();
         if (nodes == null || nodes.length < 1) {
@@ -129,12 +128,6 @@ public class Broadcaster {
                 while (true) {
                     try {
                         final BroadcastEvent broadcastEvent = broadcastEvents.takeFirst();
-                        broadcastEvent.setRetryTime(broadcastEvent.getRetryTime() + 1);
-                        if (broadcastEvent.getRetryTime() > retryLimitTimes) {
-                            logger.info("broadcastEvent retry up to limit times, broadcastEvent:{}", broadcastEvent);
-                            continue;
-                        }
-
                         String[] restServers = config.getRestServers();
                         logger.debug("Servers in the cluster: " + Arrays.toString(restServers));
                         for (final String node : restServers) {
@@ -152,16 +145,7 @@ public class Broadcaster {
                                         restClientMap.get(node).wipeCache(broadcastEvent.getEntity(),
                                                 broadcastEvent.getEvent(), broadcastEvent.getCacheKey());
                                     } catch (IOException e) {
-                                        logger.warn("Thread failed during wipe cache at {}, error msg: {}",
-                                                broadcastEvent, e);
-                                        // when sync failed, put back to queue
-                                        try {
-                                            broadcastEvents.putLast(broadcastEvent);
-                                        } catch (InterruptedException ex) {
-                                            logger.warn(
-                                                    "error reentry failed broadcastEvent to queue, broacastEvent:{}, error: {} ",
-                                                    broadcastEvent, ex);
-                                        }
+                                        logger.warn("Thread failed during wipe cache at " + broadcastEvent, e);
                                     }
                                 }
                             });
@@ -313,7 +297,6 @@ public class Broadcaster {
     }
 
     public static class BroadcastEvent {
-        private int retryTime;
         private String entity;
         private String event;
         private String cacheKey;
@@ -323,14 +306,6 @@ public class Broadcaster {
             this.entity = entity;
             this.event = event;
             this.cacheKey = cacheKey;
-        }
-
-        public int getRetryTime() {
-            return retryTime;
-        }
-
-        public void setRetryTime(int retryTime) {
-            this.retryTime = retryTime;
         }
 
         public String getEntity() {
