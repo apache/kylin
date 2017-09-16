fd4a178cdf5f7c81463938f6909fafb41f54fe9c
liveandevil <1361605435@qq.com>
2017-09-16 19:30:22 +0800

    KYLIN-2871, fix ineffective null check

--- core-metadata/src/main/java/org/apache/kylin/metadata/model/SegmentRange.java
+++ core-metadata/src/main/java/org/apache/kylin/metadata/model/SegmentRange.java
@@ -147,6 +147,8 @@ public class SegmentRange<T extends Comparable> implements Serializable {
             return new Comparator<Endpoint>() {
                 @Override
                 public int compare(Endpoint a, Endpoint b) {
+                    if (a == null || b == null)
+                        throw new IllegalStateException();
                     if (a.isMin) {
                         return b.isMin ? 0 : -1;
                     } else if (b.isMin) {
@@ -156,9 +158,6 @@ public class SegmentRange<T extends Comparable> implements Serializable {
                     } else if (b.isMax) {
                         return a.isMax ? 0 : -1;
                     } else {
-                        if (a == null || b == null)
-                            throw new IllegalStateException();
-
                         return valueComparator.compare(a.v, b.v);
                     }
                 }
