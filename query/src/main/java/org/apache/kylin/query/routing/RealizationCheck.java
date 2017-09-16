d04819d7436171f2570d16ddbec541a1686b68c1
liveandevil <1361605435@qq.com>
2017-09-16 21:39:46 +0800

    minor, refine query error message

--- query/src/main/java/org/apache/kylin/query/routing/RealizationCheck.java
+++ query/src/main/java/org/apache/kylin/query/routing/RealizationCheck.java
@@ -107,7 +107,7 @@ public class RealizationCheck {
         // notFoundColumns = notFoundDimensions + notFoundMeasures;
         private Collection<TblColRef> notFoundColumns;
         private Collection<TblColRef> notFoundDimensions;
-        private Collection<TblColRef> notFoundMeasures;
+        private Collection<FunctionDesc> notFoundMeasures;
         private Collection<TblColRef> unmatchedDimensions;
         private Collection<FunctionDesc> unmatchedAggregations;
         private Collection<OLAPTableScan> notFoundTables;
@@ -169,7 +169,7 @@ public class RealizationCheck {
             return incapableReason;
         }
 
-        public static IncapableReason notContainAllMeasures(Collection<TblColRef> notFoundMeasures) {
+        public static IncapableReason notContainAllMeasures(Collection<FunctionDesc> notFoundMeasures) {
             IncapableReason incapableReason = new IncapableReason();
             incapableReason.setIncapableType(IncapableType.CUBE_NOT_CONTAIN_ALL_MEASURE);
             incapableReason.setNotFoundMeasures(notFoundMeasures);
@@ -207,7 +207,7 @@ public class RealizationCheck {
             this.notFoundDimensions = notFoundDimensions;
         }
 
-        public void setNotFoundMeasures(Collection<TblColRef> notFoundMeasures) {
+        public void setNotFoundMeasures(Collection<FunctionDesc> notFoundMeasures) {
             this.notFoundMeasures = notFoundMeasures;
         }
 
@@ -215,7 +215,7 @@ public class RealizationCheck {
             return notFoundDimensions;
         }
 
-        public Collection<TblColRef> getNotFoundMeasures() {
+        public Collection<FunctionDesc> getNotFoundMeasures() {
             return notFoundMeasures;
         }
 
