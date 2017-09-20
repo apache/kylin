af8d8855572dc28b16317f5864a9c7aca989001e
Li Yang <liyang@apache.org>
2017-09-20 21:27:47 +0800

    fix release smote test

--- build/smoke-test/testQuery.py
+++ build/smoke-test/testQuery.py
@@ -51,7 +51,7 @@ class testQuery(unittest.TestCase):
                     sql_statement += sql_statement_line.strip() + ' '
             payload = "{\"sql\": \"" + sql_statement.strip() + "\", \"offset\": 0, \"limit\": \"50000\", \"acceptPartial\":false, \"project\":\"learn_kylin\"}"
             print 'Test Query #' + str(index) + ': \n' + sql_statement
-            response = requests.request("POST", query_url, data=payload, headers=headers)
+            response = requests.request("POST", query_url, data=payload, headers=testQuery.headers)
 
             self.assertEqual(response.status_code, 200, 'Query failed.')
             actual_result = json.loads(response.text)
