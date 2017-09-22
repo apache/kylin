a09cc4bc80327a201013192494421642dd0ebf08
nichunen <chunen.ni@kyligence.io>
2017-09-22 23:02:07 +0800

    Fix the bug of jobs with  upper case cube names can not be searched

--- server-base/src/main/java/org/apache/kylin/rest/service/JobService.java
+++ server-base/src/main/java/org/apache/kylin/rest/service/JobService.java
@@ -438,7 +438,7 @@ public class JobService extends BasicService implements InitializingBean {
                             if (executableCubeName == null)
                                 return true;
                             if (nameExactMatch)
-                                return executableCubeName.toLowerCase().equals(cubeName);
+                                return executableCubeName.equalsIgnoreCase(cubeName);
                             else
                                 return executableCubeName.toLowerCase().contains(cubeName.toLowerCase());
                         } else {
@@ -456,7 +456,7 @@ public class JobService extends BasicService implements InitializingBean {
                         if (null == projectName || null == getProjectManager().getProject(projectName)) {
                             return true;
                         } else {
-                            return projectName.equals(executable.getProjectName());
+                            return projectName.equalsIgnoreCase(executable.getProjectName());
                         }
                     }
                 }, new Predicate<CubingJob>() {
@@ -487,9 +487,9 @@ public class JobService extends BasicService implements InitializingBean {
                         }
 
                         if (nameExactMatch) {
-                            return cubeJob.getName().toLowerCase().equals(jobName);
+                            return cubeJob.getName().equalsIgnoreCase(jobName);
                         } else {
-                            return cubeJob.getName().toLowerCase().contains(jobName);
+                            return cubeJob.getName().toLowerCase().contains(jobName.toLowerCase());
                         }
                     }
                 })));
