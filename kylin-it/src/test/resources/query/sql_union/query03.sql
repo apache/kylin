460de4edeee3866a25e8056d2630d38e58a37d8e
Roger Shi <rogershijicheng@hotmail.com>
2017-10-22 14:45:27 +0800

    minor, fix union query for ci

--- kylin-it/src/test/resources/query/sql_union/query03.sql
+++ kylin-it/src/test/resources/query/sql_union/query03.sql
@@ -1,1 +1,1 @@
-select count(*) as cnt from TEST_KYLIN_FACT where TRANS_ID < 1000 union select count(*) as cnt from TEST_KYLIN_FACT where TRANS_ID > 9000
+select count(*) as cnt from TEST_KYLIN_FACT where ORDER_ID < 100 union select count(*) as cnt from TEST_KYLIN_FACT where ORDER_ID > 200
