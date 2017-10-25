3d83f164a705357ed5cf9540c0fa212fa732b8ef
Ma,Gang <ganma@ebay.com>
2017-10-25 13:11:47 +0800

    KYLIN-2965 RealizationChooser cost calculation logic different with CubeInstance
    Signed-off-by: Li Yang <liyang@apache.org>

--- query/src/main/java/org/apache/kylin/query/routing/RealizationChooser.java
+++ query/src/main/java/org/apache/kylin/query/routing/RealizationChooser.java
@@ -36,6 +36,7 @@ import org.apache.kylin.metadata.model.TableRef;
 import org.apache.kylin.metadata.model.TblColRef;
 import org.apache.kylin.metadata.project.ProjectManager;
 import org.apache.kylin.metadata.realization.IRealization;
+import org.apache.kylin.metadata.realization.RealizationType;
 import org.apache.kylin.query.relnode.OLAPContext;
 import org.apache.kylin.query.relnode.OLAPTableScan;
 import org.apache.kylin.query.routing.rules.RemoveBlackoutRealizationsRule;
@@ -248,7 +249,13 @@ public class RealizationChooser {
             this.priority = Candidate.PRIORITIES.get(real.getType());
 
             // ref CubeInstance.getCost()
-            int c = real.getAllDimensions().size() * CubeInstance.COST_WEIGHT_DIMENSION
+            int countedDimensionNum;
+            if (RealizationType.CUBE == real.getType()) {
+                countedDimensionNum = ((CubeInstance) real).getRowKeyColumnCount();
+            } else {
+                countedDimensionNum = real.getAllDimensions().size();
+            }
+            int c = countedDimensionNum * CubeInstance.COST_WEIGHT_DIMENSION
                     + real.getMeasures().size() * CubeInstance.COST_WEIGHT_MEASURE;
             for (JoinTableDesc join : real.getModel().getJoinTables()) {
                 if (join.getJoin().isInnerJoin())
