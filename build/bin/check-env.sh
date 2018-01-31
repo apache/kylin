c771833434523aecef2e6f33d9055a92a2c6f7cb
xingpeng1 <xingpeng1@zte.com.cn>
2018-01-31 18:16:35 +0800

    KYLIN-3216
    Signed-off-by: shaofengshi <shaofengshi@apache.org>

--- build/bin/check-env.sh
+++ build/bin/check-env.sh
@@ -62,8 +62,22 @@ then
     quit "Failed to create $WORKING_DIR. Please make sure the user has right to access $WORKING_DIR"
 fi
 
-hadoop ${hadoop_conf_param} fs -mkdir -p $WORKING_DIR/spark-history
-if [ $? != 0 ]
+SPARK_EVENTLOG_DIR=`bash $KYLIN_HOME/bin/get-properties.sh kylin.engine.spark-conf.spark.eventLog.dir`
+if [ -n "$SPARK_EVENTLOG_DIR" ]
 then
-    quit "Failed to create $WORKING_DIR/spark-history. Please make sure the user has right to access $WORKING_DIR"
+    hadoop ${hadoop_conf_param} fs -mkdir -p $SPARK_EVENTLOG_DIR
+    if [ $? != 0 ]
+    then
+        quit "Failed to create $SPARK_EVENTLOG_DIR. Please make sure the user has right to access $SPARK_EVENTLOG_DIR"
+    fi
+fi
+
+SPARK_HISTORYLOG_DIR=`bash $KYLIN_HOME/bin/get-properties.sh kylin.engine.spark-conf.spark.history.fs.logDirectory`
+if [ -n "$SPARK_HISTORYLOG_DIR" ]
+then
+    hadoop ${hadoop_conf_param} fs -mkdir -p $SPARK_HISTORYLOG_DIR
+    if [ $? != 0 ]
+    then
+        quit "Failed to create $SPARK_HISTORYLOG_DIR. Please make sure the user has right to access $SPARK_HISTORYLOG_DIR"
+    fi
 fi
