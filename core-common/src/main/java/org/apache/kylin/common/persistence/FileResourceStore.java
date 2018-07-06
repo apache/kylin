59354acced7fdb35e0325fb7a8ea2e8c862f658c
yangcao <ycsjsibada@aliyun.com>
2018-07-06 16:39:19 +0800

    KYLIN-3428 too large dict file cause OOM
    Signed-off-by: shaofengshi <shaofengshi@apache.org>

--- core-common/src/main/java/org/apache/kylin/common/persistence/FileResourceStore.java
+++ core-common/src/main/java/org/apache/kylin/common/persistence/FileResourceStore.java
@@ -19,7 +19,6 @@
 package org.apache.kylin.common.persistence;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -128,17 +127,10 @@ public class FileResourceStore extends ResourceStore {
 
             File f = file(resPath);
             if (f.exists() && f.isFile()) {
-                if (f.length() == 0) {
-                    logger.warn("Zero length file: " + f.getAbsolutePath());
-                }
+                logger.info("file length = " + f.length() + " , " + f.getAbsolutePath());
 
                 FileInputStream resource = new FileInputStream(f);
-                ByteArrayOutputStream baos = new ByteArrayOutputStream(1000);
-                IOUtils.copy(resource, baos);
-                IOUtils.closeQuietly(resource);
-                byte[] data = baos.toByteArray();
-
-                return new RawResource(new ByteArrayInputStream(data), f.lastModified());
+                return new RawResource(resource, f.lastModified());
             } else {
                 return null;
             }
