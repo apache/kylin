9eec8c9a3d33254bc20ea5de69078bc324c1c13c
Zhong <nju_yaho@apache.org>
2018-08-20 16:00:13 +0800

    KYLIN-3503 fix missing java.util.logging.config.file when starting kylin instance

--- build/bin/kylin.sh
+++ build/bin/kylin.sh
@@ -106,6 +106,7 @@ then
 
     # KYLIN_EXTRA_START_OPTS is for customized settings, checkout bin/setenv.sh
     hbase ${KYLIN_EXTRA_START_OPTS} \
+    -Djava.util.logging.config.file=${tomcat_root}/conf/logging.properties \
     -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager \
     -Dlog4j.configuration=file:${KYLIN_HOME}/conf/kylin-server-log4j.properties \
     -Dorg.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH=true \
