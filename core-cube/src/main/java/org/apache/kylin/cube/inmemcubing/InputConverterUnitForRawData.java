e9ccd0711d46b5db1d2b48b89f2d1be67f949632
Zhong <nju_yaho@apache.org>
2018-08-29 17:31:27 +0800

    KYLIN-3520 Deal with NULL values of measures for inmem cubing

--- core-cube/src/main/java/org/apache/kylin/cube/inmemcubing/InputConverterUnitForRawData.java
+++ core-cube/src/main/java/org/apache/kylin/cube/inmemcubing/InputConverterUnitForRawData.java
@@ -17,24 +17,19 @@
 */
 package org.apache.kylin.cube.inmemcubing;
 
-import java.util.List;
 import java.util.Map;
 
-import org.apache.kylin.common.util.Bytes;
 import org.apache.kylin.common.util.Dictionary;
 import org.apache.kylin.cube.model.CubeDesc;
 import org.apache.kylin.cube.model.CubeJoinedFlatTableEnrich;
+import org.apache.kylin.cube.util.KeyValueBuilder;
 import org.apache.kylin.gridtable.GTRecord;
 import org.apache.kylin.measure.MeasureIngester;
-import org.apache.kylin.metadata.model.FunctionDesc;
 import org.apache.kylin.metadata.model.IJoinedFlatTableDesc;
 import org.apache.kylin.metadata.model.MeasureDesc;
-import org.apache.kylin.metadata.model.ParameterDesc;
 import org.apache.kylin.metadata.model.TblColRef;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.google.common.collect.Lists;
 
 /**
  */
@@ -51,7 +46,7 @@ public class InputConverterUnitForRawData implements InputConverterUnit<String[]
     private final MeasureIngester<?>[] measureIngesters;
     private final int measureCount;
     private final Map<TblColRef, Dictionary<String>> dictionaryMap;
-    protected List<byte[]> nullBytes;
+    private final KeyValueBuilder kvBuilder;
 
     public InputConverterUnitForRawData(CubeDesc cubeDesc, IJoinedFlatTableDesc flatDesc,
             Map<TblColRef, Dictionary<String>> dictionaryMap) {
@@ -60,12 +55,12 @@ public class InputConverterUnitForRawData implements InputConverterUnit<String[]
         this.measureDescs = cubeDesc.getMeasures().toArray(new MeasureDesc[measureCount]);
         this.measureIngesters = MeasureIngester.create(cubeDesc.getMeasures());
         this.dictionaryMap = dictionaryMap;
-        initNullBytes(cubeDesc);
+        this.kvBuilder = new KeyValueBuilder(this.flatDesc);
     }
 
     @Override
     public final void convert(String[] row, GTRecord record) {
-        Object[] dimensions = buildKey(row);
+        Object[] dimensions = kvBuilder.buildKey(row);
         Object[] metricsValues = buildValue(row);
         Object[] recordValues = new Object[dimensions.length + metricsValues.length];
         System.arraycopy(dimensions, 0, recordValues, 0, dimensions.length);
@@ -93,20 +88,6 @@ public class InputConverterUnitForRawData implements InputConverterUnit<String[]
         return CUT_ROW;
     }
 
-    private Object[] buildKey(String[] row) {
-        int keySize = flatDesc.getRowKeyColumnIndexes().length;
-        Object[] key = new Object[keySize];
-
-        for (int i = 0; i < keySize; i++) {
-            key[i] = row[flatDesc.getRowKeyColumnIndexes()[i]];
-            if (key[i] != null && isNull(Bytes.toBytes((String) key[i]))) {
-                key[i] = null;
-            }
-        }
-
-        return key;
-    }
-
     @Override
     public boolean ifChange() {
         return true;
@@ -115,53 +96,10 @@ public class InputConverterUnitForRawData implements InputConverterUnit<String[]
     private Object[] buildValue(String[] row) {
         Object[] values = new Object[measureCount];
         for (int i = 0; i < measureCount; i++) {
-            values[i] = buildValueOf(i, row);
+            String[] colValues = kvBuilder.buildValueOf(i, row);
+            MeasureDesc measure = measureDescs[i];
+            values[i] = measureIngesters[i].valueOf(colValues, measure, dictionaryMap);
         }
         return values;
     }
-
-    private Object buildValueOf(int idxOfMeasure, String[] row) {
-        MeasureDesc measure = measureDescs[idxOfMeasure];
-        FunctionDesc function = measure.getFunction();
-        int[] colIdxOnFlatTable = flatDesc.getMeasureColumnIndexes()[idxOfMeasure];
-
-        int paramCount = function.getParameterCount();
-        String[] inputToMeasure = new String[paramCount];
-
-        // pick up parameter values
-        ParameterDesc param = function.getParameter();
-        int paramColIdx = 0; // index among parameters of column type
-        for (int i = 0; i < paramCount; i++, param = param.getNextParameter()) {
-            String value;
-            if (function.isCount()) {
-                value = "1";
-            } else if (param.isColumnType()) {
-                value = row[colIdxOnFlatTable[paramColIdx++]];
-            } else {
-                value = param.getValue();
-            }
-            inputToMeasure[i] = value;
-        }
-
-        return measureIngesters[idxOfMeasure].valueOf(inputToMeasure, measure, dictionaryMap);
-    }
-
-    private void initNullBytes(CubeDesc cubeDesc) {
-        nullBytes = Lists.newArrayList();
-        String[] nullStrings = cubeDesc.getNullStrings();
-        if (nullStrings != null) {
-            for (String s : nullStrings) {
-                nullBytes.add(Bytes.toBytes(s));
-            }
-        }
-    }
-
-    private boolean isNull(byte[] v) {
-        for (byte[] nullByte : nullBytes) {
-            if (Bytes.equals(v, nullByte))
-                return true;
-        }
-        return false;
-    }
-
 }
