f15ae54b10ef7b9f22f605d61618d16038512113
chao long <wayne.l@qq.com>
2018-09-10 16:00:46 +0800

    KYLIN-3539 Hybrid segment overlap check

--- tool/src/test/java/org/apache/kylin/tool/HybridCubeCLITest.java
+++ tool/src/test/java/org/apache/kylin/tool/HybridCubeCLITest.java
@@ -22,6 +22,9 @@ import java.io.IOException;
 
 import org.apache.kylin.common.KylinConfig;
 import org.apache.kylin.common.util.LocalFileMetadataTestCase;
+import org.apache.kylin.cube.CubeInstance;
+import org.apache.kylin.cube.CubeManager;
+import org.apache.kylin.metadata.model.SegmentRange;
 import org.apache.kylin.metadata.project.ProjectManager;
 import org.apache.kylin.metadata.realization.RealizationType;
 import org.apache.kylin.storage.hybrid.HybridInstance;
@@ -30,11 +33,16 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class HybridCubeCLITest extends LocalFileMetadataTestCase {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void setUp() throws Exception {
@@ -87,6 +95,46 @@ public class HybridCubeCLITest extends LocalFileMetadataTestCase {
         HybridInstance hybridInstance = hybridManager.getHybridInstance("ssb_hybrid");
         Assert.assertNull(hybridInstance);
         Assert.assertEquals(0, ProjectManager.getInstance(KylinConfig.getInstanceFromEnv()).findProjects(RealizationType.HYBRID, "ssb_hybrid").size());
+    }
+
+    @Test
+    public void testSegmentOverlap() throws IOException {
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("Segments has overlap");
+
+        HybridManager hybridManager = HybridManager.getInstance(KylinConfig.getInstanceFromEnv());
+        Assert.assertNull(hybridManager.getHybridInstance("ssb_hybrid"));
+        HybridCubeCLI.main(new String[] { "-name", "ssb_hybrid", "-project", "default", "-model", "ssb", "-cubes", "ssb_cube1,ssb_cube2", "-action", "create" });
+
+        CubeManager cubeManager = CubeManager.getInstance(KylinConfig.getInstanceFromEnv());
+        CubeInstance cube1 = cubeManager.getCube("ssb_cube1");
+        CubeInstance cube2 = cubeManager.getCube("ssb_cube2");
+
+        // 2012-01-01,2012-01-03
+        cubeManager.appendSegment(cube1, new SegmentRange.TSRange(1325376000000L, 1325548800000L));
+        // 2012-01-02,2012-01-04
+        cubeManager.appendSegment(cube2, new SegmentRange.TSRange(1325462400000L, 1325635200000L));
+
+        HybridCubeCLI.main(new String[] { "-name", "ssb_hybrid", "-project", "default", "-model", "ssb", "-cubes", "ssb_cube1,ssb_cube2", "-action", "update" });
+    }
+
+    @Test
+    public void testSegmentNotOverlap() throws IOException {
+        HybridManager hybridManager = HybridManager.getInstance(KylinConfig.getInstanceFromEnv());
+        Assert.assertNull(hybridManager.getHybridInstance("ssb_hybrid"));
+        HybridCubeCLI.main(new String[] { "-name", "ssb_hybrid", "-project", "default", "-model", "ssb", "-cubes", "ssb_cube1,ssb_cube2", "-action", "create" });
+
+        CubeManager cubeManager = CubeManager.getInstance(KylinConfig.getInstanceFromEnv());
+        CubeInstance cube1 = cubeManager.getCube("ssb_cube1");
+        CubeInstance cube2 = cubeManager.getCube("ssb_cube2");
+
+        // 2012-01-01,2012-01-03
+        cubeManager.appendSegment(cube1, new SegmentRange.TSRange(1325376000000L, 1325548800000L));
+        // 2012-01-03,2012-01-04
+        cubeManager.appendSegment(cube2, new SegmentRange.TSRange(1325548800000L, 1325635200000L));
+
+        // not throw exception
+        HybridCubeCLI.main(new String[] { "-name", "ssb_hybrid", "-project", "default", "-model", "ssb", "-cubes", "ssb_cube1,ssb_cube2", "-action", "update" });
     }
 
 }
