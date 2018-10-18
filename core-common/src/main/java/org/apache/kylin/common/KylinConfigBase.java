1fcf9679e14978b9d0fa36a500797991be34e63e
Zhong <nju_yaho@apache.org>
2018-10-18 17:04:25 +0800

    KYLIN-2894 add trigger kylin.query.cache-signature-enabled for enabling query signature

--- core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
+++ core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
@@ -1558,6 +1558,10 @@ abstract public class KylinConfigBase implements Serializable {
                 "org.apache.kylin.rest.signature.RealizationSetCalculator");
     }
 
+    public boolean isQueryCacheSignatureEnabled() {
+        return Boolean.parseBoolean(this.getOptional("kylin.query.cache-signature-enabled", "false"));
+    }
+
     // ============================================================================
     // SERVER
     // ============================================================================
