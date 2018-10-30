c324ca65926bdaf40f50047dca15b7a424d2721f
GodDispose <992532148@qq.com>
2018-10-30 16:31:57 +0800

    KYLIN-3611 upgrade to apache tomcat 7.0.91

--- build/script/download-tomcat.sh
+++ build/script/download-tomcat.sh
@@ -27,8 +27,8 @@ if [[ `uname -a` =~ "Darwin" ]]; then
     alias md5cmd="md5 -q"
 fi
 
-tomcat_pkg_version="7.0.90"
-tomcat_pkg_md5="cd4890e4e6a212dafd970da37d040877"
+tomcat_pkg_version="7.0.91"
+tomcat_pkg_md5="8bfbb358b51f90374067879f8db1e91c"
 
 if [ ! -f "build/apache-tomcat-${tomcat_pkg_version}.tar.gz" ]
 then
