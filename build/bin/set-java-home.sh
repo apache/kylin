846bfae76cda46b91f549eccf1b2d580f6e39044
Jin Xu <xu.jin@shopeemobile.com>
2018-12-13 11:19:16 +0800

    add JDK version validation

--- build/bin/set-java-home.sh
+++ build/bin/set-java-home.sh
@@ -26,3 +26,10 @@ then
     fi
     export JAVA_HOME=$JAVA_HOME
 fi
+
+# Validate kylin JDK version
+JAVA_VERSION=$(hbase -version 2>&1  | awk -F '"' '/version/ {print $2}' | awk -F "." '{print $1$2}')
+if [ "$JAVA_VERSION" -lt 18 ]
+then
+    quit "Kylin requires JDK 1.8+, please install or upgrade your JDK"
+fi
