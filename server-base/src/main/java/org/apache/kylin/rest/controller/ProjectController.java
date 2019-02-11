44fd1ba672524f3e6a4b27ccc36249b0ec29c5b8
shaofengshi <shaofengshi@apache.org>
2019-02-11 11:51:17 +0800

    minor, fix NPE

--- server-base/src/main/java/org/apache/kylin/rest/controller/ProjectController.java
+++ server-base/src/main/java/org/apache/kylin/rest/controller/ProjectController.java
@@ -193,7 +193,11 @@ public class ProjectController extends BasicController {
         try {
 
             ProjectInstance project = projectService.getProjectManager().getProject(projectName);
-            projectService.deleteProject(projectName, project);
+            if (project != null) {
+                projectService.deleteProject(projectName, project);
+            } else {
+                logger.info("Project {} not exists", projectName);
+            }
         } catch (Exception e) {
             logger.error(e.getLocalizedMessage(), e);
             throw new InternalErrorException("Failed to delete project. " + " Caused by: " + e.getMessage(), e);
