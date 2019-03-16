3911a4e5fdce18e167bf14fb0bed816521589f51
yanghua <yanghua1127@gmail.com>
2019-03-16 20:39:08 +0800

    KYLIN-3879 Implement FlinkEntry

---
+++ core-common/src/main/java/org/apache/kylin/common/util/FlinkEntry.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kylin.common.util;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * All flink job steps' endpoint.
+ */
+public final class FlinkEntry {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("FlinkEntry args:" + StringUtils.join(args, " "));
+        if (!(args.length >= 2)) {
+            throw new IllegalArgumentException(String.valueOf("-className is required"));
+        }
+        if (!(args[0].equals("-className"))) {
+            throw new IllegalArgumentException(String.valueOf("-className is required"));
+        }
+        final String className = args[1];
+        final Object o = Class.forName(className).newInstance();
+        if (!(o instanceof AbstractApplication)) {
+            throw new IllegalArgumentException(String.valueOf(className + " is not a subClass of AbstractApplication"));
+        }
+        String[] appArgs = new String[args.length - 2];
+        for (int i = 2; i < args.length; i++) {
+            appArgs[i - 2] = args[i];
+        }
+
+        AbstractApplication abstractApplication = (AbstractApplication) o;
+        abstractApplication.execute(appArgs);
+    }
+}
