4286d69f895d5f2b50c4a8a87d3f418c4ba9b32e
chao long <wayne.l@qq.com>
2019-04-02 10:01:15 +0800

    KYLIN-3929 Check satisfaction before execute cubeplanner algorithm

--- core-cube/src/main/java/org/apache/kylin/cube/cuboid/algorithm/CuboidRecommender.java
+++ core-cube/src/main/java/org/apache/kylin/cube/cuboid/algorithm/CuboidRecommender.java
@@ -133,6 +133,10 @@ public class CuboidRecommender {
         int allCuboidCount = cuboidStats.getAllCuboidsForMandatory().size()
                 + cuboidStats.getAllCuboidsForSelection().size();
 
+        if (!ifForceRecommend && allCuboidCount <= threshold1) {
+            return null;
+        }
+
         BenefitPolicy benefitPolicy = new PBPUSCalculator(cuboidStats);
         CuboidRecommendAlgorithm algorithm = null;
 
@@ -162,9 +166,6 @@ public class CuboidRecommender {
             }
         }
 
-        if (!ifForceRecommend && allCuboidCount <= threshold1) {
-            return null;
-        }
         return recommendCuboidsWithStats;
     }
 
