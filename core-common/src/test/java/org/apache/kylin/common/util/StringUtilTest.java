63ac334d27f24ced12850b635cdfe2e2e1afa7cc
Michael Hausegger <hausegger.michael@googlemail.com>
2019-05-02 08:35:10 +0200

    Removed remaining star import

--- core-common/src/test/java/org/apache/kylin/common/util/StringUtilTest.java
+++ core-common/src/test/java/org/apache/kylin/common/util/StringUtilTest.java
@@ -31,7 +31,13 @@ import java.util.Locale;
 import java.util.Set;
 import java.util.Vector;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 
 public class StringUtilTest {
     @Test
