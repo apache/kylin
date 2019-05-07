c1368132478497ce79a4475dbb8250df447d4bbc
Michael Hausegger <hausegger.michael@googlemail.com>
2019-05-07 15:57:20 +0200

    Removed star import that smuggled itself in.

--- core-common/src/test/java/org/apache/kylin/common/util/SortUtilTest.java
+++ core-common/src/test/java/org/apache/kylin/common/util/SortUtilTest.java
@@ -25,7 +25,11 @@ import java.util.Iterator;
 import java.util.ListIterator;
 import java.util.Stack;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 
 /**
  * Unit tests for class {@link SortUtil}.
