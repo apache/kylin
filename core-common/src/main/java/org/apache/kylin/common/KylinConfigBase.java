766f9b6ac61c4bb24001ba29927f5675b5bb11e9
Liu Shaohui <liushaohui@xiaomi.com>
2019-05-17 13:48:42 +0800

    KYLIN-4096: Make cube metadata validator rules configuable

--- core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
+++ core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
@@ -2219,4 +2219,8 @@ public abstract class KylinConfigBase implements Serializable {
     public int getStaleJobThresholdInDays() {
         return Integer.parseInt(getOptional("kylin.tool.health-check.stale-job-threshold-days", "30"));
     }
+
+    public String[] getCubeMetadataExtraValidators() {
+        return getOptionalStringArray("kylin.cube.metadata.extra.validators", new String[0]);
+    }
 }
