efd4ad400c8b8bfb4be87b892db7607173502b32
Michael Hausegger <hausegger.michael@googlemail.com>
2019-05-23 16:20:41 +0200

    Prettified class in order to retrigger build

--- jdbc/src/main/java/org/apache/kylin/jdbc/KylinMeta.java
+++ jdbc/src/main/java/org/apache/kylin/jdbc/KylinMeta.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 package org.apache.kylin.jdbc;
 
@@ -188,7 +188,7 @@ public class KylinMeta extends MetaImpl {
                 "IS_GENERATEDCOLUMN");
     }
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @SuppressWarnings({"rawtypes", "unchecked"})
     private MetaResultSet createResultSet(List iterable, Class clazz, String... names) {
         final List<ColumnMetaData> columns = new ArrayList<ColumnMetaData>();
         final List<Field> fields = new ArrayList<Field>();
@@ -267,20 +267,20 @@ public class KylinMeta extends MetaImpl {
                 buf.append('[').append(c).append(']');
             } else {
                 switch (c) {
-                case '\\':
-                    slash = i;
-                    break;
-                case '%':
-                    buf.append(".*");
-                    break;
-                case '[':
-                    buf.append("\\[");
-                    break;
-                case ']':
-                    buf.append("\\]");
-                    break;
-                default:
-                    buf.append('[').append(c).append(']');
+                    case '\\':
+                        slash = i;
+                        break;
+                    case '%':
+                        buf.append(".*");
+                        break;
+                    case '[':
+                        buf.append("\\[");
+                        break;
+                    case ']':
+                        buf.append("\\]");
+                        break;
+                    default:
+                        buf.append('[').append(c).append(']');
                 }
             }
         }
@@ -292,6 +292,7 @@ public class KylinMeta extends MetaImpl {
     // ============================================================================
 
     private static final List<MetaTableType> metaTableTypes = new ArrayList<MetaTableType>();
+
     static {
         metaTableTypes.add(new MetaTableType("TABLE"));
     }
