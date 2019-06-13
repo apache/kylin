b38fbcaf943a33a80e7400e21da60116266a5d62
nichunen <nic@apache.org>
2019-06-13 19:11:33 +0800

    KYLIN-4042 Fix too many posted messages on system page with same text

--- webapp/app/js/controllers/admin.js
+++ webapp/app/js/controllers/admin.js
@@ -29,7 +29,7 @@ KylinApp.controller('AdminCtrl', function ($scope, AdminService, CacheService, T
   $scope.getEnv = function () {
     AdminService.env({}, function (env) {
       $scope.envStr = env.env;
-      MessageBox.successNotify('Server environment get successfully');
+      MessageBox.successNotify('Server environment get successfully', "server-env");
 //            SweetAlert.swal('Success!', 'Server environment get successfully', 'success');
     }, function (e) {
       if (e.data && e.data.exception) {
@@ -45,7 +45,7 @@ KylinApp.controller('AdminCtrl', function ($scope, AdminService, CacheService, T
   $scope.getConfig = function () {
     AdminService.config({}, function (config) {
       $scope.configStr = config.config;
-      MessageBox.successNotify('Server config get successfully');
+      MessageBox.successNotify('Server config get successfully', "server-config");
     }, function (e) {
       if (e.data && e.data.exception) {
         var message = e.data.exception;
