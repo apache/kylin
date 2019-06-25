55f714007b21b8b7e197dd99d0f081b77fb4efce
Yifei.Wu <vafuler945@gmail.com>
2019-06-25 18:44:51 +0800

    KYLIN-4041 minor,fix it

--- kylin-it/src/test/resources/query/sql_verifyContent/query02.sql
+++ kylin-it/src/test/resources/query/sql_verifyContent/query02.sql
@@ -19,16 +19,15 @@
 -- This is a sample case
 
 
-select
- concat(meta_categ_name, CAST(null as VARCHAR)) as c1,
-
+select (
+ case when concat(meta_categ_name, cast(null as varchar)) is null then 'TRUE' else 'FALSE' end ) as C1
  from test_kylin_fact
- left JOIN edw.test_cal_dt as test_cal_dt
- ON test_kylin_fact.cal_dt = test_cal_dt.cal_dt
- left JOIN test_category_groupings
- ON test_kylin_fact.leaf_categ_id = test_category_groupings.leaf_categ_id AND test_kylin_fact.lstg_site_id = test_category_groupings.site_id
- left JOIN edw.test_sites as test_sites
- ON test_kylin_fact.lstg_site_id = test_sites.site_id
+ left join edw.test_cal_dt as test_cal_dt
+ on test_kylin_fact.cal_dt = test_cal_dt.cal_dt
+ left join test_category_groupings
+ on test_kylin_fact.leaf_categ_id = test_category_groupings.leaf_categ_id and test_kylin_fact.lstg_site_id = test_category_groupings.site_id
+ left join edw.test_sites as test_sites
+ on test_kylin_fact.lstg_site_id = test_sites.site_id
 
  where meta_categ_name = 'Computers'
  group by meta_categ_name
