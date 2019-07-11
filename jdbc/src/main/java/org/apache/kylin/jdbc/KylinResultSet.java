1c0d0e09b13c9be22d02cb5c2271694468fb289a
pnaszarkowski <pnaszarkowski@gmail.com>
2019-07-11 22:48:18 +0200

    Upgraded avatica calcite version due to issue in jdbc driver like https://community.jaspersoft.com/questions/1047476/i-can-not-connect-kylin-using-jdbc-driver

--- jdbc/src/main/java/org/apache/kylin/jdbc/KylinResultSet.java
+++ jdbc/src/main/java/org/apache/kylin/jdbc/KylinResultSet.java
@@ -39,7 +39,7 @@ import org.apache.kylin.jdbc.IRemoteClient.QueryResult;
 
 public class KylinResultSet extends AvaticaResultSet {
 
-    public KylinResultSet(AvaticaStatement statement, QueryState state, Signature signature, ResultSetMetaData resultSetMetaData, TimeZone timeZone, Frame firstFrame) {
+    public KylinResultSet(AvaticaStatement statement, QueryState state, Signature signature, ResultSetMetaData resultSetMetaData, TimeZone timeZone, Frame firstFrame) throws SQLException {
         super(statement, state, signature, resultSetMetaData, timeZone, firstFrame);
     }
 
