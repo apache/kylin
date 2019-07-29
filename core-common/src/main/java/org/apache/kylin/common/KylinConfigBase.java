f7ace48a0dedd1309b09b28c4a02d1fc7c0129fa
shaofengshi <shaofengshi@apache.org>
2019-07-29 08:58:25 +0800

    KYLIN-4314 extend intersect_count and add intersect_value UDAF

--- core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
+++ core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
@@ -2459,4 +2459,7 @@ public abstract class KylinConfigBase implements Serializable {
         return Integer.parseInt(getOptional("kylin.tool.health-check.stale-job-threshold-days", "30"));
     }
 
+    public String getIntersectFilterOrSeparator() {
+        return getOptional("kylin.query.intersect.separator", "|");
+    }
 }
