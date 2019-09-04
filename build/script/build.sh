fd4e6bcddc6667894edf7ae087ef7dc54c29958b
andras-istvan-nagy-epam <andras_istvan_nagy@epam.com>
2019-09-04 09:05:38 -0400

    allow to build as non-root; specify version of scala-maven-plugin

--- build/script/build.sh
+++ build/script/build.sh
@@ -27,8 +27,8 @@ mvn clean install -DskipTests $@ || { exit 1; }
 #package webapp
 echo 'package front-end'
 cd webapp
-npm install -g bower			 || { exit 1; }
+sudo npm install -g bower			 || { exit 1; }
 bower --allow-root install		 || { exit 1; }
 npm install						 || { exit 1; }
-npm install -g grunt-cli		 || { exit 1; }
+sudo npm install -g grunt-cli		 || { exit 1; }
 grunt dev --buildEnv=dev --buildNumber=`date "+%Y%m%d%H%M%S"`  || { exit 1; }
