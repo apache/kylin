6a28aa79a9a7abe10a1639b69f2828a0dbcb6a08
rupengwang <wangrupeng@livec.cn>
2019-11-11 15:40:39 +0800

    Revert "KYLIN-3999, make kylin.query.enable-dynamic-column default true"
    This reverts commit 5b1d0a5c

--- core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
+++ core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
@@ -1560,7 +1560,7 @@ public abstract class KylinConfigBase implements Serializable {
     // check KYLIN-3358, need deploy coprocessor if enabled
     // finally should be deprecated
     public boolean isDynamicColumnEnabled() {
-        return Boolean.parseBoolean(getOptional("kylin.query.enable-dynamic-column", TRUE));
+        return Boolean.parseBoolean(getOptional("kylin.query.enable-dynamic-column", FALSE));
     }
 
     //check KYLIN-1684, in most cases keep the default value
