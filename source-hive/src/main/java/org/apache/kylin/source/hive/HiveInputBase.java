5ef12cde52ee43ec4abfdccff21fb6d661d68cb7
weibin0516 <codingforfun@126.com>
2019-11-17 13:15:10 +0800

    KYLIN-4224 Create flat table wich spark sql

--- source-hive/src/main/java/org/apache/kylin/source/hive/HiveInputBase.java
+++ source-hive/src/main/java/org/apache/kylin/source/hive/HiveInputBase.java
@@ -19,14 +19,16 @@
 package org.apache.kylin.source.hive;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
+import java.util.Base64;
 import java.util.Objects;
 import java.util.Set;
+import java.util.Locale;
+import java.util.Collections;
 
 import com.google.common.collect.Lists;
 import org.apache.hadoop.fs.FileSystem;
@@ -41,6 +43,10 @@ import org.apache.kylin.cube.model.CubeDesc;
 import org.apache.kylin.engine.mr.IInput;
 import org.apache.kylin.engine.mr.JobBuilderSupport;
 import org.apache.kylin.engine.mr.steps.CubingExecutableUtil;
+import org.apache.kylin.engine.spark.SparkCreatingFlatTable;
+import org.apache.kylin.engine.spark.SparkExecutable;
+import org.apache.kylin.engine.spark.SparkExecutableFactory;
+import org.apache.kylin.engine.spark.SparkSqlBatch;
 import org.apache.kylin.job.JoinedFlatTable;
 import org.apache.kylin.job.common.ShellExecutable;
 import org.apache.kylin.job.constant.ExecutableConstants;
@@ -253,8 +259,14 @@ public class HiveInputBase {
             final KylinConfig kylinConfig = KylinConfig.getInstanceFromEnv();
             CubeInstance cubeInstance = CubeManager.getInstance(kylinConfig).getCube(cubeName);
 
-            if (kylinConfig.isLivyEnabled() && cubeInstance.getEngineType() == IEngineAware.ID_SPARK) {
-                jobFlow.addTask(createFlatHiveTableByLivyStep(hiveInitStatements, jobWorkingDir, cubeName, flatDesc));
+            if (cubeInstance.getEngineType() == IEngineAware.ID_SPARK) {
+                if (kylinConfig.isLivyEnabled()) {
+                    jobFlow.addTask(createFlatHiveTableByLivyStep(hiveInitStatements,
+                            jobWorkingDir, cubeName, flatDesc));
+                } else {
+                    jobFlow.addTask(createFlatHiveTableBySparkSql(hiveInitStatements,
+                            jobWorkingDir, cubeName, flatDesc));
+                }
             } else {
                 jobFlow.addTask(createFlatHiveTableStep(hiveInitStatements, jobWorkingDir, cubeName, flatDesc));
             }
@@ -339,6 +351,59 @@ public class HiveInputBase {
         CubingExecutableUtil.setCubeName(cubeName, step.getParams());
         step.setName(ExecutableConstants.STEP_NAME_CREATE_FLAT_HIVE_TABLE);
         return step;
+    }
+
+    protected static AbstractExecutable createFlatHiveTableBySparkSql(String hiveInitStatements,
+            String jobWorkingDir, String cubeName, IJoinedFlatTableDesc flatDesc) {
+        final String dropTableHql = JoinedFlatTable.generateDropTableStatement(flatDesc);
+        final String createTableHql = JoinedFlatTable.generateCreateTableStatement(flatDesc,
+                jobWorkingDir);
+        String insertDataHqls = JoinedFlatTable.generateInsertDataStatement(flatDesc);
+
+        KylinConfig config = flatDesc.getSegment().getConfig();
+        final SparkExecutable sparkExecutable = SparkExecutableFactory.instance(config);
+        sparkExecutable.setName(ExecutableConstants.STEP_NAME_CREATE_FLAT_TABLE_WITH_SPARK);
+        sparkExecutable.setClassName(SparkCreatingFlatTable.class.getName());
+
+        sparkExecutable.setParam(SparkSqlBatch.OPTION_CUBE_NAME.getOpt(), cubeName);
+        sparkExecutable.setParam(SparkSqlBatch.OPTION_STEP_NAME.getOpt(),
+                base64EncodeStr(ExecutableConstants.STEP_NAME_CREATE_FLAT_TABLE_WITH_SPARK));
+        sparkExecutable.setParam(SparkSqlBatch.OPTION_SEGMENT_ID.getOpt(),
+                flatDesc.getSegment().getName());
+        sparkExecutable.setParam(SparkSqlBatch.OPTION_SQL_COUNT.getOpt(),
+                String.valueOf(SparkCreatingFlatTable.SQL_COUNT));
+
+        sparkExecutable.setParam(SparkCreatingFlatTable.getSqlOption(0).getOpt(),
+                base64EncodeStr(hiveInitStatements));
+        sparkExecutable.setParam(SparkCreatingFlatTable.getSqlOption(1).getOpt(),
+                base64EncodeStr(dropTableHql));
+
+        // createTableHql include create table sql and alter table sql
+        String[] sqlArr = createTableHql.trim().split(";");
+        if (2 != sqlArr.length) {
+            throw new RuntimeException("create table hql should combined by a create table sql " +
+                    "and a alter sql, but got: " + createTableHql);
+        }
+        sparkExecutable.setParam(SparkCreatingFlatTable.getSqlOption(2).getOpt(),
+                base64EncodeStr(sqlArr[0]));
+        sparkExecutable.setParam(SparkCreatingFlatTable.getSqlOption(3).getOpt(),
+                base64EncodeStr(sqlArr[1]));
+
+        sparkExecutable.setParam(SparkCreatingFlatTable.getSqlOption(4).getOpt(),
+                base64EncodeStr(insertDataHqls));
+
+        StringBuilder jars = new StringBuilder();
+        StringUtil.appendWithSeparator(jars, config.getSparkAdditionalJars());
+        sparkExecutable.setJars(jars.toString());
+
+        return sparkExecutable;
+    }
+
+    private static String base64EncodeStr(String str) {
+        return new String(
+                Base64.getEncoder().encode(str.getBytes(StandardCharsets.UTF_8)),
+                StandardCharsets.UTF_8
+        );
     }
 
     protected static AbstractExecutable createRedistributeFlatHiveTableStep(String hiveInitStatements, String cubeName,
