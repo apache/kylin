922e996d95df25e4775d3645ae87e46611eaba76
yaqian.zhang <598593183@qq.com>
2019-12-19 15:24:20 +0800

    Add yarn tracking url in build cube with flink step

--- core-job/src/main/java/org/apache/kylin/job/common/PatternedLogger.java
+++ core-job/src/main/java/org/apache/kylin/job/common/PatternedLogger.java
@@ -54,6 +54,10 @@ public class PatternedLogger extends BufferedLogger {
     private static final Pattern PATTERN_SPARK_APP_URL = Pattern.compile("tracking URL: (.*)");
     private static final Pattern PATTERN_JOB_STATE = Pattern.compile("Final-State : (.*?)$");
 
+    //flink
+    private static final Pattern PATTERN_FLINK_APP_ID = Pattern.compile("Submitted application (.*)");
+    private static final Pattern PATTERN_FLINK_APP_URL = Pattern.compile("tracking URL: (.*)");
+
 
     private static Map<Pattern, Pair<String, Integer>> patternMap = Maps.newHashMap(); // key is pattern, value is a pair, the first is property key, second is pattern index.
 
@@ -70,6 +74,8 @@ public class PatternedLogger extends BufferedLogger {
         patternMap.put(PATTERN_SPARK_APP_ID, new Pair(ExecutableConstants.SPARK_JOB_ID, 1));
         patternMap.put(PATTERN_SPARK_APP_URL, new Pair(ExecutableConstants.YARN_APP_URL, 1));
         patternMap.put(PATTERN_JOB_STATE, new Pair(ExecutableConstants.YARN_APP_STATE, 1));
+        patternMap.put(PATTERN_FLINK_APP_ID, new Pair(ExecutableConstants.FLINK_JOB_ID, 1));
+        patternMap.put(PATTERN_FLINK_APP_URL, new Pair(ExecutableConstants.YARN_APP_URL, 1));
     }
 
     public PatternedLogger(Logger wrappedLogger) {
