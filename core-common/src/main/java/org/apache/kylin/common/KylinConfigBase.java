0bae42290ed04644f4cae2da7c44f633d9b1d4ba
liupan010 <liupan010@ke.com>
2019-12-26 19:38:29 +0800

    htable current-limiting

--- core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
+++ core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
@@ -2400,4 +2400,7 @@ public abstract class KylinConfigBase implements Serializable {
         return Integer.parseInt(getOptional("kylin.tool.health-check.stale-job-threshold-days", "30"));
     }
 
+    public int getHTableQuotaLimit(){
+        return Integer.parseInt(getOptional("kylin.hbase.htable-quota-limit", "10000"));
+    }
 }
--- storage-hbase/src/main/java/org/apache/kylin/storage/hbase/steps/CubeHTableUtil.java
+++ storage-hbase/src/main/java/org/apache/kylin/storage/hbase/steps/CubeHTableUtil.java
@@ -21,6 +21,8 @@ package org.apache.kylin.storage.hbase.steps;
 import java.io.IOException;
 
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
@@ -31,6 +33,10 @@ import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.io.compress.Compression.Algorithm;
 import org.apache.hadoop.hbase.io.encoding.DataBlockEncoding;
+import org.apache.hadoop.hbase.protobuf.ProtobufUtil;
+import org.apache.hadoop.hbase.protobuf.generated.QuotaProtos;
+import org.apache.hadoop.hbase.quotas.QuotaScope;
+import org.apache.hadoop.hbase.quotas.QuotaUtil;
 import org.apache.hadoop.hbase.regionserver.BloomType;
 import org.apache.hadoop.hbase.regionserver.DisabledRegionSplitPolicy;
 import org.apache.hadoop.hbase.security.User;
@@ -104,6 +110,10 @@ public class CubeHTableUtil {
             DeployCoprocessorCLI.deployCoprocessor(tableDesc);
 
             admin.createTable(tableDesc, splitKeys);
+
+            int limit = kylinConfig.getHTableQuotaLimit();
+            QuotaProtos.Quotas quotaTable = QuotaProtos.Quotas.newBuilder().setThrottle(QuotaProtos.Throttle.newBuilder().setReqNum(ProtobufUtil.toTimedQuota(limit, TimeUnit.SECONDS, QuotaScope.CLUSTER))).build();
+            QuotaUtil.addTableQuota(conn, TableName.valueOf(tableName), quotaTable);
             Preconditions.checkArgument(admin.isTableAvailable(TableName.valueOf(tableName)), "table " + tableName + " created, but is not available due to some reasons");
             logger.info("create hbase table " + tableName + " done.");
         } finally {
