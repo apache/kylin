ea40f0c7136618c6b29422b61f4584757cb37110
Zhong, Yanghong <nju_yaho@apache.org>
2020-03-10 13:02:25 +0800

    KYLIN-4410 Limit the number of simultaneous rpcs to HBase for project

--- storage-hbase/src/main/java/org/apache/kylin/storage/hbase/cube/v2/CubeHBaseEndpointRPC.java
+++ storage-hbase/src/main/java/org/apache/kylin/storage/hbase/cube/v2/CubeHBaseEndpointRPC.java
@@ -39,6 +39,7 @@ import org.apache.hadoop.hbase.ipc.ServerRpcController;
 import org.apache.kylin.common.KylinConfig;
 import org.apache.kylin.common.QueryContext;
 import org.apache.kylin.common.QueryContext.CubeSegmentStatistics;
+import org.apache.kylin.common.SubThreadPoolExecutor;
 import org.apache.kylin.common.debug.BackdoorToggles;
 import org.apache.kylin.common.exceptions.KylinTimeoutException;
 import org.apache.kylin.common.exceptions.ResourceLimitExceededException;
@@ -57,7 +58,10 @@ import org.apache.kylin.gridtable.GTScanRange;
 import org.apache.kylin.gridtable.GTScanRequest;
 import org.apache.kylin.gridtable.GTUtil;
 import org.apache.kylin.gridtable.IGTScanner;
+import org.apache.kylin.metadata.cachesync.Broadcaster;
 import org.apache.kylin.metadata.model.ISegment;
+import org.apache.kylin.metadata.project.ProjectInstance;
+import org.apache.kylin.metadata.project.ProjectManager;
 import org.apache.kylin.storage.StorageContext;
 import org.apache.kylin.storage.gtrecord.DummyPartitionStreamer;
 import org.apache.kylin.storage.gtrecord.StorageResponseGTScatter;
@@ -71,6 +75,9 @@ import org.apache.kylin.storage.hbase.util.HBaseUnionUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.common.collect.Lists;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.HBaseZeroCopyByteString;
@@ -81,6 +88,38 @@ public class CubeHBaseEndpointRPC extends CubeHBaseRPC {
 
     private static ExecutorService executorService = new LoggableCachedThreadPool();
 
+    private static LoadingCache<String, ExecutorService> projectThreadPoolMap = CacheBuilder.newBuilder()
+            .build(new CacheLoader<String, ExecutorService>() {
+                @Override
+                public ExecutorService load(String projName) throws Exception {
+                    ExecutorService sharedPool = HBaseConnection.getCoprocessorPool();
+                    ProjectInstance projInst = ProjectManager.getInstance(KylinConfig.getInstanceFromEnv())
+                            .getProject(projName);
+                    return new SubThreadPoolExecutor(sharedPool, "PROJECT",
+                            projInst.getConfig().getHBaseMaxConnectionThreadsPerProject());
+                }
+            });
+
+    private static class ProjectThreadPoolSyncListener extends Broadcaster.Listener {
+        @Override
+        public void onClearAll(Broadcaster broadcaster) throws IOException {
+            projectThreadPoolMap.invalidateAll();
+            logger.info("Project level thread pools are cleared");
+        }
+
+        @Override
+        public void onEntityChange(Broadcaster broadcaster, String entity, Broadcaster.Event event, String cacheKey)
+                throws IOException {
+            projectThreadPoolMap.invalidate(cacheKey);
+            logger.info("Thread pool map for project {} is cleared", cacheKey);
+        }
+    }
+
+    static {
+        Broadcaster.getInstance(KylinConfig.getInstanceFromEnv())
+                .registerStaticListener(new ProjectThreadPoolSyncListener(), "project");
+    }
+    
     public CubeHBaseEndpointRPC(ISegment segment, Cuboid cuboid, GTInfo fullGTInfo, StorageContext context) {
         super(segment, cuboid, fullGTInfo, context);
     }
@@ -258,8 +297,10 @@ public class CubeHBaseEndpointRPC extends CubeHBaseRPC {
 
         try {
             final Connection conn =  HBaseUnionUtil.getConnection(cubeSeg.getConfig(), cubeSeg.getStorageLocationIdentifier());
+            ExecutorService projThreadPool = projectThreadPoolMap
+                    .get(queryContext.getProject().toUpperCase(Locale.ROOT));
             final Table table = conn.getTable(TableName.valueOf(cubeSeg.getStorageLocationIdentifier()),
-                    queryContext.getConnectionPool(HBaseConnection.getCoprocessorPool()));
+                    queryContext.getConnectionPool(projThreadPool));
 
             table.coprocessorService(CubeVisitService.class, startKey, endKey, //
                     new Batch.Call<CubeVisitService, CubeVisitResponse>() {
