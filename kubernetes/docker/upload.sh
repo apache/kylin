41eb4f992bc138eb90bebb3ebe537a373e7300b9
XiaoxiangYu <hit_lacus@126.com>
2020-04-26 20:05:26 +0800

    KYLIN-4447 Upload a complete example for CDH5.7 env(2 job + 2 query)

--- kubernetes/docker/upload.sh
+++
@@ -1,2 +0,0 @@
-docker save -o kylin-cdh.tar kylin-client:3.0.1-cdh57
-scp kylin-cdh.tar ubuntu@10.1.2.41/home/ubuntu/
--- kubernetes/template/quickstart/cleanup.sh
+++
@@ -1,2 +0,0 @@
-kubectl delete -f deployment/kylin/kylin-serivce.yaml
-kubectl delete -f deployment/kylin/kylin-all-statefulset.yaml
