7322b5f9d275a9f29962bea76ea185d83f89962a
rupengwang <wangrupeng@live.cn>
2020-06-11 13:54:59 +0800

    Minor, refactor
    - disbale setting kylin config
    - add check for database name
    - add check for command parameter

--- core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
+++ core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
@@ -1102,7 +1102,7 @@ public abstract class KylinConfigBase implements Serializable {
     }
 
     public String getHiveDatabaseForIntermediateTable() {
-        return this.getOptional("kylin.source.hive.database-for-flat-table", DEFAULT);
+        return CliCommandExecutor.checkHiveProperty(this.getOptional("kylin.source.hive.database-for-flat-table", DEFAULT));
     }
 
 
@@ -2219,6 +2219,10 @@ public abstract class KylinConfigBase implements Serializable {
 
     public boolean isWebDashboardEnabled() {
         return Boolean.parseBoolean(getOptional("kylin.web.dashboard-enabled", FALSE));
+    }
+
+    public boolean isWebConfigEnabled() {
+        return Boolean.parseBoolean(getOptional("kylin.web.set-config-enable", FALSE));
     }
 
     public String getPropertiesWhiteList() {
