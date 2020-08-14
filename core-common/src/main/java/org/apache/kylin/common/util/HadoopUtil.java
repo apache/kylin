e4779e08b3e0ef291ec67f6212ef48b700a1e7e0
ITzhangqiang <ITzhangqiang@163.com>
2020-08-14 16:29:46 +0800

    KYLIN-4688 Too many tmp files in HDFS tmp dictionary

--- core-common/src/main/java/org/apache/kylin/common/util/HadoopUtil.java
+++ core-common/src/main/java/org/apache/kylin/common/util/HadoopUtil.java
@@ -69,13 +69,6 @@ public class HadoopUtil {
     }
 
     public static Configuration healSickConfig(Configuration conf) {
-        // https://issues.apache.org/jira/browse/KYLIN-953
-        if (StringUtils.isBlank(conf.get("hadoop.tmp.dir"))) {
-            conf.set("hadoop.tmp.dir", "/tmp");
-        }
-        if (StringUtils.isBlank(conf.get("hbase.fs.tmp.dir"))) {
-            conf.set("hbase.fs.tmp.dir", "/tmp");
-        }
         //  https://issues.apache.org/jira/browse/KYLIN-3064
         conf.set("yarn.timeline-service.enabled", "false");
         return conf;
