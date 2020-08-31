7294e6dd4d7b546f1bc480e7c074c3518cbcd97c
Zhichao Zhang <441586683@qq.com>
2020-08-31 22:34:05 +0800

    KYLIN-4731 Kylin query failing with 'NPE while executing SQL'
    Please see the detailed info: https://issues.apache.org/jira/projects/KYLIN/issues/KYLIN-4731
    
    (cherry picked from commit e6d9fb0c3d5684d391ea1c8085e2e4bfe8f303db)

--- core-storage/src/main/java/org/apache/kylin/storage/gtrecord/CubeTupleConverter.java
+++ core-storage/src/main/java/org/apache/kylin/storage/gtrecord/CubeTupleConverter.java
@@ -299,7 +299,7 @@ public class CubeTupleConverter implements ITupleConverter {
                     for (int i = 0; i < hostTmpIdx.length; i++) {
                         lookupKey.data[i] = CubeTupleConverter.toString(gtValues[hostTmpIdx[i]]);
                         // if the primary key of lookup table is date time type, do this change in case of data type inconsistency
-                        if (deriveInfo.join.getPrimaryKeyColumns()[i].getType().isDateTimeFamily()) {
+                        if (lookupKey.data[i] != null && deriveInfo.join.getPrimaryKeyColumns()[i].getType().isDateTimeFamily()) {
                             lookupKey.data[i] = String.valueOf(DateFormat.stringToMillis(lookupKey.data[i]));
                         }
                     }
