a722e937b269a962318ce6ad3cac243bd8d6006e
benjobs <benjobs@qq.com>
2021-01-04 09:29:06 +0800

    fix build-incremental-cube.sh authorization'error

--- build/bin/system-cube.sh
+++ build/bin/system-cube.sh
@@ -20,12 +20,41 @@
 
 source ${KYLIN_HOME:-"$(cd -P -- "$(dirname -- "$0")" && pwd -P)/../"}/bin/header.sh
 
+build_incremental_cube="${KYLIN_HOME}/bin/build-incremental-cube.sh"
+
 function printHelp {
     echo "usage: system-cube.sh setup"
     echo "       system-cube.sh build [INTERVAL:600000] [DELAY:0]"
     echo "       system-cube.sh cron"
     echo "       system-cube.sh help"
     exit 1
+}
+
+function authorization() {
+    authed=`grep "%Auth%" $build_incremental_cube`|wc -l`
+    if [[ $authed -eq 0 ]]
+    then
+        read -p $'Please Enter the ADMIN\'password or \'N\' to exit ' pwd
+        if [[ "$pwd" == "N"  ]]
+        then
+          return 1
+        else
+            auth=`echo "ADMIN:$pwd"|base64`
+            tomcat_root=${dir}/../tomcat
+            kylin_rest_address=`hostname -f`":"`grep "<Connector port=" ${tomcat_root}/conf/server.xml |grep protocol=\"HTTP/1.1\" | cut -d '=' -f 2 | cut -d \" -f 2`
+            http_code=$(curl -I -m 10 -o /dev/null -s -w %{http_code} -c -X POST -H "Authorization: Basic $auth" -H 'Content-Type: application/json' http://${kylin_rest_address}/kylin/api/user/authentication)
+            if [[ $http_code -eq 200  ]]
+            then
+                sed -i "s/%Auth%/${auth}/g" $build_incremental_cube
+                return 0
+            else
+                echo "Unauthorized,password error."
+                authorization
+            fi
+        fi
+    else
+        return 0
+    fi
 }
 
 if [[ "$@" == *"help"* ]]
@@ -54,91 +83,94 @@ then
     then
         BUILD_INTERVAL=${2:-"600000"}
         BUILD_DELAY=${3:-"0"}
-
         echo "build system cubes, build_interval:${BUILD_INTERVAL}, build_delay:${BUILD_DELAY}"
-
-		sh ${KYLIN_HOME}/bin/build-incremental-cube.sh ${SC_NAME_1} ${BUILD_INTERVAL} ${BUILD_DELAY}
-		sh ${KYLIN_HOME}/bin/build-incremental-cube.sh ${SC_NAME_2} ${BUILD_INTERVAL} ${BUILD_DELAY}
-		sh ${KYLIN_HOME}/bin/build-incremental-cube.sh ${SC_NAME_3} ${BUILD_INTERVAL} ${BUILD_DELAY}
-		sh ${KYLIN_HOME}/bin/build-incremental-cube.sh ${SC_NAME_4} ${BUILD_INTERVAL} ${BUILD_DELAY}
-		sh ${KYLIN_HOME}/bin/build-incremental-cube.sh ${SC_NAME_5} ${BUILD_INTERVAL} ${BUILD_DELAY}
+        sh $build_incremental_cube ${SC_NAME_1} ${BUILD_INTERVAL} ${BUILD_DELAY}
+        sh $build_incremental_cube ${SC_NAME_2} ${BUILD_INTERVAL} ${BUILD_DELAY}
+        sh $build_incremental_cube ${SC_NAME_3} ${BUILD_INTERVAL} ${BUILD_DELAY}
+        sh $build_incremental_cube ${SC_NAME_4} ${BUILD_INTERVAL} ${BUILD_DELAY}
+        sh $build_incremental_cube ${SC_NAME_5} ${BUILD_INTERVAL} ${BUILD_DELAY}
     else
-    	echo "Please setup system cube first."
-		exit 1
+    	  echo "Please setup system cube first."
+		    exit 1
     fi
 elif [ "$1" == "setup" ]
 then
-	#creat SCSinkTools.json
-	SINK_TOOLS_FILE=$KYLIN_HOME/SCSinkTools.json
+    #creat SCSinkTools.json
+    SINK_TOOLS_FILE=$KYLIN_HOME/SCSinkTools.json
 
-	echo "setup system cubes"
+    echo "setup system cubes"
 
-	rm -rf $SINK_TOOLS_FILE $OUTPUT_FORDER
+    rm -rf $SINK_TOOLS_FILE $OUTPUT_FORDER
 
-	cat <<-EOF > ${SINK_TOOLS_FILE}
-	[
-    {
-       "sink": "hive",
-       "storage_type": 2,
-       "cube_desc_override_properties": {
-         "kylin.cube.algorithm": "INMEM",
-         "kylin.cube.max-building-segments": "1"
-       }
-    }
-	]
-	EOF
-  $KYLIN_HOME/bin/kylin.sh org.apache.kylin.tool.metrics.systemcube.SCCreator \
-  -inputConfig ${SINK_TOOLS_FILE} \
-  -output ${OUTPUT_FORDER}
+    cat <<-EOF > ${SINK_TOOLS_FILE}
+    [
+      {
+         "sink": "hive",
+         "storage_type": 2,
+         "cube_desc_override_properties": {
+           "kylin.cube.algorithm": "INMEM",
+           "kylin.cube.max-building-segments": "1"
+         }
+      }
+    ]
+    EOF
+    $KYLIN_HOME/bin/kylin.sh org.apache.kylin.tool.metrics.systemcube.SCCreator \
+    -inputConfig ${SINK_TOOLS_FILE} \
+    -output ${OUTPUT_FORDER}
 
-  hive_client_mode=`bash ${KYLIN_HOME}/bin/get-properties.sh kylin.source.hive.client`
+    hive_client_mode=`bash ${KYLIN_HOME}/bin/get-properties.sh kylin.source.hive.client`
 
-  # Get Database, default is KYLIN
-  system_database="KYLIN"
+    # Get Database, default is KYLIN
+    system_database="KYLIN"
 
-  # 'create database' failed will not exit when donot have permission to create database;
-  sed -i -e 's/CREATE DATABASE /-- CREATE DATABASE /g' ${OUTPUT_FORDER}/create_hive_tables_for_system_cubes.sql
+    # 'create database' failed will not exit when donot have permission to create database;
+    sed -i -e 's/CREATE DATABASE /-- CREATE DATABASE /g' ${OUTPUT_FORDER}/create_hive_tables_for_system_cubes.sql
 
-  if [ "${hive_client_mode}" == "beeline" ]
-  then
-      beeline_params=`bash ${KYLIN_HOME}/bin/get-properties.sh kylin.source.hive.beeline-params`
-      beeline ${beeline_params} -e "CREATE DATABASE IF NOT EXISTS "$system_database
+    if [ "${hive_client_mode}" == "beeline" ]
+    then
+        beeline_params=`bash ${KYLIN_HOME}/bin/get-properties.sh kylin.source.hive.beeline-params`
+        beeline ${beeline_params} -e "CREATE DATABASE IF NOT EXISTS "$system_database
 
-      hive2_url=`expr match "${beeline_params}" '.*\(hive2:.*:[0-9]\{4,6\}\/\)'`
-      if [ -z ${hive2_url} ]; then
-          hive2_url=`expr match "${beeline_params}" '.*\(hive2:.*:[0-9]\{4,6\}\)'`
-          beeline_params=${beeline_params/${hive2_url}/${hive2_url}/${system_database}}
-      else
-          beeline_params=${beeline_params/${hive2_url}/${hive2_url}${system_database}}
-      fi
+        hive2_url=`expr match "${beeline_params}" '.*\(hive2:.*:[0-9]\{4,6\}\/\)'`
+        if [ -z ${hive2_url} ]; then
+            hive2_url=`expr match "${beeline_params}" '.*\(hive2:.*:[0-9]\{4,6\}\)'`
+            beeline_params=${beeline_params/${hive2_url}/${hive2_url}/${system_database}}
+        else
+            beeline_params=${beeline_params/${hive2_url}/${hive2_url}${system_database}}
+        fi
 
-      beeline ${beeline_params} -f ${OUTPUT_FORDER}/create_hive_tables_for_system_cubes.sql  || { exit 1; }
-  else
-      hive -e "CREATE DATABASE IF NOT EXISTS "$system_database
-      hive --database $system_database -f ${OUTPUT_FORDER}/create_hive_tables_for_system_cubes.sql  || { exit 1; }
-  fi
+        beeline ${beeline_params} -f ${OUTPUT_FORDER}/create_hive_tables_for_system_cubes.sql  || { exit 1; }
+    else
+        hive -e "CREATE DATABASE IF NOT EXISTS "$system_database
+        hive --database $system_database -f ${OUTPUT_FORDER}/create_hive_tables_for_system_cubes.sql  || { exit 1; }
+    fi
 
-  $KYLIN_HOME/bin/metastore.sh restore ${OUTPUT_FORDER}
+    $KYLIN_HOME/bin/metastore.sh restore ${OUTPUT_FORDER}
 
-  #refresh signature
-  $KYLIN_HOME/bin/kylin.sh org.apache.kylin.cube.cli.CubeSignatureRefresher ${SC_NAME_1},${SC_NAME_2},${SC_NAME_3},${SC_NAME_4},${SC_NAME_5}
+    #refresh signature
+    $KYLIN_HOME/bin/kylin.sh org.apache.kylin.cube.cli.CubeSignatureRefresher ${SC_NAME_1},${SC_NAME_2},${SC_NAME_3},${SC_NAME_4},${SC_NAME_5}
 
 elif [ "$1" == "cron" ]
 then
     #add a crontab job
     echo "add to a crontab job"
-
-    CRONTAB_FILE=$KYLIN_HOME/crontabJob
-    	crontab -l >> ${CRONTAB_FILE}
-	cat <<-EOF >> ${CRONTAB_FILE}
-    0 */2 * * * sh ${KYLIN_HOME}/bin/build-incremental-cube.sh ${SC_NAME_1} 3600000 1200000
-    20 */2 * * * sh ${KYLIN_HOME}/bin/build-incremental-cube.sh ${SC_NAME_2} 3600000 1200000
-    40 */4 * * * sh ${KYLIN_HOME}/bin/build-incremental-cube.sh ${SC_NAME_3} 3600000 1200000
-    30 */4 * * * sh ${KYLIN_HOME}/bin/build-incremental-cube.sh ${SC_NAME_4} 3600000 1200000
-    50 */12 * * * sh ${KYLIN_HOME}/bin/build-incremental-cube.sh ${SC_NAME_5} 3600000 1200000
-	EOF
-    crontab ${CRONTAB_FILE}
-    rm ${CRONTAB_FILE}
+    authorization
+    if [[ $? == 1 ]]
+        echo "add to a crontab job exit."
+        exit 0
+    else
+        CRONTAB_FILE=$KYLIN_HOME/crontabJob
+        crontab -l >> ${CRONTAB_FILE}
+        cat <<-EOF >> ${CRONTAB_FILE}
+        0 */2 * * * sh $build_incremental_cube ${SC_NAME_1} 3600000 1200000
+        20 */2 * * * sh $build_incremental_cube ${SC_NAME_2} 3600000 1200000
+        40 */4 * * * sh $build_incremental_cube ${SC_NAME_3} 3600000 1200000
+        30 */4 * * * sh $build_incremental_cube ${SC_NAME_4} 3600000 1200000
+        50 */12 * * * sh $build_incremental_cube ${SC_NAME_5} 3600000 1200000
+        EOF
+        crontab ${CRONTAB_FILE}
+        rm ${CRONTAB_FILE}
+    if
 else
     printHelp
 fi
