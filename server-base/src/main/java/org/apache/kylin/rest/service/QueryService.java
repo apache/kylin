a3f61a806ae3f849fb88f9882ddf40e33ae979a3
wangweijian123456 <cn709394@gmail.com>
2021-06-01 00:50:17 +0800

    sql param optimize

--- server-base/src/main/java/org/apache/kylin/rest/service/QueryService.java
+++ server-base/src/main/java/org/apache/kylin/rest/service/QueryService.java
@@ -1251,61 +1251,46 @@ public class QueryService extends BasicService {
             throws SQLException {
         boolean isNull = (null == param.getValue());
 
-        Class<?> clazz;
-        try {
-            clazz = Class.forName(param.getClassName());
-        } catch (ClassNotFoundException e) {
-            throw new InternalErrorException(e);
-        }
+        String paramClassName = param.getClassName();
+        switch (paramClassName){
+            case "java.lang.Character":
+            case "java.lang.String":
+                preparedState.setString(index, isNull ? null : String.valueOf(param.getValue()));
+                break;
+            case "java.lang.Integer":
+                preparedState.setInt(index, isNull ? 0 : Integer.parseInt(param.getValue()));
+                break;
+            case "java.lang.Short":
+                preparedState.setShort(index, isNull ? 0 : Short.parseShort(param.getValue()));
+                break;
+            case "java.lang.Long":
+                preparedState.setLong(index, isNull ? 0 : Long.parseLong(param.getValue()));
+                break;
+            case "java.lang.Float":
+                preparedState.setFloat(index, isNull ? 0 : Float.parseFloat(param.getValue()));
+                break;
+            case "java.lang.Double":
+                preparedState.setDouble(index, isNull ? 0 : Double.parseDouble(param.getValue()));
+                break;
+            case "java.lang.Boolean":
+                preparedState.setBoolean(index, !isNull && Boolean.parseBoolean(param.getValue()));
+                break;
+            case "java.lang.Byte":
+                preparedState.setByte(index, isNull ? 0 : Byte.parseByte(param.getValue()));
+                break;
+            case "java.util.Date":
+            case "java.sql.Date":
+                preparedState.setDate(index, isNull ? null : java.sql.Date.valueOf(param.getValue()));
+                break;
+            case "java.sql.Time":
+                preparedState.setTime(index, isNull ? null : Time.valueOf(param.getValue()));
+                break;
+            case "java.sql.Timestamp":
+                preparedState.setTimestamp(index, isNull ? null : Timestamp.valueOf(param.getValue()));
+                break;
+            default:
+                preparedState.setObject(index, isNull ? null : param.getValue());
 
-        Rep rep = Rep.of(clazz);
-
-        switch (rep) {
-        case PRIMITIVE_CHAR:
-        case CHARACTER:
-        case STRING:
-            preparedState.setString(index, isNull ? null : String.valueOf(param.getValue()));
-            break;
-        case PRIMITIVE_INT:
-        case INTEGER:
-            preparedState.setInt(index, isNull ? 0 : Integer.parseInt(param.getValue()));
-            break;
-        case PRIMITIVE_SHORT:
-        case SHORT:
-            preparedState.setShort(index, isNull ? 0 : Short.parseShort(param.getValue()));
-            break;
-        case PRIMITIVE_LONG:
-        case LONG:
-            preparedState.setLong(index, isNull ? 0 : Long.parseLong(param.getValue()));
-            break;
-        case PRIMITIVE_FLOAT:
-        case FLOAT:
-            preparedState.setFloat(index, isNull ? 0 : Float.parseFloat(param.getValue()));
-            break;
-        case PRIMITIVE_DOUBLE:
-        case DOUBLE:
-            preparedState.setDouble(index, isNull ? 0 : Double.parseDouble(param.getValue()));
-            break;
-        case PRIMITIVE_BOOLEAN:
-        case BOOLEAN:
-            preparedState.setBoolean(index, !isNull && Boolean.parseBoolean(param.getValue()));
-            break;
-        case PRIMITIVE_BYTE:
-        case BYTE:
-            preparedState.setByte(index, isNull ? 0 : Byte.parseByte(param.getValue()));
-            break;
-        case JAVA_UTIL_DATE:
-        case JAVA_SQL_DATE:
-            preparedState.setDate(index, isNull ? null : java.sql.Date.valueOf(param.getValue()));
-            break;
-        case JAVA_SQL_TIME:
-            preparedState.setTime(index, isNull ? null : Time.valueOf(param.getValue()));
-            break;
-        case JAVA_SQL_TIMESTAMP:
-            preparedState.setTimestamp(index, isNull ? null : Timestamp.valueOf(param.getValue()));
-            break;
-        default:
-            preparedState.setObject(index, isNull ? null : param.getValue());
         }
     }
 
