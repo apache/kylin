e4feff0b0dfb9c1e80aee11d5be86c5462b4af9e
Mukvin <boyboys520@163.com>
2022-03-21 17:16:34 +0800

    # fix user api

--- server-base/src/main/java/org/apache/kylin/rest/controller/UserController.java
+++ server-base/src/main/java/org/apache/kylin/rest/controller/UserController.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,7 +18,12 @@
 
 package org.apache.kylin.rest.controller;
 
+import org.apache.kylin.rest.response.EnvelopeResponse;
+import org.apache.kylin.rest.response.ResponseCode;
+import org.apache.kylin.rest.security.ManagedUser;
 import org.apache.kylin.rest.service.UserService;
+import org.apache.kylin.rest.util.AclEvaluate;
+import org.apache.kylin.rest.util.PagingUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,13 +34,19 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Handle user authentication request to protected kylin rest resources by
  * spring security.
- * 
+ *
  * @author xduo
- * 
+ *
  */
 @Controller
 @RequestMapping(value = "/user")
@@ -45,6 +56,9 @@ public class UserController extends BasicController {
     @Autowired
     @Qualifier("userService")
     UserService userService;
+
+    @Autowired
+    private AclEvaluate aclEvaluate;
 
     @RequestMapping(value = "/authentication", method = RequestMethod.POST, produces = { "application/json" })
     public UserDetails authenticate() {
@@ -71,5 +85,30 @@ public class UserController extends BasicController {
         }
 
         return null;
+    }
+
+    @RequestMapping(value = "/users", method = { RequestMethod.GET }, produces = { "application/json" })
+    @ResponseBody
+    public EnvelopeResponse listAllUsers(@RequestParam(value = "project", required = false) String project,
+                                         @RequestParam(value = "name", required = false) String name,
+                                         @RequestParam(value = "groupName", required = false) String groupName,
+                                         @RequestParam(value = "isFuzzMatch", required = false) boolean isFuzzMatch,
+                                         @RequestParam(value = "offset", required = false, defaultValue = "0") Integer offset,
+                                         @RequestParam(value = "limit", required = false, defaultValue = "10") Integer limit) throws IOException {
+        if (project == null) {
+            aclEvaluate.checkIsGlobalAdmin();
+        } else {
+            aclEvaluate.checkProjectAdminPermission(project);
+        }
+        HashMap<String, Object> data = new HashMap<>();
+        List<ManagedUser> usersByFuzzMatching = userService.listUsers();
+        List<ManagedUser> subList = PagingUtil.cutPage(usersByFuzzMatching, offset, limit);
+        //LDAP users dose not have authorities
+        for (ManagedUser u : subList) {
+            userService.completeUserInfo(u);
+        }
+        data.put("users", subList);
+        data.put("size", usersByFuzzMatching.size());
+        return new EnvelopeResponse(ResponseCode.CODE_SUCCESS, data, "");
     }
 }
