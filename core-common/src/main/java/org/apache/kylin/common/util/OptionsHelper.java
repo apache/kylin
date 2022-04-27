a6941e35bda6a4d819b966c0c040b0931c84630f
Mukvin <boyboys520@163.com>
2022-04-27 20:07:29 +0800

    KYLIN-5179, fix StorageCleanupJob remove invalid segement

--- core-common/src/main/java/org/apache/kylin/common/util/OptionsHelper.java
+++ core-common/src/main/java/org/apache/kylin/common/util/OptionsHelper.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -66,6 +66,10 @@ public class OptionsHelper {
     public void printUsage(String programName, Options options) {
         HelpFormatter formatter = new HelpFormatter();
         formatter.printHelp(programName, options);
+    }
+
+    public boolean isHelpOption() throws ParseException {
+        return commandLine.hasOption("help");
     }
 
     public static String convertToFileURL(String path) {
