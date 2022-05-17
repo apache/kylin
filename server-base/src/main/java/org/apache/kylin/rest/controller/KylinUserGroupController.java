b3149f2bcde2c45e3acb4b565a97725afd527286
Mukvin <boyboys520@163.com>
2022-05-17 16:57:47 +0800

    # support mdx api 2

--- server-base/src/main/java/org/apache/kylin/rest/controller/KylinUserGroupController.java
+++ server-base/src/main/java/org/apache/kylin/rest/controller/KylinUserGroupController.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,7 +40,8 @@ public class KylinUserGroupController extends BasicController {
 
     @RequestMapping(value = "/groups", method = {RequestMethod.GET}, produces = {"application/json"})
     @ResponseBody
-    public List<String> listUserAuthorities(@RequestParam(value = "project") String project) throws IOException {
+    public List<String> listUserAuthorities(@RequestParam(value = "project", required = false) String project)
+            throws IOException {
         return userGroupService.listAllAuthorities(project);
     }
 }
