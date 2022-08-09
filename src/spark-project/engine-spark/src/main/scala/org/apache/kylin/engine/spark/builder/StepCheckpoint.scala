b3e8d18a594ba169193ca5f49911040000ed4566
Guoliang Sun <guoliang.sun@kyligence.io>
2022-08-09 13:06:48 +0800

    KYLIN-5217 Create a initial commit

--- src/spark-project/engine-spark/src/main/scala/org/apache/kylin/engine/spark/builder/StepCheckpoint.scala
+++
@@ -1,3 +0,0 @@
-package org.apache.kylin.engine.spark.builder
-
-trait StepCheckpoint {}
--- src/spark-project/engine-spark/src/main/scala/org/apache/kylin/engine/spark/utils/SparkConfRuleConstants.java
+++
@@ -1,9 +0,0 @@
-package org.apache.kylin.engine.spark.utils;
-
-public final class SparkConfRuleConstants {
-    public static final String DEFUALT_EXECUTOR_CORE = "1";
-
-    private SparkConfRuleConstants() {
-        // not call
-    }
-}
