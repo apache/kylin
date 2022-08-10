3895d495c4af5bf8905403aaa2614f1d7d151ef2
Mukvin <boyboys520@163.com>
2022-08-10 16:35:52 +0800

    KYLIN-5221, manage the sidebar in manual

--- document/website/docusaurus.config.js
+++ document/website/docusaurus.config.js
@@ -170,6 +170,11 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      docs: {
+        sidebar: {
+          autoCollapseCategories: true,
+        }
+      }
     }),
 };
 
