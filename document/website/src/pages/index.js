f77355e05cbdb59c78624dccdbfb4750659cd57e
XiaoxiangYu <xxyu@apache.org>
2022-08-10 11:13:24 +0800

    KYLIN-5221 Create document based on docusaurus

--- document/website/src/pages/index.js
+++ document/website/src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/category/tutorial">
             Kylin 5.0 Tutorial - 15min ⏱️
           </Link>
         </div>
