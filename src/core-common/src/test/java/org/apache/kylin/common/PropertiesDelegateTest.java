76cbc5f5b5dfcaac41aecbbf27391b1b5318a87d
XiaoxiangYu <xxyu@apache.org>
2022-10-19 15:18:12 +0800

    Fix import

--- src/core-common/src/test/java/org/apache/kylin/common/PropertiesDelegateTest.java
+++ src/core-common/src/test/java/org/apache/kylin/common/PropertiesDelegateTest.java
@@ -22,7 +22,6 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.stream.Collectors;
