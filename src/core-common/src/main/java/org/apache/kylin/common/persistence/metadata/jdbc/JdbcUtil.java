89b5dece0af41fe377ffa721f49356870dcf99ed
Jiawei Li <1019037687@qq.com>
2023-01-06 14:45:59 +0800

    KYLIN-5493 set epoch_target as primary key of epoch table

--- src/core-common/src/main/java/org/apache/kylin/common/persistence/metadata/jdbc/JdbcUtil.java
+++ src/core-common/src/main/java/org/apache/kylin/common/persistence/metadata/jdbc/JdbcUtil.java
@@ -123,23 +123,20 @@ public class JdbcUtil {
     }
 
     private static boolean isPrimaryKeyExists(Connection conn, String... tables) throws SQLException {
-        try {
-            for (String table : tables) {
-                try {
-                    val resultSet = conn.getMetaData().getPrimaryKeys(conn.getCatalog(), conn.getSchema(), table);
-                    if (resultSet.next()) {
-                        return true;
-                    }
-                } catch (Exception e) {
-                    logger.warn("get primary key from table {} failed", table, e);
+
+        for (String table : tables) {
+            try {
+                val resultSet = conn.getMetaData().getPrimaryKeys(conn.getCatalog(), conn.getSchema(), table);
+                if (resultSet.next()) {
+                    return true;
                 }
-            }
-        } finally {
-            if (!conn.isClosed()) {
-                conn.close();
+            } catch (Exception e) {
+                log.warn("get primary key from table {} failed", table, e);
             }
         }
-
+        if (!conn.isClosed()) {
+            conn.close();
+        }
         return false;
 
     }
