b33685668d3f9bbd518e202a43719518b0c6ebdf
jiawei.li <1019037687@qq.com>
2023-01-08 10:41:54 +0800

    KYLIN-5445 fix connection

--- src/core-common/src/main/java/org/apache/kylin/common/persistence/metadata/jdbc/JdbcUtil.java
+++ src/core-common/src/main/java/org/apache/kylin/common/persistence/metadata/jdbc/JdbcUtil.java
@@ -123,20 +123,23 @@ public class JdbcUtil {
     }
 
     private static boolean isPrimaryKeyExists(Connection conn, String... tables) throws SQLException {
-
-        for (String table : tables) {
-            try {
-                val resultSet = conn.getMetaData().getPrimaryKeys(conn.getCatalog(), conn.getSchema(), table);
-                if (resultSet.next()) {
-                    return true;
+        try {
+            for (String table : tables) {
+                try {
+                    val resultSet = conn.getMetaData().getPrimaryKeys(conn.getCatalog(), conn.getSchema(), table);
+                    if (resultSet.next()) {
+                        return true;
+                    }
+                } catch (Exception e) {
+                    log.warn("get primary key from table {} failed", table, e);
                 }
-            } catch (Exception e) {
-                log.warn("get primary key from table {} failed", table, e);
+            }
+        } finally {
+            if (!conn.isClosed()) {
+                conn.close();
             }
         }
-        if (!conn.isClosed()) {
-            conn.close();
-        }
+
         return false;
 
     }
