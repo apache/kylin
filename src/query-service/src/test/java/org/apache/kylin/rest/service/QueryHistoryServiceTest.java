4fb0007edc244aea80feb2dcda5aabe68ae90e88
liukun4515 <liukun@apache.org>
2023-02-09 17:11:18 +0800

    fix UT: add new metadata for cube planner

--- src/query-service/src/test/java/org/apache/kylin/rest/service/QueryHistoryServiceTest.java
+++ src/query-service/src/test/java/org/apache/kylin/rest/service/QueryHistoryServiceTest.java
@@ -365,7 +365,7 @@ public class QueryHistoryServiceTest extends NLocalFileMetadataTestCase {
 
         // get all tables
         tableMap = queryHistoryService.getQueryHistoryTableMap(null);
-        Assert.assertEquals(28, tableMap.size());
+        Assert.assertEquals(29, tableMap.size());
 
         // not existing project
         try {
