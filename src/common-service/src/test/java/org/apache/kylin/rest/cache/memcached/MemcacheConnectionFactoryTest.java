200c0165b42a55b10571518ea41eddecadff5131
zhennzhang <zhennzhang@ebay.com>
2023-02-28 10:36:31 +0800

    KYLIN-5388 Fix ut case

--- src/common-service/src/test/java/org/apache/kylin/rest/cache/memcached/MemcacheConnectionFactoryTest.java
+++ src/common-service/src/test/java/org/apache/kylin/rest/cache/memcached/MemcacheConnectionFactoryTest.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.kylin.rest.cache.memcached;
 
 import net.spy.memcached.ConnectionFactory;
@@ -14,7 +32,6 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.MockitoAnnotations;
 
 public class MemcacheConnectionFactoryTest extends NLocalFileMetadataTestCase {
 
@@ -22,7 +39,6 @@ public class MemcacheConnectionFactoryTest extends NLocalFileMetadataTestCase {
     public void setup() {
         overwriteSystemProp("kylin.cache.memcached.enabled", "true");
         createTestMetadata();
-        MockitoAnnotations.initMocks(this);
     }
 
     @After
