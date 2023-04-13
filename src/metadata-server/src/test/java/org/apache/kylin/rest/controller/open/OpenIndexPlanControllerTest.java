e878c32a09b8d01916bb45e88418aa4b2adcf597
lixiang <447399170@qq.com>
2023-04-13 18:54:04 +0800

    KYLIN-5606 minor fix (#30283)

--- src/metadata-server/src/test/java/org/apache/kylin/rest/controller/open/OpenIndexPlanControllerTest.java
+++ src/metadata-server/src/test/java/org/apache/kylin/rest/controller/open/OpenIndexPlanControllerTest.java
@@ -21,8 +21,11 @@ package org.apache.kylin.rest.controller.open;
 import static org.apache.kylin.common.constant.HttpConstant.HTTP_VND_APACHE_KYLIN_V4_PUBLIC_JSON;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.kylin.common.KylinConfig;
+import org.apache.kylin.common.exception.KylinException;
 import org.apache.kylin.common.util.JsonUtil;
 import org.apache.kylin.common.util.NLocalFileMetadataTestCase;
 import org.apache.kylin.guava30.shaded.common.collect.Lists;
@@ -32,8 +35,11 @@ import org.apache.kylin.rest.constant.Constant;
 import org.apache.kylin.rest.controller.NIndexPlanController;
 import org.apache.kylin.rest.request.OpenUpdateRuleBasedCuboidRequest;
 import org.apache.kylin.rest.request.UpdateRuleBasedCuboidRequest;
+import org.apache.kylin.rest.response.DiffRuleBasedIndexResponse;
+import org.apache.kylin.rest.response.EnvelopeResponse;
 import org.apache.kylin.rest.service.FusionIndexService;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -45,6 +51,7 @@ import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -60,7 +67,7 @@ public class OpenIndexPlanControllerTest extends NLocalFileMetadataTestCase {
     private FusionIndexService fusionIndexService;
 
     @Mock
-    private NIndexPlanController nIndexPlanController;
+    private NIndexPlanController indexPlanController;
 
     private MockMvc mockMvc;
 
@@ -85,10 +92,13 @@ public class OpenIndexPlanControllerTest extends NLocalFileMetadataTestCase {
     public void testCreateAggGroups() throws Exception {
         val request = UpdateRuleBasedCuboidRequest.builder().project("default")
                 .modelId("89af4ee2-2cdb-4b07-b39e-4c29856309aa").aggregationGroups(Lists.newArrayList()).build();
-
         OpenUpdateRuleBasedCuboidRequest openRequest = new OpenUpdateRuleBasedCuboidRequest();
         openRequest.setModelAlias("nmodel_basic");
         openRequest.setProject("default");
+        List<OpenUpdateRuleBasedCuboidRequest.OpenAggGroupRequest> aggregationGroups = new ArrayList<>();
+        val aggregationGroup = new OpenUpdateRuleBasedCuboidRequest.OpenAggGroupRequest();
+        aggregationGroups.add(aggregationGroup);
+        openRequest.setAggregationGroups(aggregationGroups);
         val modelManager = NDataModelManager.getInstance(KylinConfig.getInstanceFromEnv(), request.getProject());
         NDataModel model = modelManager.getDataModelDescByAlias(openRequest.getModelAlias());
 
@@ -98,17 +108,52 @@ public class OpenIndexPlanControllerTest extends NLocalFileMetadataTestCase {
                 .accept(MediaType.parseMediaType(HTTP_VND_APACHE_KYLIN_V4_PUBLIC_JSON)))
                 .andExpect(MockMvcResultMatchers.status().isOk());
         Mockito.verify(openIndexPlanController).updateRule(openRequest);
+
+        DiffRuleBasedIndexResponse tmp = new DiffRuleBasedIndexResponse("test", 1, 2, 0);
+        EnvelopeResponse<DiffRuleBasedIndexResponse> internalResponse = new EnvelopeResponse<>(KylinException.CODE_SUCCESS, tmp, "");
+        Mockito.when(indexPlanController.calculateDiffRuleBasedIndex(request)).thenReturn(internalResponse);
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/index_plans/agg_groups")
+                .contentType(MediaType.APPLICATION_JSON).content(JsonUtil.writeValueAsString(openRequest))
+                .accept(MediaType.parseMediaType(HTTP_VND_APACHE_KYLIN_V4_PUBLIC_JSON)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
     public void testCreateAggGroupsNotExistModel() throws Exception {
+        val request = UpdateRuleBasedCuboidRequest.builder().project("default")
+                .modelId("89af4ee2-2cdb-4b07-b39e-4c29856309aa").aggregationGroups(Lists.newArrayList()).build();
         OpenUpdateRuleBasedCuboidRequest openRequest = new OpenUpdateRuleBasedCuboidRequest();
         openRequest.setModelAlias("model_not_exist");
         openRequest.setProject("default");
-        mockMvc.perform(MockMvcRequestBuilders.put("/api/index_plans/agg_groups")
-                .contentType(MediaType.APPLICATION_JSON).content(JsonUtil.writeValueAsString(openRequest))
-                .accept(MediaType.parseMediaType(HTTP_VND_APACHE_KYLIN_V4_PUBLIC_JSON)))
-                .andExpect(MockMvcResultMatchers.status().is5xxServerError());
+        val modelManager = NDataModelManager.getInstance(KylinConfig.getInstanceFromEnv(), request.getProject());
+        NDataModel model = modelManager.getDataModelDescByAlias(openRequest.getModelAlias());
+        List<OpenUpdateRuleBasedCuboidRequest.OpenAggGroupRequest> aggregationGroups = new ArrayList<>();
+        val aggregationGroup = new OpenUpdateRuleBasedCuboidRequest.OpenAggGroupRequest();
+        aggregationGroups.add(aggregationGroup);
+        openRequest.setAggregationGroups(aggregationGroups);
+
+        Mockito.when(fusionIndexService.convertOpenToInternal(openRequest, model)).thenReturn(request);
+        MvcResult mvcResult = mockMvc
+                .perform(MockMvcRequestBuilders.put("/api/index_plans/agg_groups")
+                        .contentType(MediaType.APPLICATION_JSON).content(JsonUtil.writeValueAsString(openRequest))
+                        .accept(MediaType.parseMediaType(HTTP_VND_APACHE_KYLIN_V4_PUBLIC_JSON)))
+                .andExpect(MockMvcResultMatchers.status().is5xxServerError()).andReturn();
+        Assert.assertTrue(
+                mvcResult.getResponse().getContentAsString().contains("Can't find model. Please check and try again"));
+        Mockito.verify(openIndexPlanController).updateRule(openRequest);
+    }
+
+    @Test
+    public void testCreateAggGroupsNotExistAggregationGroups() throws Exception {
+        OpenUpdateRuleBasedCuboidRequest openRequest = new OpenUpdateRuleBasedCuboidRequest();
+        openRequest.setModelAlias("nmodel_basic");
+        openRequest.setProject("default");
+        MvcResult mvcResult = mockMvc
+                .perform(MockMvcRequestBuilders.put("/api/index_plans/agg_groups")
+                        .contentType(MediaType.APPLICATION_JSON).content(JsonUtil.writeValueAsString(openRequest))
+                        .accept(MediaType.parseMediaType(HTTP_VND_APACHE_KYLIN_V4_PUBLIC_JSON)))
+                .andExpect(MockMvcResultMatchers.status().is5xxServerError()).andReturn();
+        Assert.assertTrue(mvcResult.getResponse().getContentAsString().contains("KE-010043201"));
         Mockito.verify(openIndexPlanController).updateRule(openRequest);
     }
 }
