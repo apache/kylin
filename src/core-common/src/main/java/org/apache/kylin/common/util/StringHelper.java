ab705641440544890b6e11648fcd0b206ba74823
Yaguang Jia <jiayaguang@foxmail.com>
2023-04-27 13:34:22 +0800

    [DIRTY] Fix Sonar

--- src/core-common/src/main/java/org/apache/kylin/common/util/StringHelper.java
+++ src/core-common/src/main/java/org/apache/kylin/common/util/StringHelper.java
@@ -181,25 +181,6 @@ public class StringHelper {
         return r.toArray(new String[0]);
     }
 
-    // calculating length in UTF-8 of Java String without actually encoding it
-    public static int utf8Length(CharSequence sequence) {
-        int count = 0;
-        for (int i = 0, len = sequence.length(); i < len; i++) {
-            char ch = sequence.charAt(i);
-            if (ch <= 0x7F) {
-                count++;
-            } else if (ch <= 0x7FF) {
-                count += 2;
-            } else if (Character.isHighSurrogate(ch)) {
-                count += 4;
-                ++i;
-            } else {
-                count += 3;
-            }
-        }
-        return count;
-    }
-
     public static boolean equals(String a, String b) {
         return a == null ? b == null : a.equals(b);
     }
