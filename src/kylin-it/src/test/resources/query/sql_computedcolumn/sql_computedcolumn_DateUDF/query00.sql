38eeaa524dccaaeec0380420bcf0684cf0d1fd6d
liang.huang <83992752+lhuang09287750@users.noreply.github.com>
2023-05-20 09:46:30 +0800

    KYLIN-5691 make KYLIN5 date udfs  hold  string args

--- src/kylin-it/src/test/resources/query/sql_computedcolumn/sql_computedcolumn_DateUDF/query00.sql
+++ src/kylin-it/src/test/resources/query/sql_computedcolumn/sql_computedcolumn_DateUDF/query00.sql
@@ -21,5 +21,6 @@ select  --add_months(CAL_DT,2),
 	   count(date_part('YEAR',CAL_DT)),
 	   count(date_part('MONTH',CAL_DT)),
 	   count(date_part('DAY',CAL_DT)),
-	   count(datediff(CAL_DT,date'2019-8-3'))
+	   count(datediff(CAL_DT,date'2019-8-3')),
+	   count(datediff(cast(CAL_DT as string),'2019-8-3'))
 from TEST_KYLIN_FACT
