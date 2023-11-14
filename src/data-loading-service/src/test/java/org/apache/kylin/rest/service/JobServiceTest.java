b37818c3f86266e35573760d9250b4d47b73fda5
hongrong.cao <hongrong.cao@kyligence.io>
2023-11-14 17:21:35 +0800

    License Limitation related
    ---------
    Co-authored-by: longfei.jiang <longfei.jiang@kyligence.io>

--- src/data-loading-service/src/test/java/org/apache/kylin/rest/service/JobServiceTest.java
+++ src/data-loading-service/src/test/java/org/apache/kylin/rest/service/JobServiceTest.java
@@ -1578,6 +1578,7 @@ public class JobServiceTest extends NLocalFileMetadataTestCase {
         }, project);
 //        SecondStorageUtil.initModelMetaData(project, model);
 //        Assert.assertTrue(SecondStorageUtil.isModelEnable(project, model));
+//        Assert.assertTrue(SecondStorageUtil.isModelEnableWithoutCheckKylinInfo(project, model));
 
         val job3 = new DefaultExecutable();
         job3.setProject(getProject());
@@ -1610,6 +1611,7 @@ public class JobServiceTest extends NLocalFileMetadataTestCase {
         }, project);
 //        SecondStorageUtil.initModelMetaData(project, model);
 //        Assert.assertTrue(SecondStorageUtil.isModelEnable(project, model));
+//        Assert.assertTrue(SecondStorageUtil.isModelEnableWithoutCheckKylinInfo(project, model));
 
         val manager = NExecutableManager.getInstance(KylinConfig.getInstanceFromEnv(), getProject());
         val job = new DefaultExecutable();
